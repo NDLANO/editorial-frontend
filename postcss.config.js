c45171979988d827709ff7e8fcb830afd5d3af16
Øyvind Marthinsen <oma@knowit.no>
2017-05-18 11:43:12 +0200

    Add stylelint plugin

--- postcss.config.js
+++ postcss.config.js
@@ -10,18 +10,22 @@ const cssnext = require('postcss-cssnext');
 const postcssFocus = require('postcss-focus');
 const postcssImport = require('postcss-import');
 const postcssReporter = require('postcss-reporter');
+const stylelint = require('stylelint');
 
 module.exports = {
   plugins: [
     postcssImport({
       glob: true,
+      plugins: [
+        stylelint(),
+      ],
     }),
     postcssFocus(), // Add a :focus to every :hover
     cssnext({ // Allow future CSS features to be used, also auto-prefixes the CSS...
       browsers: ['last 2 versions', 'IE >= 10'], // ...based on this browser list
     }),
     postcssReporter({ // Posts messages from plugins to the terminal
-      clearMessages: true,
+      clearAllMessages: true,
     }),
   ],
 };

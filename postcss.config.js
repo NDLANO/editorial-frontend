c18483cad4a7f5462b8fea8f2624e22939ec3ba7
Øyvind Marthinsen <oma@knowit.no>
2017-05-18 16:28:35 +0200

    Remove postcss-reporter

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,6 @@
 const cssnext = require('postcss-cssnext');
 const postcssFocus = require('postcss-focus');
 const postcssImport = require('postcss-import');
-const postcssReporter = require('postcss-reporter');
 
 module.exports = {
   plugins: [
@@ -19,9 +18,6 @@ module.exports = {
     postcssFocus(), // Add a :focus to every :hover
     cssnext({ // Allow future CSS features to be used, also auto-prefixes the CSS...
       browsers: ['last 2 versions', 'IE >= 10'], // ...based on this browser list
-    }),
-    postcssReporter({ // Posts messages from plugins to the terminal
-      clearAllMessages: true,
     }),
   ],
 };

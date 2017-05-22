874c542781839dec7039786b8248de03cca9b5fc
Tanja Gruschke <tmg@knowit.no>
2017-05-22 15:04:59 +0200

    #316 removing twitter

--- src/containers/App/LoginProviders.js
+++ src/containers/App/LoginProviders.js
@@ -32,7 +32,6 @@ export const LoginProviders = (props) => {
       <ul className="vertical-menu">
         <li className="vertical-menu_item"><button onClick={() => loginSocialMedia('google-oauth2')} className="login-button btn-google c-button">Google</button></li>
         <li className="vertical-menu_item"><button onClick={() => loginSocialMedia('facebook')} className="login-button btn-fb c-button">Facebook</button></li>
-        <li className="vertical-menu_item"><button onClick={() => loginSocialMedia('twitter')} className="login-button btn-twitter c-button">Twitter</button></li>
       </ul>
     </OneColumn>
   );

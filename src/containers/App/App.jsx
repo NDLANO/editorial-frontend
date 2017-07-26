f3e282a89796b28891a15e38f9a012057d4c64c7
Christian Peter <christian150790@gmail.com>
2017-07-26 13:15:13 +0200

    Adds learning resource shell #394

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -27,6 +27,7 @@ import PrivateRoute from '../PrivateRoute/PrivateRoute';
 import WelcomePage from '../WelcomePage/WelcomePage';
 import SearchPage from '../SearchPage/SearchPage';
 import TopicArticlePage from '../TopicArticlePage/TopicArticlePage';
+import LearningResourcePage from '../LearningResourcePage/LearningResourcePage';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import ForbiddenPage from '../ForbiddenPage/ForbiddenPage';
 
@@ -59,6 +60,7 @@ export class App extends React.Component {
           <Route path="/logout" component={Logout} />
           <PrivateRoute path="/search" component={SearchPage} />
           <PrivateRoute path="/topic-article/" component={TopicArticlePage} />
+          <PrivateRoute path="/learning-resource" component={LearningResourcePage} />
           <Route path="/forbidden" component={ForbiddenPage} />
           <Route component={NotFoundPage} />
         </Switch>
---
+++ src/containers/LearningResourcePage/components/LearningResourceForm.jsx
@@ -0,0 +1,253 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React, { Component } from 'react';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
+import BEMHelper from 'react-bem-helper';
+import { Button } from 'ndla-ui';
+import { EditorState } from 'draft-js';
+import { injectT } from 'ndla-i18n';
+
+import reformed from '../../../components/reformed';
+import validateSchema from '../../../components/validateSchema';
+import {
+  TextField,
+  RichTextField,
+  PlainTextField,
+  RemainingCharacters,
+  Field,
+} from '../../../components/Fields';
+
+import converter from '../topicArticleContentConverter';
+import {
+  createEditorStateFromText,
+  getPlainTextFromEditorState,
+} from '../../../util/draftjsHelpers';
+import { parseEmbedTag } from '../../../util/embedTagHelpers';
+import TopicArticleVisualElement from './TopicArticleVisualElement';
+import TopicArticleMetadata from './TopicArticleMetadata';
+
+const DEFAULT_LICENSE = {
+  description: 'Creative Commons Attribution-ShareAlike 2.0 Generic',
+  license: 'by-sa',
+  url: 'https://creativecommons.org/licenses/by-sa/2.0/',
+};
+
+export const getInitialModel = (article = {}) => {
+  const visualElement = parseEmbedTag(article.visualElement) || {};
+  return {
+    id: article.id,
+    revision: article.revision,
+    title: article.title || '',
+    introduction: createEditorStateFromText(article.introduction),
+    content: article.content
+      ? converter.toEditorState(article.content)
+      : EditorState.createEmpty(),
+    tags: article.tags || [],
+    authors: article.copyright
+      ? article.copyright.authors.map(author => author.name)
+      : [],
+    copyright: article.copyright
+      ? article.copyright
+      : { license: DEFAULT_LICENSE, origin: '' },
+    metaDescription: createEditorStateFromText(article.metaDescription) || '',
+    visualElementId: visualElement.id || '',
+    visualElementCaption: visualElement.caption || '',
+    visualElementAlt: visualElement.alt || '',
+    visualElementType: visualElement.resource || '',
+  };
+};
+
+const classes = new BEMHelper({
+  name: 'topic-article-form',
+  prefix: 'c-',
+});
+
+class TopicArticleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    const {
+      model,
+      schema,
+      revision,
+      locale: language,
+      setSubmitted,
+    } = this.props;
+    if (!schema.isValid) {
+      setSubmitted(true);
+      return;
+    }
+    this.props.onUpdate({
+      id: model.id,
+      revision,
+      title: [{ title: model.title, language }],
+      introduction: [
+        {
+          introduction: getPlainTextFromEditorState(model.introduction),
+          language,
+        },
+      ],
+      tags: [{ tags: model.tags, language }],
+      content: [{ content: converter.toHtml(model.content), language }],
+      visualElement: [
+        {
+          content: `<embed data-size="fullbredde" data-align="" data-alt="${model.visualElementAlt}" data-caption="${model.visualElementCaption}" data-resource="${model.visualElementType}" data-resource_id="${model.visualElementId}" />`,
+          language,
+        },
+      ],
+      metaDescription: [
+        {
+          metaDescription: getPlainTextFromEditorState(model.metaDescription),
+          language,
+        },
+      ],
+      articleType: 'topic-article',
+      copyright: {
+        ...model.copyright,
+        authors: model.authors.map(name => ({ type: 'Forfatter', name })),
+      },
+    });
+  }
+
+  render() {
+    const {
+      t,
+      bindInput,
+      schema,
+      model,
+      submitted,
+      tags,
+      isSaving,
+    } = this.props;
+
+    const commonFieldProps = { bindInput, schema, submitted };
+    const visualElementTag = {
+      resource: model.visualElementType,
+      id: model.visualElementId,
+      caption: model.visualElementCaption,
+      alt: model.visualElementAlt,
+    };
+    return (
+      <form onSubmit={this.handleSubmit} {...classes()}>
+        <div {...classes('title')}>
+          {model.id
+            ? t('topicArticleForm.title.update')
+            : t('topicArticleForm.title.create')}
+        </div>
+        <TextField
+          label={t('topicArticleForm.fields.title.label')}
+          name="title"
+          big
+          noBorder
+          placeholder={t('topicArticleForm.fields.title.label')}
+          {...commonFieldProps}
+        />
+        <PlainTextField
+          label={t('topicArticleForm.fields.introduction.label')}
+          placeholder={t('topicArticleForm.fields.introduction.label')}
+          name="introduction"
+          noBorder
+          maxLength={300}
+          {...commonFieldProps}>
+          <RemainingCharacters
+            maxLength={300}
+            getRemainingLabel={(maxLength, remaining) =>
+              t('form.remainingCharacters', { maxLength, remaining })}
+            value={bindInput('introduction').value
+              .getCurrentContent()
+              .getPlainText()}
+          />
+        </PlainTextField>
+        <TopicArticleVisualElement
+          visualElementTag={visualElementTag}
+          commonFieldProps={commonFieldProps}
+          bindInput={bindInput}
+        />
+
+        <RichTextField
+          noBorder
+          label={t('topicArticleForm.fields.content.label')}
+          placeholder={t('topicArticleForm.fields.content.placeholder')}
+          name="content"
+          {...commonFieldProps}
+        />
+        <TopicArticleMetadata
+          classes={classes}
+          commonFieldProps={commonFieldProps}
+          bindInput={bindInput}
+          tags={tags}
+        />
+        <Field right>
+          <Button
+            submit
+            outline
+            disabled={isSaving}
+            {...classes('save-button')}>
+            {t('topicArticleForm.save')}
+          </Button>
+        </Field>
+      </form>
+    );
+  }
+}
+
+TopicArticleForm.propTypes = {
+  model: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+  }),
+  schema: PropTypes.shape({
+    fields: PropTypes.object.isRequired,
+    isValid: PropTypes.bool.isRequired,
+  }),
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  submitted: PropTypes.bool.isRequired,
+  bindInput: PropTypes.func.isRequired,
+  locale: PropTypes.string.isRequired,
+  revision: PropTypes.number,
+  setSubmitted: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  isSaving: PropTypes.bool.isRequired,
+};
+
+export default compose(
+  injectT,
+  reformed,
+  validateSchema({
+    title: {
+      required: true,
+    },
+    introduction: {
+      required: true,
+      maxLength: 300,
+    },
+    content: {
+      required: true,
+    },
+    metaDescription: {
+      required: true,
+      maxLength: 150,
+    },
+    visualElementId: {
+      required: true,
+    },
+    tags: {
+      minItems: 3,
+    },
+    authors: {
+      minItems: 1,
+    },
+  }),
+)(TopicArticleForm);

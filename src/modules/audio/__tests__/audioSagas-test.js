8a0dbba7f5790245e052bcc4addaf40e250975e1
Kien Vu <kien.vu@usit.uio.no>
2017-08-10 12:15:13 +0200

    #492 updated test for audio edit

--- src/modules/audio/__tests__/audioSagas-test.js
+++ src/modules/audio/__tests__/audioSagas-test.js
@@ -16,17 +16,63 @@ expectSaga.DEFAULT_TIMEOUT = 100;
 
 test('audioSagas watchUpdateAudio create new audio', () => {
   nock('http://ndla-api')
-    .post('/audio-api/v1/audio/')
+    .post('/audio-api/v1/audio')
     .reply(200, { id: '123', title: 'unit test' });
 
   return expectSaga(sagas.watchUpdateAudio)
     .withState({})
-    .dispatch(actions.setAudio({ id: '123', title: 'unit test' }))
-    .dispatch(actions.updateAudioSuccess())
+    .put(actions.setAudio({ id: '123', title: 'unit test' }))
+    .put(actions.updateAudioSuccess())
     .dispatch(
       actions.updateAudio({
         audio: { title: 'update title test' },
         history: { push: () => {} },
+      }),
+    )
+    .run({ silenceTimeout: true });
+});
+
+test('audioSagas watchFetchAudio fetch audio if not in state', () => {
+  nock('http://ndla-api')
+    .get('/audio-api/v1/audio/123?language=nb')
+    .reply(200, { id: 123, title: { title: 'unit test', langauge: 'nb' } });
+
+  return expectSaga(sagas.watchFetchAudio)
+    .withState({ audios: { all: {} } })
+    .put(
+      actions.setAudio({
+        id: 123,
+        title: { title: 'unit test', langauge: 'nb' },
+      }),
+    )
+    .dispatch(actions.fetchAudio({ id: 123, locale: 'nb' }))
+    .run({ silenceTimeout: true });
+});
+
+test('audioSagas watchFetchAudio do not refetch existing audio ', () =>
+  expectSaga(sagas.watchFetchAudio)
+    .withState({ audios: { all: { 123: { id: 123 } } } })
+    .dispatch(actions.fetchAudio({ id: 123, locale: 'nb' }))
+    .run({ silenceTimeout: true }));
+
+test('audioSagas watchUpdateAudio update audio', () => {
+  nock('http://ndla-api').put('/audio-api/v1/audio/123').reply(200, {
+    id: 123,
+    title: { title: 'unit test updated', language: 'en' },
+  });
+
+  return expectSaga(sagas.watchUpdateAudio)
+    .withState({})
+    .put(
+      actions.setAudio({
+        id: 123,
+        title: { title: 'unit test updated', language: 'en' },
+      }),
+    )
+    .put(actions.updateAudioSuccess())
+    .dispatch(
+      actions.updateAudio({
+        audio: { id: 123, title: { title: 'unit test', language: 'nb' } },
       }),
     )
     .run({ silenceTimeout: true });
--- src/modules/audio/__tests__/audioSelectors-test.js
+++ src/modules/audio/__tests__/audioSelectors-test.js
@@ -6,15 +6,39 @@
  *
  */
 
-import { getSaving } from '../audio';
+import { getAudio, getSaving } from '../audio';
+import { audio } from './mockAudios';
 
 const state = {
   locale: 'nb',
   audios: {
     isSaving: true,
+    all: {
+      [audio.id]: audio,
+      2: {
+        id: '2',
+        title: { title: 'Testing', language: 'en' },
+        tags: {
+          tags: [],
+        },
+      },
+      3: {
+        id: '3',
+        title: { title: 'Tester', language: 'nb' },
+        tags: {
+          tags: [],
+        },
+      },
+    },
   },
 };
 
 test('audioSelectors getSaving', () => {
   expect(getSaving(state)).toBe(true);
+});
+
+test('articleSelectors getAudio with id', () => {
+  expect(getAudio(1)(state).id).toBe('1');
+  expect(getAudio(2)(state).id).toBe('2');
+  expect(getAudio(3)(state).id).toBe('3');
 });

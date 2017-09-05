908a2e2356fd59b0808231ca3d2a012f8421fb6c
Øyvind Marthinsen <oma@knowit.no>
2017-09-05 13:34:18 +0200

    Test reformed with nested object

--- src/components/__tests__/reformed-test.js
+++ src/components/__tests__/reformed-test.js
@@ -17,27 +17,32 @@ const Form = props => {
   return (
     <form>
       <input type="text" {...bindInput('title')} />
-      <input type="text" {...bindInput('introcution')} />
+      <input type="text" {...bindInput('introduction')} />
       <input type="text" {...bindInput('content')} />
+      <input type="text" {...bindInput('image.alt')} />
+      <input type="text" {...bindInput('image.caption')} />
     </form>
   );
 };
+
 Form.propTypes = {
   bindInput: PropTypes.func.isRequired,
 };
 
 const Reformed = reformed(Form);
+const initialModel = {
+  title: 'Olé',
+  introduction: '',
+  content: 'Olé, Olé, Olé',
+  image: {
+    src: 'image.jpg',
+    caption: '',
+    alt: 'Alternative image text',
+  },
+};
 
 test('reformed HOC renderers form component correctly', () => {
-  const component = renderer.create(
-    <Reformed
-      initialModel={{
-        title: 'Olé',
-        introduction: '',
-        content: 'Olé, Olé, Olé',
-      }}
-    />,
-  );
+  const component = renderer.create(<Reformed initialModel={initialModel} />);
 
   expect(component.toJSON()).toMatchSnapshot();
 });

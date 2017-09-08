ef333939b78f5fc57e62ee286930ad8c6a5db39c
Øyvind Marthinsen <oma@knowit.no>
2017-09-08 13:06:30 +0200

    Handle submitted prop in plugin (wip)

--- src/components/SlateEditor/embedPlugin.js
+++ src/components/SlateEditor/embedPlugin.js
@@ -5,17 +5,72 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-
-import SlateFigure from './SlateFigure';
+import React from 'react';
+import PropTypes from 'prop-types';
+// import SlateFigure from './SlateFigure';
 
 export default function createEmbedPlugin() {
+  // const store = new Store();
+
+  // function editorDidMount(editor, props) {
+  //   store.set(props.pathname);
+  // }
+
+  // function editorDidUpdate(editor, props) {
+  //   store.set(props.pathname);
+  // }
+
+  class Test extends React.Component {
+    constructor(props) {
+      super();
+      console.log(props.editor.props.submitted);
+      this.state = {
+        submitted: props.editor.props.submitted,
+      };
+      this.onSubmittedChange = this.onSubmittedChange.bind(this);
+    }
+
+    componentDidMount() {
+      // store.on(this.onSubmittedChange);
+    }
+
+    componentWillUnmount() {
+      // store.off(this.onSubmittedChange);
+    }
+
+    onSubmittedChange(submitted) {
+      this.setState({ submitted });
+    }
+
+    render() {
+      return (
+        <div>
+          submitted: {this.state.submitted ? 'true' : 'false'}
+        </div>
+      );
+    }
+  }
+  Test.propTypes = {
+    editor: PropTypes.shape({
+      props: PropTypes.shape({
+        submitted: PropTypes.bool.isRequired,
+      }),
+    }),
+  };
+
   const schema = {
     nodes: {
-      embed: SlateFigure,
+      // embed: SlateFigure,
+      embed: Test,
     },
   };
 
   return {
     schema,
+    render: props => {
+      console.log(props);
+      //   store.set(props.pathname);
+      return props.children;
+    },
   };
 }

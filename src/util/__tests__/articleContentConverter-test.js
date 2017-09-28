2d1892a552be1d1541a13dc8ba929613901acf6f
Christian Peter <christian150790@gmail.com>
2017-09-28 15:11:04 +0200

    Adds tests #598

--- src/util/__tests__/articleContentConverter-test.js
+++ src/util/__tests__/articleContentConverter-test.js
@@ -9,13 +9,45 @@
 import {
   topicArticleContentToEditorState,
   topicArticleContentToHTML,
+  learningResourceContentToEditorState,
+  learningResourceContentToHTML,
 } from '../articleContentConverter';
 
 const contentHTML = `<section><h2>Lorem ipsum</h2></section>`;
+
+const learningResourceContent = `<section><h2>Lorem ipsum</h2><embed date-resource-type="image"/></section>`;
+
+const contentHTMLWithSections = `<section><h2>Section 1</h2></section><section><h2>Section 2</h2></section><section><h2>Section 3</h2></section>`;
 
 test('articleContentConverter convert topic article content to and from editorState', () => {
   // Todo fix test to handle empty text nodes
   const editorState = topicArticleContentToEditorState(contentHTML);
   const html = topicArticleContentToHTML(editorState);
+  expect(html).toMatchSnapshot();
+});
+
+test('articleContentConverter convert learningresource content to and from editorState', () => {
+  const editorState = learningResourceContentToEditorState(contentHTML);
+  const html = learningResourceContentToHTML(editorState);
+  expect(html).toMatchSnapshot();
+});
+
+test('articleContentConverter convert learningresource content with rule applied', () => {
+  const editorState = learningResourceContentToEditorState(
+    learningResourceContent,
+  );
+  expect(editorState).toMatchSnapshot();
+});
+
+test('articleContentConverter convert learningresource content with no rule applied', () => {
+  const editorState = learningResourceContentToEditorState(contentHTML);
+  expect(editorState).toMatchSnapshot();
+});
+
+test('articleContentConverter convert learningresource content with multiple sections to and from editorState', () => {
+  const editorState = learningResourceContentToEditorState(
+    contentHTMLWithSections,
+  );
+  const html = learningResourceContentToHTML(editorState);
   expect(html).toMatchSnapshot();
 });

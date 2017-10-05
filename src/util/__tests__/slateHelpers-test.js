2d75fe3dc211c3c2d9fecc60572fff9a45e5ea30
Christian Peter <christian150790@gmail.com>
2017-10-05 14:33:10 +0200

    Adds fix for heading test #576

--- src/util/__tests__/slateHelpers-test.js
+++ src/util/__tests__/slateHelpers-test.js
@@ -112,7 +112,7 @@ test('serialize footnote', () => {
 });
 
 test('deserializing any heading becomes heading-two', () => {
-  const serializer = new Html({ rules: [blockRules] });
+  const serializer = new Html({ rules: [blockRules], parseHtml: fragment });
   const deserialized = serializer.deserialize(
     '<h1>heading 1</h1><h2>heading 2</h2><h3>heading 3</h3><h4>heading 4</h4><h5>heading 5</h5><h6>heading 6</h6>',
   );

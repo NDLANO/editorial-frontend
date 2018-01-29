6e0359e42d089579efd7431a4c231354fe23c8dd
Sebastian Jara <sebastian.jara@knowit.no>
2018-01-29 08:50:38 +0100

    Updated tests

--- src/util/__tests__/taxonomyHelpers-test.js
+++ src/util/__tests__/taxonomyHelpers-test.js
@@ -19,7 +19,7 @@ test('taxonomy/flattenResourceTypes flattening', () => {
   );
 });
 
-test('taxonomy/spliceChangedItems no changes', () => {
+test('taxonomy/spliceChangedItems different item changes', () => {
   // No changes
   expect(spliceChangedItems([...filtersMock], [...filtersMock])).toEqual([
     [],
@@ -27,8 +27,8 @@ test('taxonomy/spliceChangedItems no changes', () => {
     [],
   ]);
 
-  // Create item
-  const createItem = [
+  // Deleting items
+  const keptItem = [
     {
       id: 'urn:filter:203679b7-7312-493c-97a2-5621437fa28e',
       name: 'VG1',
@@ -37,7 +37,7 @@ test('taxonomy/spliceChangedItems no changes', () => {
     },
   ];
 
-  expect(spliceChangedItems([...createItem], [...filtersMock])).toEqual([
+  expect(spliceChangedItems([...keptItem], [...filtersMock])).toEqual([
     [],
     [
       {
@@ -59,35 +59,38 @@ test('taxonomy/spliceChangedItems no changes', () => {
   ]);
 
   // Update Item
-  const updateFilter = [
+  const updatedItem = [
     {
       id: 'urn:filter:203679b7-7312-493c-97a2-5621437fa28e',
       name: 'VG1',
       connectionId: 'urn:resource-filter:0b6c66fe-c86f-4aa4-b5c0-8ebd26a53df4',
       relevanceId: 'urn:relevance:supplementary',
     },
+    {
+      connectionId: 'urn:resource-filter:314662e7-9a7d-4e02-9a8c-76a8c744b9e7',
+      id: 'urn:filter:04b81e41-dc1a-4635-b139-8fe25036ae45',
+      name: 'VG2',
+      relevanceId: 'urn:relevance:core',
+    },
+    {
+      connectionId: 'urn:resource-filter:c32f6276-8ff8-439d-ae3c-485e99e9d40a',
+      id: 'urn:filter:3b6061e1-b611-47b5-9e48-3346fa7e20c0',
+      name: 'VG2-YF',
+      relevanceId: 'urn:relevance:core',
+    },
   ];
 
   expect(
-    spliceChangedItems(updateFilter, filtersMock, 'id', 'id', 'relevanceId'),
+    spliceChangedItems(
+      [...updatedItem],
+      [...filtersMock],
+      'id',
+      'id',
+      'relevanceId',
+    ),
   ).toEqual([
     [],
-    [
-      {
-        connectionId:
-          'urn:resource-filter:314662e7-9a7d-4e02-9a8c-76a8c744b9e7',
-        id: 'urn:filter:04b81e41-dc1a-4635-b139-8fe25036ae45',
-        name: 'VG2',
-        relevanceId: 'urn:relevance:core',
-      },
-      {
-        connectionId:
-          'urn:resource-filter:c32f6276-8ff8-439d-ae3c-485e99e9d40a',
-        id: 'urn:filter:3b6061e1-b611-47b5-9e48-3346fa7e20c0',
-        name: 'VG2-YF',
-        relevanceId: 'urn:relevance:core',
-      },
-    ],
+    [],
     [
       {
         connectionId:
@@ -97,5 +100,47 @@ test('taxonomy/spliceChangedItems no changes', () => {
         relevanceId: 'urn:relevance:supplementary',
       },
     ],
+  ]);
+
+  // Update Item
+  const createNewItem = [
+    {
+      id: 'urn:filter:3ae5a086-8444-43c3-8fa4-db869e7292d2',
+      name: 'Medieuttrykk',
+      connectionId: 'urn:resource-filter:5e5de687-2412-46dc-b657-c09c1f340c94',
+      relevanceId: 'urn:relevance:core',
+    },
+    {
+      id: 'urn:filter:203679b7-7312-493c-97a2-5621437fa28e',
+      name: 'VG1',
+      connectionId: 'urn:resource-filter:0b6c66fe-c86f-4aa4-b5c0-8ebd26a53df4',
+      relevanceId: 'urn:relevance:core',
+    },
+    {
+      connectionId: 'urn:resource-filter:314662e7-9a7d-4e02-9a8c-76a8c744b9e7',
+      id: 'urn:filter:04b81e41-dc1a-4635-b139-8fe25036ae45',
+      name: 'VG2',
+      relevanceId: 'urn:relevance:core',
+    },
+    {
+      connectionId: 'urn:resource-filter:c32f6276-8ff8-439d-ae3c-485e99e9d40a',
+      id: 'urn:filter:3b6061e1-b611-47b5-9e48-3346fa7e20c0',
+      name: 'VG2-YF',
+      relevanceId: 'urn:relevance:core',
+    },
+  ];
+
+  expect(spliceChangedItems([...createNewItem], [...filtersMock])).toEqual([
+    [
+      {
+        connectionId:
+          'urn:resource-filter:5e5de687-2412-46dc-b657-c09c1f340c94',
+        id: 'urn:filter:3ae5a086-8444-43c3-8fa4-db869e7292d2',
+        name: 'Medieuttrykk',
+        relevanceId: 'urn:relevance:core',
+      },
+    ],
+    [],
+    [],
   ]);
 });

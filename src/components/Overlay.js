8b091a9fe66ab894d46b89ba53f77b8f827664df
Christian Peter <christian150790@gmail.com>
2018-02-20 14:23:33 +0100

    Adds license in top of file. Added to all style files. #800

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,22 +1,31 @@
+/**
+ * Copyright (c) 2017-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import BEMHelper from 'react-bem-helper';
 
-const Overlay = ({ onExit, cssModifiers }) => {
-  const overlayClasses = new BEMHelper({
-    name: 'overlay',
-    prefix: 'c-',
-  });
+const overlayClasses = new BEMHelper({
+  name: 'overlay',
+  prefix: 'c-',
+});
 
-  return (
+const Overlay = ({ onExit, cssModifiers }) =>
+  onExit ? (
     <div
       onClick={onExit}
       role="button"
       tabIndex={0}
       {...overlayClasses('', cssModifiers)}
     />
+  ) : (
+    <div {...overlayClasses('', cssModifiers)} />
   );
-};
 
 Overlay.propTypes = {
   onExit: PropTypes.func.isRequired,

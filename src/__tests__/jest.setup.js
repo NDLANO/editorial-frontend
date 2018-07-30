28a6e8733d60584fad5e5d2bd91c55384e9c1687
Sebastian Jara <sebastian.jara@knowit.no>
2018-07-30 17:03:50 +0200

    Test config update

--- src/__tests__/jest.setup.js
+++ src/__tests__/jest.setup.js
@@ -1,3 +1,31 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { expectSaga } from 'redux-saga-test-plan';
+
+global.DEFAULT_TIMEOUT = process.env.DEFAULT_TIMEOUT
+  ? parseInt(process.env.DEFAULT_TIMEOUT, 10)
+  : 100;
+
 jest.setTimeout = process.env.JEST_TIMEOUT
   ? parseInt(process.env.JEST_TIMEOUT, 10)
   : 30000;
+
+expectSaga.DEFAULT_TIMEOUT = global.DEFAULT_TIMEOUT;
+
+/* eslint-disable */
+// fix: `matchMedia` not present, legacy browsers require a polyfill
+global.matchMedia =
+  global.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener() {},
+      removeListener() {},
+    };
+  };

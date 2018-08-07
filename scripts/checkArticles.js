58b70c5460315d025bf8efda93fcad332b215486
Christian Peter <christian150790@gmail.com>
2018-08-07 11:54:37 +0200

    Runs prettier on check articles #1049

--- scripts/checkArticles.js
+++ scripts/checkArticles.js
@@ -7,14 +7,15 @@
  */
 
 import jsdom from 'jsdom';
-import { learningResourceContentToEditorValue, learningResourceContentToHTML } from '../src/util/articleContentConverter';
 import {
-  resolveJsonOrRejectWithError,
-} from '../src/util/apiHelpers';
+  learningResourceContentToEditorValue,
+  learningResourceContentToHTML,
+} from '../src/util/articleContentConverter';
+import { resolveJsonOrRejectWithError } from '../src/util/apiHelpers';
 
 const chalk = require('chalk');
 const queryString = require('query-string');
-const fetch = require('isomorphic-fetch')
+const fetch = require('isomorphic-fetch');
 const fs = require('fs');
 
 const url = 'https://staging.api.ndla.no/article-api/v2/articles/';
@@ -26,37 +27,46 @@ let token = '';
 /* eslint no-console: 0 */
 async function fetchSystemAccessToken() {
   token = await fetch(`https://ndla.eu.auth0.com/oauth/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        grant_type: 'client_credentials',
-        client_id: process.env.NDLA_TEST_CLIENT_ID,
-        client_secret: process.env.NDLA_TEST_CLIENT_SECRET,
-        audience: 'ndla_system',
-      }),
-      json: true,
-    }).then(resolveJsonOrRejectWithError)
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      grant_type: 'client_credentials',
+      client_id: process.env.NDLA_TEST_CLIENT_ID,
+      client_secret: process.env.NDLA_TEST_CLIENT_SECRET,
+      audience: 'ndla_system',
+    }),
+    json: true,
+  }).then(resolveJsonOrRejectWithError);
 }
 
 async function fetchArticles(query) {
-   const result = await fetch(`${url}?${queryString.stringify(query)}`, {
-      headers: {
-        Authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(resolveJsonOrRejectWithError).catch((err) => console.log(`${chalk.red(`Search with query page: ${query.page} is failing`)}`, err));
-    return result;
+  const result = await fetch(`${url}?${queryString.stringify(query)}`, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  })
+    .then(resolveJsonOrRejectWithError)
+    .catch(err =>
+      console.log(
+        `${chalk.red(`Search with query page: ${query.page} is failing`)}`,
+        err,
+      ),
+    );
+  return result;
 }
 
 async function fetchArticle(id) {
-    await sleep(100); // eslint-disable-line
-    let result;
-    result = await fetch(`${url}${id}`, {
-      headers: {
-        Authorization: `Bearer ${token.access_token}`,
-      },
-    }).then(resolveJsonOrRejectWithError).catch(async (err) => {
+  await sleep(100); // eslint-disable-line
+  let result;
+  result = await fetch(`${url}${id}`, {
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  })
+    .then(resolveJsonOrRejectWithError)
+    .catch(async err => {
       if (err.status === 401) {
         await fetchSystemAccessToken();
         result = await fetchArticle(id);
@@ -64,7 +74,7 @@ async function fetchArticle(id) {
         console.log(`${chalk.red(`Article with id ${id} is failing`)}`, err);
       }
     });
-    return result;
+  return result;
 }
 
 function sleep(ms) {
@@ -72,72 +82,98 @@ function sleep(ms) {
 }
 
 async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index+= 1) {
-    await callback(array[index], index, array) //eslint-disable-line
+  for (let index = 0; index < array.length; index += 1) {
+    await callback(array[index], index, array); //eslint-disable-line
   }
 }
 
-async function fetchAllArticles(){
+async function fetchAllArticles() {
   const query = {
     page: 1,
     'page-size': 100,
   };
-  const articleIds = []
+  const articleIds = [];
   const firstResult = await fetchArticles(query, token);
-  articleIds.push(...firstResult.results.map(article => article.id))
-  const numberOfPages = Math.ceil(firstResult.totalCount / firstResult.pageSize);
+  articleIds.push(...firstResult.results.map(article => article.id));
+  const numberOfPages = Math.ceil(
+    firstResult.totalCount / firstResult.pageSize,
+  );
   const requests = [];
   const estimatedTime = (numberOfPages * query['page-size'] * 0.5) / 60;
-  console.log(`Fetching ${numberOfPages} pages with a page size of ${query['page-size']}. Estimated time is ${estimatedTime} minutes`)
-  for (let i = 2; i < numberOfPages + 1; i+= 1) {
-    requests.push(fetchArticles({...query, page: i}, token));
+  console.log(
+    `Fetching ${numberOfPages} pages with a page size of ${
+      query['page-size']
+    }. Estimated time is ${estimatedTime} minutes`,
+  );
+  for (let i = 2; i < numberOfPages + 1; i += 1) {
+    requests.push(fetchArticles({ ...query, page: i }, token));
     await sleep(500); // eslint-disable-line
-    console.log(`🔍  ${chalk.green(`Fetching page ${i} with page size ${query['page-size']}`)}`)  //eslint-disable-line
+    console.log(
+      `🔍  ${chalk.green(
+        `Fetching page ${i} with page size ${query['page-size']}`,
+      )}`,
+    ); //eslint-disable-line
   }
 
-  const requestPromises = await Promise.all(requests)
-  requestPromises.forEach((result) => {
+  const requestPromises = await Promise.all(requests);
+  requestPromises.forEach(result => {
     const articles = result ? result.results.map(article => article.id) : [];
-    articleIds.push(...articles)
+    articleIds.push(...articles);
   });
   return articleIds;
 }
 
-
 async function testArticle(id, article) {
   try {
     if (article) {
-      const converted = learningResourceContentToEditorValue(article.content.content, fragment);
+      const converted = learningResourceContentToEditorValue(
+        article.content.content,
+        fragment,
+      );
       learningResourceContentToHTML(converted);
-      console.log(`${chalk.green(`Id ${id} was sucessfully converted to slate and back. The article id is: ${article.id}`)}`);
+      console.log(
+        `${chalk.green(
+          `Id ${id} was sucessfully converted to slate and back. The article id is: ${
+            article.id
+          }`,
+        )}`,
+      );
     }
   } catch (err) {
-    errors.push({error: err, id})
+    errors.push({ error: err, id });
     console.log(`${chalk.red(`Article with id ${id} is failing`)}`, err); //eslint-disable-line
   }
 }
 
- async function run() {
+async function run() {
   await fetchSystemAccessToken();
   const readFromFile = process.argv[2] !== '-write';
   if (!readFromFile) {
     const articles = [];
     const articleIds = await fetchAllArticles();
-    await asyncForEach(articleIds, async (id) => {
+    await asyncForEach(articleIds, async id => {
       const article = await fetchArticle(id, token);
       articles.push(article);
-      await testArticle(id, article)
+      await testArticle(id, article);
     });
-    fs.writeFileSync('./scripts/articles.json', JSON.stringify(articles), 'utf8');
+    fs.writeFileSync(
+      './scripts/articles.json',
+      JSON.stringify(articles),
+      'utf8',
+    );
   } else {
-    const contents = fs.readFileSync("./scripts/articles.json");
+    const contents = fs.readFileSync('./scripts/articles.json');
 
     const articles = await JSON.parse(contents);
     articles.forEach(article => {
       if (article) testArticle(article.id, article);
-    })
+    });
   }
-  console.log(`Total errors: ${errors.length}. Articles that is failing is: ${errors ? errors.map(error => error ? error.id : '') : '[]'}`)  //eslint-disable-line
+  console.log(
+    `Total errors: ${errors.length}. Articles that is failing is: ${
+      errors ? errors.map(error => (error ? error.id : '')) : '[]'
+    }`,
+  ); //eslint-disable-line
 }
 /* eslint-enable */
 run();

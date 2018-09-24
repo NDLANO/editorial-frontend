ff8211dda0e8bd44f70b84d11c6903be0b6fd579
Øyvind Marthinsen <oma@knowit.no>
2018-09-24 11:34:47 +0200

    feat: show admin buttons on seperate line. Update messages

--- src/containers/Form/components/FormStatusActions.jsx
+++ src/containers/Form/components/FormStatusActions.jsx
@@ -6,11 +6,46 @@
  *
  */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { injectT } from 'ndla-i18n';
 import { Button } from 'ndla-ui';
 import { formClasses } from '..';
+import { PossibleStatusShape } from '../../../shapes';
+
+const isAdminStatus = status =>
+  status === 'PUBLISHED' || status === 'UNPUBLISHED';
+
+function AdminActions({ possibleStatuses, articleStatus, onUpdateStatus, t }) {
+  if (possibleStatuses[articleStatus.current].find(isAdminStatus)) {
+    return (
+      <div {...formClasses('actions')}>
+        {possibleStatuses[articleStatus.current].map(status => {
+          if (status === 'PUBLISHED' || status === 'UNPUBLISHED') {
+            return (
+              <Button
+                key={`status_action_${status}`}
+                onClick={() => onUpdateStatus(status)}>
+                {t(`form.status.actions.${status}`)}
+              </Button>
+            );
+          }
+          return null;
+        })}
+      </div>
+    );
+  }
+  return null;
+}
+
+AdminActions.propTypes = {
+  articleStatus: PropTypes.shape({
+    current: PropTypes.string,
+    other: PropTypes.arrayOf(PropTypes.string),
+  }),
+  onUpdateStatus: PropTypes.func.isRequired,
+  possibleStatuses: PossibleStatusShape.isRequired,
+};
 
 const FormStatusActions = props => {
   const {
@@ -30,22 +65,33 @@ const FormStatusActions = props => {
   }
 
   return (
-    <div {...formClasses('actions')}>
-      {possibleStatuses[articleStatus.current].map(status => (
-        <Button
-          key={`status_action_${status}`}
-          onClick={() => onUpdateStatus(status)}>
-          {t(`form.status.actions.${status}`)}
-        </Button>
-      ))}
-      {model.id ? (
-        <Button outline onClick={onValidateClick}>
-          {t('form.validate')}
-        </Button>
-      ) : (
-        ''
-      )}
-    </div>
+    <Fragment>
+      <span {...formClasses('title')}>{t('form.status.change')}</span>
+      <div {...formClasses('actions')}>
+        {possibleStatuses[articleStatus.current]
+          .filter(status => !isAdminStatus(status))
+          .map(status => (
+            <Button
+              key={`status_action_${status}`}
+              onClick={() => onUpdateStatus(status)}>
+              {t(`form.status.actions.${status}`)}
+            </Button>
+          ))}
+        {model.id ? (
+          <Button outline onClick={onValidateClick}>
+            {t('form.validate')}
+          </Button>
+        ) : (
+          ''
+        )}
+      </div>
+      <AdminActions
+        t={t}
+        possibleStatuses={possibleStatuses}
+        articleStatus={articleStatus}
+        onUpdateStatus={onUpdateStatus}
+      />
+    </Fragment>
   );
 };
 
@@ -59,20 +105,7 @@ FormStatusActions.propTypes = {
   }),
   onUpdateStatus: PropTypes.func.isRequired,
   onValidateClick: PropTypes.func.isRequired,
-  possibleStatuses: PropTypes.shape({
-    CREATED: PropTypes.arrayOf(PropTypes.string),
-    PROPOSAL: PropTypes.arrayOf(PropTypes.string),
-    AWAITING_QUALITY_ASSURANCE: PropTypes.arrayOf(PropTypes.string),
-    DRAFT: PropTypes.arrayOf(PropTypes.string),
-    USER_TEST: PropTypes.arrayOf(PropTypes.string),
-    IMPORTED: PropTypes.arrayOf(PropTypes.string),
-    QUALITY_ASSURED: PropTypes.arrayOf(PropTypes.string),
-    PUBLISHED: PropTypes.arrayOf(PropTypes.string),
-    AWAITING_UNPUBLISHING: PropTypes.arrayOf(PropTypes.string),
-    UNPUBLISHED: PropTypes.arrayOf(PropTypes.string),
-    ARCHIEVED: PropTypes.arrayOf(PropTypes.string),
-    QUEUED_FOR_PUBLISHING: PropTypes.arrayOf(PropTypes.string),
-  }),
+  possibleStatuses: PossibleStatusShape.isRequired,
 };
 
 FormStatusActions.defaultProps = {

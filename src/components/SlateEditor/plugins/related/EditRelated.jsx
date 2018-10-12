1bb86f4323edb925123e9401dfdecac6739d9c36
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-10-12 17:45:18 +0200

    create placeholder height after new height is calculated

--- src/components/SlateEditor/plugins/related/EditRelated.jsx
+++ src/components/SlateEditor/plugins/related/EditRelated.jsx
@@ -41,16 +41,17 @@ class EditRelated extends React.PureComponent {
     const { placeholderEl, embedEl } = this;
 
     const bodyRect = document.body.getBoundingClientRect();
-    const embedRect = embedEl.getBoundingClientRect();
     const placeholderRect = placeholderEl.getBoundingClientRect();
 
     // Placing embed within placeholder div on mount
-    placeholderEl.style.height = `${embedRect.height + 120}px`;
     embedEl.style.position = 'absolute';
     embedEl.style.background = 'white';
     embedEl.style.top = `${placeholderRect.top - bodyRect.top + 50}px`;
     embedEl.style.left = `${placeholderRect.left - 50}px`;
     embedEl.style.width = `${placeholderRect.width + 100}px`;
+
+    const embedRect = embedEl.getBoundingClientRect();
+    placeholderEl.style.height = `${embedRect.height}px`;
   }
 
   componentDidUpdate() {

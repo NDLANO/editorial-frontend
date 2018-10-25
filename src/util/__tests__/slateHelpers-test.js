db36d4eb66f1e38aee53f39130011e7ff4791aeb
Øyvind Marthinsen <oma@knowit.no>
2018-10-25 14:09:14 +0200

    Revert "reorganize test"
    This reverts commit fdb56f9ab4ebddd9ac30469fdccebbb92cd9e37a.

--- src/util/__tests__/slateHelpers-test.js
+++ src/util/__tests__/slateHelpers-test.js
@@ -26,13 +26,19 @@ import {
   spanWithAttributesValue,
 } from './slateMockValues';
 import {
+  footnoteRule,
   learningResourceEmbedRule,
   findNodesByType,
   divRule,
-  footnoteRule,
   toJSON,
+  blockRules,
+  inlineRules,
+  orderListRules,
+  unorderListRules,
+  tableRules,
+  listItemRule,
+  paragraphRule,
   learningResourceRules,
-  topicArticeRules,
 } from '../slateHelpers';
 import {
   standardArticleHTML,
@@ -42,16 +48,6 @@ import {
 } from './slateMockArticle';
 
 const { fragment } = jsdom.JSDOM;
-
-const learningResourceSerializer = new Html({
-  rules: learningResourceRules,
-  parseHtml: fragment,
-});
-
-const topicArticleSerializer = new Html({
-  rules: topicArticeRules,
-  parseHtml: fragment,
-});
 
 test('serialize embed block', () => {
   const obj = {
@@ -87,7 +83,8 @@ test('serialize bodybox block', () => {
 });
 
 test('deserialize bodybox block', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({ rules: [divRule], parseHtml: fragment });
+  const deserialized = serializer.deserialize(
     '<div class="c-bodybox">test</div>',
   );
 
@@ -95,31 +92,52 @@ test('deserialize bodybox block', () => {
 });
 
 test('deserializing a normal div block', () => {
+  const serializer = new Html({
+    rules: [divRule, paragraphRule],
+    parseHtml: fragment,
+  });
   const div = '<div><p>A paragraph</p></div>';
-  const deserialized = learningResourceSerializer.deserialize(div);
+  const deserialized = serializer.deserialize(div);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing a normal div block', () => {
+  const serializer = new Html({
+    rules: [paragraphRule, divRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(normalDivValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing a span with attributes', () => {
+  const serializer = new Html({
+    rules: [inlineRules],
+    parseHtml: fragment,
+  });
   const span = '<span lang="en">Hyper Text Markup Language</span>';
-  const deserialized = learningResourceSerializer.deserialize(span);
+  const deserialized = serializer.deserialize(span);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing a span with attributes', () => {
+  const serializer = new Html({
+    rules: [paragraphRule, inlineRules],
+    parseHtml: fragment,
+  });
+
   const value = Value.fromJSON(spanWithAttributesValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserialize footnote', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({
+    rules: [footnoteRule],
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
     `<embed data-title="Apple Watch" data-year="2015" data-resource="footnote" data-authors="Jony Ive" data-edition="2" data-publisher="Apple" data-type=""><embed data-title="iPhone" data-year="2007" data-resource="footnote" data-authors="Steve Jobs;Jony Ive" data-edition="1" data-publisher="Apple" data-type="">`,
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
@@ -156,7 +174,8 @@ test('serialize footnote', () => {
 });
 
 test('deserializing any heading becomes heading-two except heading-three', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({ rules: [blockRules], parseHtml: fragment });
+  const deserialized = serializer.deserialize(
     '<h1>heading 1</h1><h2>heading 2</h2><h3>heading 3</h3><h4>heading 4</h4><h5>heading 5</h5><h6>heading 6</h6>',
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
@@ -166,105 +185,165 @@ const tableHTML =
   '<table><thead><tr><th>column 1</th><th>column 2</th></tr></thead><tbody><tr><td>column 1</td><td>column 2</td></tr></tbody></table>';
 
 test('deserializing table', () => {
-  const deserialized = learningResourceSerializer.deserialize(tableHTML);
+  const serializer = new Html({ rules: [tableRules], parseHtml: fragment });
+  const deserialized = serializer.deserialize(tableHTML);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing table', () => {
+  const serializer = new Html({ rules: [tableRules], parseHtml: fragment });
   const value = Value.fromJSON(tableSlateValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatch(tableHTML);
 });
 
 test('deserializing bullet list', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({
+    rules: [blockRules, unorderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
     '<ul><li>Rad 1</li><li>Rad 2</li><li>Rad 3</li></ul>',
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing bullet list', () => {
+  const serializer = new Html({
+    rules: [blockRules, unorderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(listValue('bulleted-list'));
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing two column list', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({
+    rules: [blockRules, unorderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
     '<ul class="o-list--two-columns"><li>Rad 1</li><li>Rad 2</li><li>Rad 3</li></ul>',
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing two column list', () => {
+  const serializer = new Html({
+    rules: [blockRules, unorderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(listValue('two-column-list'));
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing numbered list', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({
+    rules: [blockRules, orderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
     '<ol><li>Rad 1</li><li>Rad 2</li><li>Rad 3</li></ol>',
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing numbered list', () => {
+  const serializer = new Html({
+    rules: [blockRules, orderListRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(listValue('numbered-list'));
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing alphabetical list', () => {
-  const deserialized = learningResourceSerializer.deserialize(
+  const serializer = new Html({
+    rules: [orderListRules, blockRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
     '<ol data-type="letters"><li>Rad 1</li><li>Rad 2</li><li>Rad 3</li></ol>',
   );
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing letter list', () => {
+  const serializer = new Html({
+    rules: [orderListRules, blockRules, listItemRule, paragraphRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(listValue('letter-list'));
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing list with paragraph inside li elements', () => {
+  const serializer = new Html({
+    rules: [
+      blockRules,
+      unorderListRules,
+      orderListRules,
+      listItemRule,
+      paragraphRule,
+    ],
+    parseHtml: fragment,
+  });
   const listWithParagraphs =
     '<ul><li><p>paragraph 1</p></li><li><p>paragraph 2</p></li><li><p><strong>bold paragraph 3</strong></p></li></ul>';
-  const deserialized = learningResourceSerializer.deserialize(
-    listWithParagraphs,
-  );
+  const deserialized = serializer.deserialize(listWithParagraphs);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing details box with summary element', () => {
+  const serializer = new Html({
+    rules: [blockRules],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(detailsBoxValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing details box with summary element', () => {
+  const serializer = new Html({
+    rules: [blockRules, paragraphRule],
+    parseHtml: fragment,
+  });
   const detailsWithSummary =
     '<details><summary>Summary text</summary><p>Details text</p></details>';
-  const deserialized = learningResourceSerializer.deserialize(
-    detailsWithSummary,
-  );
+  const deserialized = serializer.deserialize(detailsWithSummary);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('deserializing a section', () => {
+  const serializer = new Html({
+    rules: [blockRules, paragraphRule],
+    parseHtml: fragment,
+  });
   const section = '<section><p>Paragraph text</p></sectiob>';
-  const deserialized = learningResourceSerializer.deserialize(section);
+  const deserialized = serializer.deserialize(section);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing section', () => {
+  const serializer = new Html({
+    rules: [blockRules, paragraphRule],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(sectionValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing any heading should result in heading-two except heading-three', () => {
+  const serializer = new Html({
+    rules: [blockRules, paragraphRule],
+    parseHtml: fragment,
+  });
   const h1 = '<h1>Heading 1</h1>';
   const h2 = '<h2>Heading 2</h2>';
   const h3 = '<h3>Heading 3</h3>';
@@ -272,12 +351,12 @@ test('deserializing any heading should result in heading-two except heading-thre
   const h5 = '<h5>Heading 5</h5>';
   const h6 = '<h6>Heading 6</h6>';
 
-  const deserializedH1 = learningResourceSerializer.deserialize(h1);
-  const deserializedH2 = learningResourceSerializer.deserialize(h2);
-  const deserializedH3 = learningResourceSerializer.deserialize(h3);
-  const deserializedH4 = learningResourceSerializer.deserialize(h4);
-  const deserializedH5 = learningResourceSerializer.deserialize(h5);
-  const deserializedH6 = learningResourceSerializer.deserialize(h6);
+  const deserializedH1 = serializer.deserialize(h1);
+  const deserializedH2 = serializer.deserialize(h2);
+  const deserializedH3 = serializer.deserialize(h3);
+  const deserializedH4 = serializer.deserialize(h4);
+  const deserializedH5 = serializer.deserialize(h5);
+  const deserializedH6 = serializer.deserialize(h6);
 
   expect(toJSON(deserializedH1)).toMatchSnapshot();
   expect(toJSON(deserializedH2)).toMatchSnapshot();
@@ -288,57 +367,85 @@ test('deserializing any heading should result in heading-two except heading-thre
 });
 
 test('serializing heading-two', () => {
+  const serializer = new Html({
+    rules: [blockRules],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(headingTwoValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing a quote', () => {
+  const serializer = new Html({
+    rules: [blockRules],
+    parseHtml: fragment,
+  });
   const quote =
     '<blockquote>This quote should be both smart and wise</blockquote>';
-  const deserialized = learningResourceSerializer.deserialize(quote);
+  const deserialized = serializer.deserialize(quote);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing quote', () => {
+  const serializer = new Html({
+    rules: [blockRules],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(quoteValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserializing a br', () => {
+  const serializer = new Html({
+    rules: [blockRules, paragraphRule],
+    parseHtml: fragment,
+  });
   const br = '<br />';
-  const deserialized = learningResourceSerializer.deserialize(br);
+  const deserialized = serializer.deserialize(br);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing br', () => {
+  const serializer = new Html({
+    rules: [blockRules],
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(brValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(serialized).toMatchSnapshot();
 });
 
 test('deserialize standard article', () => {
-  const deserialized = learningResourceSerializer.deserialize(
-    standardArticleHTML,
-  );
+  const serializer = new Html({
+    rules: learningResourceRules,
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(standardArticleHTML);
   expect(toJSON(deserialized)).toMatchSnapshot();
 });
 
 test('serializing standard article', () => {
+  const serializer = new Html({
+    rules: learningResourceRules,
+    parseHtml: fragment,
+  });
   const value = Value.fromJSON(standardArticleValue);
-  const serialized = learningResourceSerializer.serialize(value);
+  const serialized = serializer.serialize(value);
   expect(global.prettifyHTML(serialized)).toMatchSnapshot();
 });
 
 test('deserialize em mark that contains embed footnote', () => {
-  const string =
-    '<em>NDLA<embed data-authors="Peter, Christian" data-edition="" data-publisher="Knowit" data-resource="footnote" data-title="Javascript." data-type="Programming" data-year="2018"> </em>';
-  const deserialized = learningResourceSerializer.deserialize(string);
-  const deserializedTopic = topicArticleSerializer.deserialize(string);
+  const serializer = new Html({
+    rules: learningResourceRules,
+    parseHtml: fragment,
+  });
+  const deserialized = serializer.deserialize(
+    '<em>NDLA<embed data-authors="Peter, Christian" data-edition="" data-publisher="Knowit" data-resource="footnote" data-title="Javascript." data-type="Programming" data-year="2018"> </em>',
+  );
 
   expect(toJSON(deserialized)).toMatchSnapshot();
-  expect(toJSON(deserializedTopic)).toMatchSnapshot();
 });
 
 test('deserializing', () => {

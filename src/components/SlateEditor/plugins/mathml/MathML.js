8035b39d445420b119a508001ff1840eb6292c6c
Øyvind Marthinsen <oma@knowit.no>
2018-10-30 12:38:04 +0100

    feat: enable mathjax in prview draft

--- src/components/SlateEditor/plugins/mathml/MathML.js
+++ src/components/SlateEditor/plugins/mathml/MathML.js
@@ -9,27 +9,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Types from 'slate-prop-types';
-import styled from 'react-emotion';
 import { EditorShape } from '../../../../shapes';
-
-const StyledMathWrapper = styled('span')`
-  /* Don't know if this is necessary (copied from ndla-frontend) */
-  .MJXc-display {
-    display: inline-block !important;
-    margin: 0;
-
-    & .mjx-chtml {
-      padding: 0;
-    }
-  }
-`;
 
 export const MathML = props => {
   const { node } = props;
 
   const { innerHTML, xlmns } = node.data.toJS();
   return (
-    <StyledMathWrapper contentEditable={false} {...props.attributes}>
+    <span contentEditable={false} {...props.attributes}>
       {node.nodes.map(text => (
         <span key={text.key} data-key={text.key}>
           <math
@@ -40,7 +27,7 @@ export const MathML = props => {
           />
         </span>
       ))}
-    </StyledMathWrapper>
+    </span>
   );
 };
 

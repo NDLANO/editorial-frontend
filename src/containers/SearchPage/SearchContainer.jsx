41dbd4bb186bddfee926d581da591bc96538a3dd
Christian Peter <christian150790@gmail.com>
2018-11-05 10:45:43 +0100

    Adds fix for language field in title #1391

--- src/containers/SearchPage/SearchContainer.jsx
+++ src/containers/SearchPage/SearchContainer.jsx
@@ -124,7 +124,6 @@ class SearchContainer extends Component {
             results={results.results}
             searching={searching}
             type={type}
-            locale={locale}
           />
           <Pager
             page={searchObject.page ? parseInt(searchObject.page, 10) : 1}

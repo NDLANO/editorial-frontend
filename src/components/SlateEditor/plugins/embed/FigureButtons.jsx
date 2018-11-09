5e89650521ac4f84db536ceb6f3b2943c8a9c42a
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-11-09 10:30:31 +0100

    functional component

--- src/components/SlateEditor/plugins/embed/FigureButtons.jsx
+++ src/components/SlateEditor/plugins/embed/FigureButtons.jsx
@@ -15,66 +15,53 @@ import { Cross, Pencil, Plus } from '@ndla/icons/action';
 import { editorClasses } from './SlateFigure';
 import { EmbedShape } from '../../../../shapes';
 
-export class FigureButtons extends React.Component {
-  constructor() {
-    super();
-    this.onDeleteConfirm = this.onDeleteConfirm.bind(this);
-  }
+const FigureButtons = ({ embed, locale, figureType, t, onRemoveClick }) => {
+  const url = {
+    audio: {
+      path: '/media/audio-upload',
+      newTitle: t('form.addNewAudio'),
+      editTitle: t('form.editAudio'),
+    },
+    image: {
+      path: '/media/image-upload',
+      newTitle: t('form.addNewImage'),
+      editTitle: t('form.editImage'),
+    },
+  };
 
-  onDeleteConfirm(e) {
-    const { onRemoveClick } = this.props;
-    onRemoveClick(e);
-  }
-
-  render() {
-    const { embed, locale, figureType, t } = this.props;
-    const url = {
-      audio: {
-        path: '/media/audio-upload',
-        newTitle: t('form.addNewAudio'),
-        editTitle: t('form.editAudio'),
-      },
-      image: {
-        path: '/media/image-upload',
-        newTitle: t('form.addNewImage'),
-        editTitle: t('form.editImage'),
-      },
-    };
-
-    const isNotCentered =
-      embed.align === 'left' ||
-      embed.align === 'right' ||
-      embed.size === 'small' ||
-      embed.size === 'xsmall';
-    return (
-      <div
-        {...editorClasses(
-          'figure-buttons',
-          isNotCentered ? 'right-adjusted' : '',
-        )}>
-        <Button
-          {...editorClasses('button', 'red')}
-          onClick={this.onDeleteConfirm}
-          stripped>
-          <Cross />
-        </Button>
-        <Link
-          to={`${url[figureType].path}/new`}
-          target="_blank"
-          title={url[figureType].newTitle}>
-          <Plus />
-        </Link>
-        <Link
-          to={`${url[figureType].path}/${embed.resource_id}/edit/${locale}`}
-          target="_blank"
-          {...editorClasses('button', 'green')}
-          title={url[figureType].editTitle}>
-          <Pencil />
-        </Link>
-      </div>
-    );
-  }
-}
+  const isNotCentered =
+    embed.align === 'left' ||
+    embed.align === 'right' ||
+    embed.size === 'small' ||
+    embed.size === 'xsmall';
+  return (
+    <div
+      {...editorClasses(
+        'figure-buttons',
+        isNotCentered ? 'right-adjusted' : '',
+      )}>
+      <Button
+        {...editorClasses('button', 'red')}
+        onClick={onRemoveClick}
+        stripped>
+        <Cross />
+      </Button>
+      <Link
+        to={`${url[figureType].path}/new`}
+        target="_blank"
+        title={url[figureType].newTitle}>
+        <Plus />
+      </Link>
+      <Link
+        to={`${url[figureType].path}/${embed.resource_id}/edit/${locale}`}
+        target="_blank"
+        {...editorClasses('button', 'green')}
+        title={url[figureType].editTitle}>
+        <Pencil />
+      </Link>
+    </div>
+  );
+};
 
 FigureButtons.propTypes = {
   onRemoveClick: PropTypes.func.isRequired,

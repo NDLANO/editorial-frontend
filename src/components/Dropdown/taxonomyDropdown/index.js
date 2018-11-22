e9d3c62356da851c1acbf92eeda4b6533fd3dd4b
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-11-22 22:34:40 +0100

    Remove unused code

--- src/components/Dropdown/taxonomyDropdown/index.js
+++ src/components/Dropdown/taxonomyDropdown/index.js
@@ -1,15 +1,5 @@
 import DropdownTag from './DropdownTag';
 import DropdownTagPropertyItem from './DropdownTagPropertyItem';
 import InputItems from './InputItems';
-import TaxonomyDropdown from './TaxonomyDropdown';
-import TaxonomyDropdownInput from './TaxonomyDropdownInput';
 
-export {
-  DropdownTag,
-  DropdownTagPropertyItem,
-  InputItems,
-  TaxonomyDropdown,
-  TaxonomyDropdownInput,
-};
-
-export default TaxonomyDropdown;
+export { DropdownTag, DropdownTagPropertyItem, InputItems };

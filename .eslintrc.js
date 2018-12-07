81649d5185c52f6352515dadede95b2b024c6958
Øyvind Marthinsen <oma@knowit.no>
2018-12-07 08:04:07 +0100

    chore: bump eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
   extends: 'ndla',
-  env: {
-    jest: true,
-  },
   rules: {
     'react/prop-types': [2, { ignore: ['children', 'className', 't'] }],
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['to'],
-      },
-    ],
   },
-  globals: {},
 };

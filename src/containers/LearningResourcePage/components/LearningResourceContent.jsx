d3a1b9a976701b5d2e6c6cd1e18307ab8ce510f0
Gunnar Velle <gunnar.velle@gmail.com>
2019-01-09 10:32:44 +0100

    Removed unused import

--- src/containers/LearningResourcePage/components/LearningResourceContent.jsx
+++ src/containers/LearningResourcePage/components/LearningResourceContent.jsx
@@ -10,7 +10,6 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { injectT } from '@ndla/i18n';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { getLocale } from '../../../modules/locale/locale';
 import { TextField } from '../../../components/Fields';
 import RichBlockTextField from '../../../components/RichBlockTextField';

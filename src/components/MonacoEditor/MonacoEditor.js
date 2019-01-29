320ff240ac5b6634bfc1f0f92180870c6686a5a5
Øyvind Marthinsen <oma@knowit.no>
2019-01-29 12:17:00 +0100

    feat: enable more monaco features

--- src/components/MonacoEditor/MonacoEditor.js
+++ src/components/MonacoEditor/MonacoEditor.js
@@ -6,14 +6,17 @@
  *
  */
 
-import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js';
-import 'monaco-editor/esm/vs/editor/browser/widget/codeEditorWidget.js';
-import 'monaco-editor/esm/vs/editor/contrib/bracketMatching/bracketMatching.js';
-import 'monaco-editor/esm/vs/editor/contrib/find/findController.js';
-import 'monaco-editor/esm/vs/editor/contrib/links/links.js';
-import 'monaco-editor/esm/vs/editor/contrib/suggest/suggestController.js';
-import 'monaco-editor/esm/vs/editor/contrib/wordHighlighter/wordHighlighter.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickOpen/quickCommand.js';
+import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands';
+import 'monaco-editor/esm/vs/editor/browser/widget/codeEditorWidget';
+import 'monaco-editor/esm/vs/editor/contrib/bracketMatching/bracketMatching';
+import 'monaco-editor/esm/vs/editor/contrib/find/findController';
+import 'monaco-editor/esm/vs/editor/contrib/links/links';
+import 'monaco-editor/esm/vs/editor/contrib/suggest/suggestController';
+import 'monaco-editor/esm/vs/editor/contrib/wordHighlighter/wordHighlighter';
+import 'monaco-editor/esm/vs/editor/standalone/browser/quickOpen/quickCommand';
+import 'monaco-editor/esm/vs/editor/contrib/multicursor/multicursor';
+import 'monaco-editor/esm/vs/editor/contrib/linesOperations/linesOperations';
+import 'monaco-editor/esm/vs/editor/contrib/fontZoom/fontZoom';
 
 // import 'monaco-editor/esm/vs/editor/browser/widget/diffEditorWidget.js';
 // import 'monaco-editor/esm/vs/editor/browser/widget/diffNavigator.js';
@@ -31,8 +34,6 @@ import 'monaco-editor/esm/vs/editor/standalone/browser/quickOpen/quickCommand.js
 // import 'monaco-editor/esm/vs/editor/contrib/gotoError/gotoError.js';
 // import 'monaco-editor/esm/vs/editor/contrib/hover/hover.js';
 // import 'monaco-editor/esm/vs/editor/contrib/inPlaceReplace/inPlaceReplace.js';
-// import 'monaco-editor/esm/vs/editor/contrib/linesOperations/linesOperations.js';
-// import 'monaco-editor/esm/vs/editor/contrib/multicursor/multicursor.js';
 // import 'monaco-editor/esm/vs/editor/contrib/parameterHints/parameterHints.js';
 // import 'monaco-editor/esm/vs/editor/contrib/referenceSearch/referenceSearch.js';
 // import 'monaco-editor/esm/vs/editor/contrib/rename/rename.js';

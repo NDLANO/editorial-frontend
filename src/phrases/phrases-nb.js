b584dc9ff17f795ffd20f4719e67ec0a6a38c7cf
Øyvind Marthinsen <oma@knowit.no>
2019-02-06 14:20:23 +0100

    fix: update help message

--- src/phrases/phrases-nb.js
+++ src/phrases/phrases-nb.js
@@ -240,7 +240,7 @@ const phrases = {
       paragraph1:
         'Det er mulig å formatter  HTML koden ved å trykke på F1 og velge Format. Merk at dette kan føre til at whitespace (indentering, linjeskift og mellomrom) kan endre seg ved lagring. Bruk gjerne forhåndsvisning funksjonaliteten aktivt. ',
       paragraph2:
-        'For å minimerer eventuelle problemer med formatering og sikre at at HTML blir konsistent så blir koden kjørt gjennom de samme transformeringen som WYSIWYG editoren. Det betyr at linjeskift f.eks. blir endret til <br> ved lagring og samt at den vil prøve å rette opp kode som ikke følger følgende regler:',
+        'For å minimerer eventuelle problemer med formatering og sikre at at HTML blir konsistent så blir koden kjørt gjennom de samme transformeringen som WYSIWYG editoren. Det betyr at linjeskift f.eks. blir endret til <br> ved lagring og samt at den vil prøve å rette opp ugyldig kode.',
     },
   },
   imageSearch: {

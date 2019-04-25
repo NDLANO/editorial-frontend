c1b1e3015b2244c772417aa1bda356a1c1b1a9e1
Alireza Balouch <gswape@gmail.com>
2019-04-25 12:24:07 +0200

    Update src/containers/SearchPage/SearchMediaPage.jsx
    Co-Authored-By: gunnarvelle <gunnar.velle@gmail.com>

--- src/containers/SearchPage/SearchMediaPage.jsx
+++ src/containers/SearchPage/SearchMediaPage.jsx
@@ -22,7 +22,7 @@ import SearchContainer from './SearchContainer';
 
 const SearchMediaPage = ({ t, ...props }) => (
   <Fragment>
-    <HelmetWithTracker title={`${t('htmlTitles.searchMediaPage')}`} />
+    <HelmetWithTracker title={t('htmlTitles.searchMediaPage')} />
     <SearchContainer type="media" {...props} />
   </Fragment>
 );

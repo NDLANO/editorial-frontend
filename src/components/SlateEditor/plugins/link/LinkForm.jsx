91e01d416c6a959b55aa6b86b9c1a86317dea91d
Christian Peter <christian150790@gmail.com>
2019-05-06 15:41:48 +0200

    Removes reformed from link form

--- src/components/SlateEditor/plugins/link/LinkForm.jsx
+++ src/components/SlateEditor/plugins/link/LinkForm.jsx
@@ -9,6 +9,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
+import { Formik, Form } from 'formik';
 import Button from '@ndla/button';
 import { injectT } from '@ndla/i18n';
 import { css } from '@emotion/core';
@@ -16,12 +17,18 @@ import { Field, FieldErrorMessages, getField } from '../../../Fields';
 import validateSchema from '../../../validateSchema';
 import { SchemaShape, LinkShape } from '../../../../shapes';
 import reformed from '../../../reformed';
-
+import validateFormik from '../../../formikValidationSchema';
+import FormikField from '../../../FormikField';
 const marginLeftStyle = css`
   margin-left: 0.2rem;
 `;
 
-export const getInitialModel = (link = {}) => ({
+const linkValidationRules = {
+  text: { required: true },
+  href: { required: true, url: true },
+};
+
+export const getInitialValues = (link = {}) => ({
   text: link.text || '',
   href: link.href || '',
   checkbox: link.checkbox || false,
@@ -33,88 +40,86 @@ class LinkForm extends Component {
     this.handleSave = this.handleSave.bind(this);
   }
 
-  handleSave(evt) {
-    evt.preventDefault();
-    const { onSave, model, validationErrors, setSubmitted } = this.props;
-    if (!validationErrors.isValid) {
-      setSubmitted(true);
-      return;
-    }
-
-    onSave(model);
+  async handleSave(values, actions) {
+    const { onSave } = this.props;
+    actions.setSubmitting(true);
+    await onSave(values);
+    actions.setSubmitting(false);
   }
 
   render() {
-    const {
-      t,
-      validationErrors,
-      submitted,
-      bindInput,
-      isEdit,
-      onRemove,
-      onClose,
-    } = this.props;
+    const { t, isEdit, link, onRemove, onClose } = this.props;
     return (
-      <form data-cy="link_form">
-        <Field>
-          <label htmlFor="text">{t('form.content.link.text')}</label>
-          <input type="text" {...bindInput('text')} />
-          <FieldErrorMessages
+      <Formik
+        initialValues={getInitialValues(link)}
+        validateOnBlur={false}
+        onSubmit={this.handleSave}
+        enableReinitialize
+        validate={values => validateFormik(values, linkValidationRules, t)}>
+        <Form data-cy="link_form">
+          <FormikField
+            name="text"
+            type="text"
             label={t('form.content.link.text')}
-            field={getField('text', validationErrors)}
-            submitted={submitted}
           />
-        </Field>
-        <Field>
-          <label htmlFor="href">{t('form.content.link.href')}</label>
-          <input type="text" {...bindInput('href')} />
-          <FieldErrorMessages
-            label={t('form.content.link.href')}
-            field={getField('href', validationErrors)}
-            submitted={submitted}
-          />
-        </Field>
-        <Field>
-          <label htmlFor="checkbox">{t('form.content.link.newTab')}</label>
+          <FormikField name="href" label={t('form.content.link.href')} />
 
-          <input
-            css={{ appearance: 'checkbox !important' }}
-            type="checkbox"
-            {...bindInput('checkbox', 'checkbox')}
-          />
-          <FieldErrorMessages
-            label={t('form.content.link.newTab')}
-            field={getField('checkbox', validationErrors)}
-            submitted={submitted}
-          />
-        </Field>
-        <Field right>
-          {isEdit ? (
-            <Button onClick={onRemove}>{t('form.content.link.remove')}</Button>
-          ) : (
-            ''
-          )}
-          <Button css={marginLeftStyle} outline onClick={onClose}>
-            {t('form.abort')}
-          </Button>
-          <Button css={marginLeftStyle} type="button" onClick={this.handleSave}>
-            {isEdit
-              ? t('form.content.link.update')
-              : t('form.content.link.insert')}
-          </Button>
-        </Field>
-      </form>
+          <Field>
+            <label htmlFor="href">{t('form.content.link.href')}</label>
+            <input type="text" {...bindInput('href')} />
+            <FieldErrorMessages
+              label={t('form.content.link.href')}
+              field={getField('href', validationErrors)}
+              submitted={submitted}
+            />
+          </Field>
+          <Field>
+            <label htmlFor="checkbox">{t('form.content.link.newTab')}</label>
+
+            <input
+              css={{ appearance: 'checkbox !important' }}
+              type="checkbox"
+              {...bindInput('checkbox', 'checkbox')}
+            />
+            <FieldErrorMessages
+              label={t('form.content.link.newTab')}
+              field={getField('checkbox', validationErrors)}
+              submitted={submitted}
+            />
+          </Field>
+          <Field right>
+            {isEdit ? (
+              <Button onClick={onRemove}>
+                {t('form.content.link.remove')}
+              </Button>
+            ) : (
+              ''
+            )}
+            <Button css={marginLeftStyle} outline onClick={onClose}>
+              {t('form.abort')}
+            </Button>
+            <Button
+              css={marginLeftStyle}
+              type="submit"
+              onClick={this.handleSave}>
+              {isEdit
+                ? t('form.content.link.update')
+                : t('form.content.link.insert')}
+            </Button>
+          </Field>
+        </Form>
+      </Formik>
     );
   }
 }
 
 LinkForm.propTypes = {
-  model: LinkShape.isRequired,
-  validationErrors: SchemaShape,
-  setSubmitted: PropTypes.func.isRequired,
-  submitted: PropTypes.bool.isRequired,
+  link: LinkShape.isRequired,
+  //validationErrors: SchemaShape,
+  //setSubmitted: PropTypes.func.isRequired,
+  //submitted: PropTypes.bool.isRequired,
   isEdit: PropTypes.bool.isRequired,
-  bindInput: PropTypes.func.isRequired,
+  //bindInput: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
   onRemove: PropTypes.func.isRequired,

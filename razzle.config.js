2a2307c7e2debeb06191d2675d25c0b7d668a1a2
Christian Peter <christian150790@gmail.com>
2019-07-01 17:56:49 +0200

    Fixes build problems

--- razzle.config.js
+++ razzle.config.js
@@ -19,7 +19,7 @@ module.exports = {
       options: {
         useBabel: true,
         tsLoader: {
-          transpileOnly: true,
+          transpileOnly: false,
           experimentalWatchApi: true,
         },
         forkTsChecker: {

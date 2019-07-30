5d0ee4ea69515b086f471b566d20f2d3be41598f
Tale Prestmo <tale.prestmo@knowit.no>
2019-07-30 13:21:53 +0200

    Fix links for learningpaths

---
+++ src/containers/StructurePage/resourceComponents/ResourceItemLink.jsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2019-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import config from '../../../config';
+import { classes } from './ResourceGroup';
+import { toEditArticle } from '../../../util/routeHelpers';
+
+const ResourceItemLink = ({ contentType, contentUri, locale, name }) => {
+  const linkTo = contentUri && contentUri.split(':').pop();
+
+  if (linkTo) {
+    if (contentType === 'learning-path') {
+      const linkProps = {
+        href: `${config.learningpathFrontendDomain}/${locale}/learningpaths/${linkTo}/first-step`,
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      };
+      return <a {...linkProps}>{name}</a>;
+    }
+    return (
+      <Link to={toEditArticle(linkTo, contentType, locale)}>
+        <h1 {...classes('title')}>{name}</h1>
+      </Link>
+    );
+  }
+  return <h1 {...classes('title')}>{name}</h1>;
+};
+
+ResourceItemLink.propTypes = {
+  contentType: PropTypes.string.isRequired,
+  contentUri: PropTypes.string,
+  locale: PropTypes.string.isRequired,
+  name: PropTypes.string,
+};
+
+export default ResourceItemLink;

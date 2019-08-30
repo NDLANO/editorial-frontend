a22846854038eef75e92102c98ee59451955d936
Gunnar Velle <gunnar.velle@gmail.com>
2019-08-30 13:37:22 +0200

    Missing phrase

--- src/phrases/phrases-nn.js
+++ src/phrases/phrases-nn.js
@@ -425,6 +425,7 @@ const phrases = {
     headingThree: 'Overskrift 3',
     footnote: 'Fotnote',
     mathml: 'Matte',
+    concept: 'Begrep',
   },
   editorBlockpicker: {
     heading: 'Legg til',

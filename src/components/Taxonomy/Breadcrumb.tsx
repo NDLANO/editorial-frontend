e5e5e47cbfbf0a467eae06d2f81f9d0a1397057d
Gunnar Velle <gunnar.velle@gmail.com>
2020-01-23 10:17:35 +0100

    Show urls to structure

--- src/components/Taxonomy/Breadcrumb.tsx
+++ src/components/Taxonomy/Breadcrumb.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
 import { fonts } from '@ndla/core';
+import SafeLink from '@ndla/safelink';
 import { ChevronRight } from '@ndla/icons/common';
 interface Props {
   breadcrumb: Array<{
+    id: string;
     name: string;
   }>;
   type?: string;
@@ -17,16 +19,22 @@ const StyledBreadCrumb = styled('div')`
 `;
 
 export default function Breadcrumb({ breadcrumb, type }: Props) {
+  let url = '/structure';
   return (
     <StyledBreadCrumb>
-      {breadcrumb.map((path, index) => (
-        <Fragment key={`${path.name}_${index}`}>
-          <span css={{ 'white-space': 'nowrap' }}>{path.name}</span>
-          {(type !== 'topic-article' || index + 1 !== breadcrumb.length) && (
-            <ChevronRight />
-          )}
-        </Fragment>
-      ))}
+      {breadcrumb.map((path, index) => {
+        url = `${url}/${path.id}`;
+        return (
+          <Fragment key={`${path.name}_${index}`}>
+            <span css={{ 'white-space': 'nowrap' }}>
+              <SafeLink to={url}>{path.name}</SafeLink>
+            </span>
+            {(type !== 'topic-article' || index + 1 !== breadcrumb.length) && (
+              <ChevronRight />
+            )}
+          </Fragment>
+        );
+      })}
     </StyledBreadCrumb>
   );
 }

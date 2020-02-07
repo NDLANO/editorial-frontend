a3a51eabad759fe4b60da80216946fee5fc297d9
Gunnar Velle <gunnar.velle@gmail.com>
2020-02-07 13:16:22 +0100

    Search for subjects

--- src/containers/SearchPage/components/form/SearchConceptForm.jsx
+++ src/containers/SearchPage/components/form/SearchConceptForm.jsx
@@ -11,22 +11,41 @@ import PropTypes from 'prop-types';
 import { injectT } from '@ndla/i18n';
 import Button from '@ndla/button';
 import { css } from '@emotion/core';
+import { fetchSubjects } from '../../../../modules/taxonomy';
 import { getResourceLanguages } from '../../../../util/resourceHelpers';
 import ObjectSelector from '../../../../components/ObjectSelector';
+import SearchTagGroup from './SearchTagGroup';
 import { searchFormClasses } from './SearchForm';
 import { LocationShape } from '../../../../shapes';
+
+const emptySearchState = {
+  query: '',
+  subjectIds: '',
+  language: '',
+};
 
 class SearchConceptForm extends Component {
   constructor(props) {
     super(props);
     const { searchObject } = props;
     this.state = {
+      subjects: [],
       search: {
+        subjectIds: searchObject.subjectIds || '',
         query: searchObject.query || '',
         language: searchObject.language || '',
         types: 'concept',
       },
     };
+    this.getExternalData = this.getExternalData.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.removeTagItem = this.removeTagItem.bind(this);
+    this.emptySearch = this.emptySearch.bind(this);
+    this.onFieldChange = this.onFieldChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.getExternalData();
   }
 
   onFieldChange = evt => {
@@ -37,6 +56,14 @@ class SearchConceptForm extends Component {
     );
   };
 
+  async getExternalData() {
+    const { locale } = this.props;
+    const subjects = await fetchSubjects(locale);
+    this.setState({
+      subjects,
+    });
+  }
+
   handleSearch = evt => {
     if (evt) {
       evt.preventDefault();
@@ -46,24 +73,48 @@ class SearchConceptForm extends Component {
     search({ ...this.state.search, page: 1 });
   };
 
-  emptySearch = evt => {
+  removeTagItem(tag) {
     this.setState(
-      { search: { query: '', language: '', types: 'concept' } },
-      () => this.handleSearch(evt),
+      prevState => ({ search: { ...prevState.search, [tag.type]: '' } }),
+      this.handleSearch,
     );
-  };
+  }
+
+  emptySearch() {
+    this.setState({ search: emptySearchState }, this.handleSearch);
+  }
+
+  sortByProperty(property) {
+    return function(a, b) {
+      return a[property].localeCompare(b[property]);
+    };
+  }
 
   render() {
     const { t } = this.props;
-    const { search } = this.state;
+    const { search, subjects } = this.state;
 
     return (
       <form onSubmit={this.handleSearch} {...searchFormClasses()}>
-        <div {...searchFormClasses('field', '50-width')}>
+        <div {...searchFormClasses('field', '25-width')}>
           <input
             name="query"
             placeholder={t('searchForm.types.conceptQuery')}
             value={search.query}
+            onChange={this.onFieldChange}
+          />
+        </div>
+        <div
+          key={`searchfield_subjects`}
+          {...searchFormClasses('field', `25-width`)}>
+          <ObjectSelector
+            name={'subjectIds'}
+            options={subjects.sort(this.sortByProperty('name'))}
+            idKey="id"
+            value={this.state.search['subjectIds']}
+            labelKey="name"
+            emptyField
+            placeholder={t(`searchForm.types.subjects`)}
             onChange={this.onFieldChange}
           />
         </div>
@@ -98,6 +149,17 @@ class SearchConceptForm extends Component {
             {t('searchForm.btn')}
           </Button>
         </div>
+        <div {...searchFormClasses('tagline')}>
+          <SearchTagGroup
+            onRemoveItem={this.removeTagItem}
+            languages={getResourceLanguages}
+            subjects={subjects}
+            searchObject={{
+              subjects: this.state.search.subjectIds,
+              ...this.state.search,
+            }}
+          />
+        </div>
       </form>
     );
   }
@@ -108,15 +170,18 @@ SearchConceptForm.propTypes = {
   location: LocationShape,
   searchObject: PropTypes.shape({
     query: PropTypes.string,
+    subjectIds: PropTypes.string,
     language: PropTypes.string,
   }),
+  locale: PropTypes.string.isRequired,
 };
 
-SearchConceptForm.defaultProps = {
+/*SearchConceptForm.defaultProps = {
   searchObject: {
     query: '',
+    subjects: '',
     language: '',
   },
-};
+};*/
 
 export default injectT(SearchConceptForm);

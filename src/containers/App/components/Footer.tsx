31ec2ac77c7a1ccce3e7aab234e8e9132e7353f3
Jonas Natten <jonas.natten@knowit.no>
2020-02-28 09:59:43 +0100

    Fix lint warning

--- src/containers/App/components/Footer.tsx
+++ src/containers/App/components/Footer.tsx
@@ -11,7 +11,6 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { spacing } from '@ndla/core';
 import { Footer, LanguageSelector, FooterText, EditorName } from '@ndla/ui';
-import { Facebook, Twitter, EmailOutline } from '@ndla/icons/common';
 import { LocationState } from 'history';
 import { withRouter } from 'react-router-dom';
 import styled from '@emotion/styled';

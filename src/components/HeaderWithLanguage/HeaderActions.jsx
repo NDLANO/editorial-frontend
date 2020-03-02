91fe959d0b6e08143479760aa576ddc179669b95
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2020-03-02 15:39:32 +0100

    Hides translate button for concept

--- src/components/HeaderWithLanguage/HeaderActions.jsx
+++ src/components/HeaderWithLanguage/HeaderActions.jsx
@@ -83,7 +83,7 @@ const HeaderActions = ({
           emptyLanguages={emptyLanguages}
           editUrl={editUrl}
         />
-        {language === 'nb' && !supportedLanguages.includes('nn') && (
+        {articleType && language === 'nb' && !supportedLanguages.includes('nn') && (
           <Fragment>
             <StyledSplitter />
             <TranslateNbToNn

0de4c1b6d4c2794c610680b14a3d5c2453654e6f
Gunnar Velle <gunnar.velle@gmail.com>
2020-10-01 19:09:01 +0200

    Remove old config

--- src/config.js
+++ src/config.js
@@ -44,8 +44,6 @@ const gaTrackingId = () => {
   switch (ndlaEnvironment) {
     case 'prod':
       return 'UA-9036010-36';
-    case 'ff':
-      return 'UA-9036010-36';
     case 'staging':
       return 'UA-9036010-36';
     default:
@@ -70,8 +68,6 @@ const h5pApiUrl = () => {
       return 'https://h5p-test.ndla.no';
     case 'test':
       return 'https://h5p-test.ndla.no';
-    case 'ff':
-      return 'https://h5p-ff.ndla.no';
     case 'staging':
       return 'https://h5p-staging.ndla.no';
     default:
@@ -82,8 +78,6 @@ const h5pApiUrl = () => {
 export const getAuth0Hostname = () => {
   switch (process.env.NDLA_ENVIRONMENT) {
     case 'prod':
-      return 'ndla.eu.auth0.com';
-    case 'ff':
       return 'ndla.eu.auth0.com';
     case 'staging':
       return 'ndla-staging.eu.auth0.com';

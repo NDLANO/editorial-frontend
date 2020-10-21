156d2f01bea9e9a68c739b7023aec76d6efe3180
Gunnar Velle <gunnar.velle@gmail.com>
2020-10-21 21:32:01 +0200

    Had to have last to ensure p inside details.

--- src/components/SlateEditor/plugins/details/index.js
+++ src/components/SlateEditor/plugins/details/index.js
@@ -86,6 +86,7 @@ export default function createDetails() {
           },
         ],
         first: { type: 'summary' },
+        last: { type: 'paragraph' },
         next: [
           {
             type: 'paragraph',

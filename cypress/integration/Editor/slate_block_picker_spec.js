ba7a739c902d25c0150e87e0c6bdc7ad4903a626
Gunnar Velle <gunnar.velle@gmail.com>
2020-12-21 09:11:25 +0100

    Yet another test.

--- cypress/integration/Editor/slate_block_picker_spec.js
+++ cypress/integration/Editor/slate_block_picker_spec.js
@@ -20,7 +20,7 @@ describe("can enter both element types SlateBlockPicker and SlateVisualElementPi
     cy.server({ force404: true });
     cy.get('[data-slate-object=block] > p').clear({force:true});
     cy.get('[data-slate-object=block] > p').first().click();
-    cy.get('[data-cy=slate-block-picker]').click();
+    cy.get('[data-cy=slate-block-picker]').click({force:true});
     cy.get('[cy="slate-block-picker-menu"]').should('be.visible');
   });
 

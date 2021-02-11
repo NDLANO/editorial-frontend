e05620d99e6ec364550f265c8dddc05f9fedb3f9
N4G1 <bartezzzzz@hotmail.com>
2021-02-11 17:17:27 +0100

    using userAccess scope from constants.js and applying formatting

--- src/containers/FormikForm/FormikMetadata.jsx
+++ src/containers/FormikForm/FormikMetadata.jsx
@@ -16,70 +16,68 @@ import { FormikMetaImageSearch } from '.';
 import AsyncSearchTags from '../../components/Dropdown/asyncDropdown/AsyncSearchTags';
 import FormikAvailability from './components/FormikAvailability';
 import { UserAccessContext } from '../App/App';
+import { DRAFT_ADMIN_SCOPE } from '../../constants';
 
 const FormikMetadata = ({ t, article, fetchSearchTags, handleSubmit, handleBlur }) => {
   const userAccess = useContext(UserAccessContext);
 
   return (
-  <Fragment>
-    <FormikField
-      name="tags"
-      label={t('form.tags.label')}
-      showError
-      description={t('form.tags.description')}>
-      {({ field, form }) => (
-        <AsyncSearchTags
-          initialTags={article.tags}
-          language={article.language}
-          field={field}
-          form={form}
-          fetchTags={fetchSearchTags}
-        />
-      )}
-    </FormikField>
-    {userAccess.includes('drafts:admin') && (
+    <Fragment>
       <FormikField
-        name="availability"
-        label={t('form.availability.label')}
-        description={t('form.availability.description')}>
-        {({ field }) => <FormikAvailability availability={article.availability} field={field} />}
+        name="tags"
+        label={t('form.tags.label')}
+        showError
+        description={t('form.tags.description')}>
+        {({ field, form }) => (
+          <AsyncSearchTags
+            initialTags={article.tags}
+            language={article.language}
+            field={field}
+            form={form}
+            fetchTags={fetchSearchTags}
+          />
+        )}
       </FormikField>
-    )}
-    <FormikField
-      name="metaDescription"
-      maxLength={155}
-      showMaxLength
-      label={t('form.metaDescription.label')}
-      description={t('form.metaDescription.description')}>
-      {({ field }) => (
-        <PlainTextEditor
-          id={field.name}
-          placeholder={t('form.metaDescription.label')}
-          handleSubmit={handleSubmit}
-          onBlur={(event, editor, next) => {
-            next();
-            // this is a hack since formik onBlur-handler interferes with slates
-            // related to: https://github.com/ianstormtaylor/slate/issues/2434
-            // formik handleBlur needs to be called for validation to work (and touched to be set)
-            setTimeout(() => handleBlur({ target: { name: 'metaDescription' } }), 0);
-          }}
-          {...field}
-        />
+      {userAccess.includes(DRAFT_ADMIN_SCOPE) && (
+        <FormikField name="availability" label={t('form.availability.label')}>
+          {({ field }) => <FormikAvailability availability={article.availability} field={field} />}
+        </FormikField>
       )}
-    </FormikField>
-    <FormikField name="metaImageId">
-      {({ field, form }) => (
-        <FormikMetaImageSearch
-          metaImageId={field.value}
-          setFieldTouched={form.setFieldTouched}
-          showRemoveButton={false}
-          {...field}
-        />
-      )}
-    </FormikField>
-  </Fragment>
-);
-}
+      <FormikField
+        name="metaDescription"
+        maxLength={155}
+        showMaxLength
+        label={t('form.metaDescription.label')}
+        description={t('form.metaDescription.description')}>
+        {({ field }) => (
+          <PlainTextEditor
+            id={field.name}
+            placeholder={t('form.metaDescription.label')}
+            handleSubmit={handleSubmit}
+            onBlur={(event, editor, next) => {
+              next();
+              // this is a hack since formik onBlur-handler interferes with slates
+              // related to: https://github.com/ianstormtaylor/slate/issues/2434
+              // formik handleBlur needs to be called for validation to work (and touched to be set)
+              setTimeout(() => handleBlur({ target: { name: 'metaDescription' } }), 0);
+            }}
+            {...field}
+          />
+        )}
+      </FormikField>
+      <FormikField name="metaImageId">
+        {({ field, form }) => (
+          <FormikMetaImageSearch
+            metaImageId={field.value}
+            setFieldTouched={form.setFieldTouched}
+            showRemoveButton={false}
+            {...field}
+          />
+        )}
+      </FormikField>
+    </Fragment>
+  );
+};
 
 FormikMetadata.propTypes = {
   article: PropTypes.shape({

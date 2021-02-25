c4fd2ee1f437e3a040e9c309b79f85bd993469e5
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2021-02-25 13:24:25 +0100

    Add paragraph under inserted element and focus

--- src/components/SlateEditor/plugins/blockPicker/SlateBlockPicker.jsx
+++ src/components/SlateEditor/plugins/blockPicker/SlateBlockPicker.jsx
@@ -56,7 +56,10 @@ class SlateBlockPicker extends Component {
 
   onInsertBlock(block) {
     const { editor } = this.props;
-    editor.insertBlock(block);
+    editor
+      .insertBlock(block)
+      .insertBlock('paragraph')
+      .focus();
   }
 
   onElementAdd(block) {

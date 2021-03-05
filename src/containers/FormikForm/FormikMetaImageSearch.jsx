dc17627c784f42b63c20ec044aacabb42307cfdc
aod <anders.dahlin@knowit.no>
2021-03-05 13:28:48 +0100

    Forslag til løsning

--- src/containers/FormikForm/FormikMetaImageSearch.jsx
+++ src/containers/FormikForm/FormikMetaImageSearch.jsx
@@ -6,12 +6,12 @@
  *
  */
 
-import React, { Fragment, useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { injectT } from '@ndla/i18n';
 import { FieldHeader } from '@ndla/forms';
-import Modal, { ModalHeader, ModalBody, ModalCloseButton } from '@ndla/modal';
 import Button from '@ndla/button';
+import { ModalCloseButton } from '@ndla/modal';
 import { LocaleContext } from '../App/App';
 import { createFormData } from '../../util/formDataHelper';
 import {
@@ -81,44 +81,32 @@ const FormikMetaImageSearch = ({
       <FieldHeader title={t('form.metaImage.title')}>
         <HowToHelper pageId="MetaImage" tooltip={t('form.metaImage.helpLabel')} />
       </FieldHeader>
-      <Modal
-        controllable
-        isOpen={showImageSelect}
-        onClose={onImageSelectClose}
-        size="large"
-        backgroundColor="white"
-        minHeight="90vh">
-        {() => (
-          <Fragment>
-            <ModalHeader>
-              <ModalCloseButton title={t('dialog.close')} onClick={onImageSelectClose} />
-            </ModalHeader>
-            <ModalBody>
-              <ImageSearchAndUploader
-                onImageSelect={onImageSet}
-                locale={locale}
-                closeModal={onImageSelectClose}
-                fetchImage={fetchImageWithLocale}
-                searchImages={searchImages}
-                onError={onError}
-                updateImage={(image, file) => {
-                  if (image.id) {
-                    updateImage(image).then(updatedImage => onImageSet(updatedImage));
-                  } else {
-                    createFormData(file, image).then(formData =>
-                      postImage(formData).then(createdImage => {
-                        onImageSet({ ...createdImage, language: locale });
-                      }),
-                    );
-                  }
-                }}
-                image={image}
-              />
-            </ModalBody>
-          </Fragment>
-        )}
-      </Modal>
-      {image ? (
+      {showImageSelect && (
+        <>
+          <ImageSearchAndUploader
+            onImageSelect={onImageSet}
+            locale={locale}
+            closeModal={onImageSelectClose}
+            fetchImage={fetchImageWithLocale}
+            searchImages={searchImages}
+            onError={onError}
+            updateImage={(image, file) => {
+              if (image.id) {
+                updateImage(image).then(updatedImage => onImageSet(updatedImage));
+              } else {
+                createFormData(file, image).then(formData =>
+                  postImage(formData).then(createdImage => {
+                    onImageSet({ ...createdImage, language: locale });
+                  }),
+                );
+              }
+            }}
+            image={image}
+          />
+        </>
+      )}
+
+      {!showImageSelect && image ? (
         <FormikMetaImage
           image={image}
           onImageSelectOpen={onImageSelectOpen}

8441cbd8c149b1f47b07ec01cad892aa3be93de3
aod <anders.dahlin@knowit.no>
2021-03-25 08:16:57 +0100

    Fikser feil ved ny læringssti

--- src/components/HeaderWithLanguage/HeaderWithLanguage.jsx
+++ src/components/HeaderWithLanguage/HeaderWithLanguage.jsx
@@ -98,7 +98,15 @@ HeaderWithLanguage.propTypes = {
   isSubmitting: PropTypes.bool,
   noStatus: PropTypes.bool,
   setTranslateOnContinue: PropTypes.func,
-  type: PropTypes.oneOf(['image', 'audio', 'iframe', 'topic-article', 'standard', 'concept']),
+  type: PropTypes.oneOf([
+    'image',
+    'audio',
+    'iframe',
+    'topic-article',
+    'standard',
+    'concept',
+    'learning-resource',
+  ]),
   translateArticle: PropTypes.func,
   values: PropTypes.shape({
     articleType: PropTypes.string,

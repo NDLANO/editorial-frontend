4c7afe82f895d96efedc25ea86f4340075be4e18
Jonas Natten <jonas.natten@knowit.no>
2021-04-12 07:14:13 +0200

    Flex to fill out iframe height

--- src/containers/H5PPage/H5PPage.jsx
+++ src/containers/H5PPage/H5PPage.jsx
@@ -14,6 +14,7 @@ const H5PPage = props => {
             styles={{
               '.o-content': {
                 height: '100vh',
+                display: 'flex',
               },
             }}
           />

ec69e3803f9cb47ccb3b271b6c1d2d87f0df55f8
Henrik Liodden <henrik.liodden@knowit.no>
2021-05-17 20:10:36 +0200

    Normalizing aside plugin

--- src/components/SlateEditor/plugins/aside/index.tsx
+++ src/components/SlateEditor/plugins/aside/index.tsx
@@ -6,13 +6,20 @@
  *
  */
 
+import {
+  afterTextBlockElement,
+  firstTextBlockElement,
+  lastTextBlockElement,
+  textBlockElements,
+} from '../../utils/normalizationHelpers';
 import React from 'react';
-import { Descendant, Editor, Element } from 'slate';
+import { Descendant, Editor, Element, Node, Path, Text, Transforms } from 'slate';
 import { jsx } from 'slate-hyperscript';
 import { RenderElementProps } from 'slate-react';
 import { SlateSerializer } from '../../interfaces';
 import SlateAside from './SlateAside';
 import { getAsideType } from './utils';
+import { TYPE_PARAGRAPH } from '../paragraph';
 
 export const TYPE_ASIDE = 'aside';
 
@@ -37,7 +44,7 @@ export const asideSerializer: SlateSerializer = {
 };
 
 export const asidePlugin = (editor: Editor) => {
-  const { renderElement: nextRenderElement } = editor;
+  const { renderElement: nextRenderElement, normalizeNode: nextNormalizeNode } = editor;
 
   editor.renderElement = ({ attributes, children, element }: RenderElementProps) => {
     if (element.type === TYPE_ASIDE) {
@@ -50,6 +57,62 @@ export const asidePlugin = (editor: Editor) => {
       return nextRenderElement({ attributes, children, element });
     }
     return undefined;
+  };
+
+  editor.normalizeNode = entry => {
+    const [node, path] = entry;
+
+    if (Element.isElement(node) && node.type === TYPE_ASIDE) {
+      if (node.children.length === 0) {
+        Transforms.insertNodes(editor, jsx('element', { type: TYPE_PARAGRAPH }, [{ text: '' }]), {
+          at: [...path, 0],
+        });
+        return;
+      }
+
+      const firstChild = node.children[0];
+      if (Element.isElement(firstChild)) {
+        if (!firstTextBlockElement.includes(firstChild.type)) {
+          Transforms.insertNodes(editor, jsx('element', { type: TYPE_PARAGRAPH }, [{ text: '' }]), {
+            at: [...path, 0],
+          });
+          return;
+        }
+      }
+
+      const lastChild = node.children[node.children.length - 1];
+      if (Element.isElement(lastChild)) {
+        if (!lastTextBlockElement.includes(lastChild.type)) {
+          Transforms.insertNodes(editor, jsx('element', { type: TYPE_PARAGRAPH }, [{ text: '' }]), {
+            at: [...path, node.children.length],
+          });
+          return;
+        }
+      }
+
+      for (const [child, childPath] of Node.children(editor, path)) {
+        if (Text.isText(child)) {
+          Transforms.wrapNodes(editor, jsx('element', { type: TYPE_PARAGRAPH }), { at: childPath });
+          return;
+        }
+        if (Element.isElement(child) && !textBlockElements.includes(child.type)) {
+          Transforms.unwrapNodes(editor, {
+            at: childPath,
+          });
+          return;
+        }
+      }
+
+      const next = Editor.next(editor, { at: path });
+      if (next) {
+        const [nextNode, nextPath] = next;
+        if (!Element.isElement(nextNode) || !afterTextBlockElement.includes(nextNode.type)) {
+          Transforms.wrapNodes(editor, jsx('element', { type: TYPE_PARAGRAPH }), { at: nextPath });
+          return;
+        }
+      }
+    }
+    nextNormalizeNode(entry);
   };
 
   return editor;

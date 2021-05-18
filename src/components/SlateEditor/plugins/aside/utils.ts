f9fd473065d535ddd91b7aaea68af1039541f900
Henrik Liodden <henrik.liodden@knowit.no>
2021-05-18 03:22:21 +0200

    Add copyright info

--- src/components/SlateEditor/plugins/aside/utils.ts
+++ src/components/SlateEditor/plugins/aside/utils.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { jsx } from 'slate-hyperscript';
 import { AsideElement, TYPE_ASIDE } from '.';
 
--- src/components/SlateEditor/plugins/concept/utils.ts
+++ src/components/SlateEditor/plugins/concept/utils.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2021-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { Editor, Transforms, Element, Range } from 'slate';
 import { jsx } from 'slate-hyperscript';
 import hasNodeOfType from '../../utils/hasNodeOfType';

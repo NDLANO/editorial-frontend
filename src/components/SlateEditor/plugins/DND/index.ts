b2bcd3ed56890e4e7cd939b75e99a2315e232dd7
Henrik Liodden <henrik.liodden@knowit.no>
2021-05-31 10:30:24 +0200

    Slate: Drag and drop plugin

--- src/components/SlateEditor/plugins/DND/index.ts
+++ src/components/SlateEditor/plugins/DND/index.ts
@@ -7,10 +7,9 @@
  */
 
 import { DragEventHandler } from 'react';
-import { Editor, Transforms, Element } from 'slate';
+import { Editor, Element, Path } from 'slate';
 import { ReactEditor } from 'slate-react';
 import onDrop from './onDrop';
-import { getTopNode, shouldCopyTableOrList } from './utils';
 
 const onDragOver = (editor: Editor): DragEventHandler<HTMLDivElement> => event => {
   event.preventDefault();
@@ -25,30 +24,7 @@ const onDragStart = (editor: Editor): DragEventHandler<HTMLDivElement> => event 
     event.dataTransfer.setData('application/slate-node-path', JSON.stringify(path));
   }
 
-  const voidMatch =
-    Editor.isVoid(editor, node) ||
-    Editor['void'](editor, {
-      at: path,
-      voids: true,
-    });
-
-  if (voidMatch) {
-    const range = Editor.range(editor, path);
-    Transforms.select(editor, range);
   }
-
-  ReactEditor.setFragmentData(editor, event.dataTransfer);
-
-  // OLD
-  // event.dataTransfer.effectAllowed = 'copy';
-  // const dragSource = editor.findNode(event.target);
-  // const { type } = getTopNode(dragSource, editor);
-  // if (Text.isText(dragSource) && !shouldCopyTableOrList(type, editor)) {
-  //   // just copy the text natively
-  //   return next();
-  // }
-
-  // event.dataTransfer.setData('text/nodeKey', dragSource.key);
 };
 
 export { onDragOver, onDragStart, onDrop };
--- src/components/SlateEditor/plugins/DND/onDrop.ts
+++ src/components/SlateEditor/plugins/DND/onDrop.ts
@@ -1,11 +1,9 @@
 import { DragEventHandler } from 'react';
 import { Editor, Element, Path, Transforms } from 'slate';
+import { HistoryEditor } from 'slate-history';
 import { ReactEditor } from 'slate-react';
-import { getTopNode } from './utils';
 
 const onDrop = (editor: Editor): DragEventHandler<HTMLDivElement> => event => {
-  event.preventDefault();
-  // NEW
   const data = event.dataTransfer;
 
   const originPath = JSON.parse(data.getData('application/slate-node-path') || '[]');
@@ -18,9 +16,11 @@ const onDrop = (editor: Editor): DragEventHandler<HTMLDivElement> => event => {
       return;
     }
 
+    // Handle case where root section is detected as the target.
     if (Element.isElement(targetNode) && targetNode.type === 'section') {
       // We need to manually calculate the correct posistion to move the node
-      const children = event.target.children as HTMLElement['children'];
+      // @ts-ignore EventTarget interface does not include children, but the property does exist on target in this case.
+      const children = event.target.children as HTMLCollection;
       let prevElementTop = 0;
       let insertAtNode;
       for (let i = 0; i < children.length; i++) {
@@ -48,99 +48,30 @@ const onDrop = (editor: Editor): DragEventHandler<HTMLDivElement> => event => {
       if (Path.equals(insertBeforeNode, originPath)) {
         return;
       }
-      Transforms.moveNodes(editor, { at: originPath, to: insertBeforeNode });
+      // Prevent default Editable function from executing.
+      event.preventDefault();
+      event.stopPropagation();
+      HistoryEditor.withoutMerging(editor, () => {
+        Transforms.moveNodes(editor, {
+          at: originPath,
+          to: insertBeforeNode,
+        });
+      });
     } else {
       if (Path.equals(originPath, targetPath) || Path.isDescendant(targetPath, originPath)) {
         return;
       }
-      Transforms.moveNodes(editor, { at: originPath, to: targetPath });
+      // Prevent default Editable function from executing.
+      event.preventDefault();
+      event.stopPropagation();
+      HistoryEditor.withoutMerging(editor, () => {
+        Transforms.moveNodes(editor, { at: originPath, to: targetPath });
+      });
     }
-  } else {
-    const draggedRange = editor.selection;
-    Transforms.select(editor, ReactEditor.findEventRange(editor, event));
-    if (draggedRange) {
-      Transforms.delete(editor, { at: draggedRange });
+    if (!ReactEditor.isFocused(editor)) {
+      ReactEditor.focus(editor);
     }
-    ReactEditor.insertData(editor, data);
   }
-  if (!ReactEditor.isFocused(editor)) {
-    ReactEditor.focus(editor);
-  }
-  return;
-
-  const targetNode = ReactEditor.toSlateNode(editor, event.target as Node);
-  const targetPath = ReactEditor.findPath(editor, targetNode);
-  console.log('targetNode', targetNode);
-
-  const range = Editor.range(editor, targetPath);
-
-  const targetRange = ReactEditor.findEventRange(editor, event);
-  //Transforms.select(editor, targetRange);
-  // ReactEditor.insertData(editor, data);
-  // Transforms.removeNodes(editor, { at: range });
-
-  // const fragment = Node.fragment(editor, range);
-  // Transforms.removeNodes(editor, {at: range});
-
-  // OLD
-  // const target = editor.findNode(event.target);
-  // const topLevelTarget = getTopNode(target, editor);
-  // const nodeKey = event.dataTransfer.getData('text/nodeKey');
-  // const sourceNode = editor.value.document.getNode(nodeKey);
-  // if (!sourceNode) {
-  //   return next();
-  // }
-  // const topLevelSource = getTopNode(sourceNode, editor);
-  // if (!topLevelSource || !topLevelTarget || topLevelTarget.key === topLevelSource.key) {
-  //   return;
-  // }
-
-  // const { key: topLevelSourceKey } = topLevelSource;
-
-  // if (topLevelTarget.type === 'paragraph' && topLevelTarget.text === '') {
-  //   editor.withoutNormalizing(() => {
-  //     editor
-  //       .moveNodeByKey(topLevelSourceKey, topLevelTarget.key, 0)
-  //    });
-  // } else if (topLevelTarget.type === 'section') {
-  //   // We need to manually calculate the correct posistion to move the node
-  //   const children = event.target.children;
-  //   let prevElementTop = 0;
-  //   let insertAtNode;
-  //   for (let i = 0; i < children.length; i++) {
-  //     const elementTop = children[i].getBoundingClientRect().top;
-  //     if (elementTop > event.clientY) {
-  //       // check if it should be inserted before or after this element
-  //       const elementOvershoot = event.clientY - prevElementTop;
-  //       const goUp = elementOvershoot > (elementTop - prevElementTop) / 2;
-  //       if (goUp && children.length > i + 1) {
-  //         insertAtNode = children[i + 1];
-  //       }
-  //       insertAtNode = children[i];
-  //       break;
-  //     }
-  //     prevElementTop = children[i].top;
-  //   }
-  //   const insertBeforeNode = editor.findNode(insertAtNode, editor);
-  //   if (insertBeforeNode.key === sourceNode.key) {
-  //     return;
-  //   }
-  //   editor.withoutNormalizing(() => {
-  //     editor
-  //       .moveNodeByKey(topLevelSourceKey, insertBeforeNode.key, 0)
-  //       .unwrapNodeByKey(topLevelSourceKey);
-  //   });
-  // } else {
-  //   // it is dropped over another element, move it to either before or after it
-  //   const { height, top } = event.target.getBoundingClientRect();
-  //   const goUp = top + height / 2 > event.clientY;
-
-  //   editor.withoutNormalizing(() => {
-  //     editor
-  //       .moveNodeByKey(topLevelSourceKey, topLevelTarget.key, goUp ? 0 : topLevelTarget.nodes.size)
-  //       .unwrapNodeByKey(topLevelSourceKey);
-  //   });
-  // }
 };
 
 export default onDrop;

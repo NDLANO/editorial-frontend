1e8643a75231a39778807a6a84ca609e55376d0f
Henrik Liodden <henrik.liodden@knowit.no>
2021-06-02 13:45:46 +0200

    Slate: Drag and drop only allows dropping blocks in root section

--- src/components/SlateEditor/plugins/paragraph/index.tsx
+++ src/components/SlateEditor/plugins/paragraph/index.tsx
@@ -89,7 +89,9 @@ export const paragraphSerializer: SlateSerializer = {
       between element with no spacing (i.e two images). We need to remove these element
       on seriaization.
      */
-    if (Node.string(node) === '' && children.length === 1 && Text.isText(children[0])) return null;
+
+    if (Node.string(node) === '' && node.children.length === 1 && Text.isText(node.children[0]))
+      return null;
 
     const attributes = node.data?.align ? ` data-align="${node.data.align}"` : '';
     return `<p${attributes}>${children}</p>`;

f5db665db49805c7fac168cde66587925b2b7bb7
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2021-07-16 08:49:24 +0200

    Converted FileUploader to TypeScript

---
+++ src/components/FileUploader/FileUploader.tsx
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2017-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+import { injectT, tType } from '@ndla/i18n';
+import { spacing } from '@ndla/core';
+import { UploadDropZone } from '@ndla/forms';
+import { uploadFile } from '../../modules/draft/draftApi';
+import { createFormData } from '../../util/formDataHelper';
+import handleError from '../../util/handleError';
+
+const wrapperCSS = css`
+  padding: 0 ${spacing.large};
+`;
+
+interface Props {
+  onFileSave: (files: { path: string; type: string; title: string }[]) => void;
+}
+
+const FileUploader = ({ onFileSave, t }: Props & tType) => {
+  const [saving, setSaving] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  const saveFile = async (file: string | Blob | undefined) => {
+    const formData = await createFormData(file);
+    return uploadFile(formData);
+  };
+
+  const onSave = async (files: (Blob & { name: string })[]) => {
+    try {
+      setSaving(true);
+      const newFiles = await Promise.all(files.map(file => saveFile(file)));
+      onFileSave(
+        newFiles.map((file, i) => ({
+          path: file.path,
+          type: file.extension.substring(1),
+          title: files[i].name.replace(/\..*/, ''),
+        })),
+      );
+      setSaving(false);
+    } catch (err) {
+      if (err && err.json && err.json.messages) {
+        setErrorMessage(
+          err.json.messages.map((message: { message: string }) => message.message).join(', '),
+        );
+      }
+      handleError(err);
+    }
+  };
+
+  if (errorMessage) {
+    return <p>{errorMessage}</p>;
+  }
+
+  return (
+    <div css={wrapperCSS}>
+      <UploadDropZone
+        name="file"
+        allowedFiles={[
+          '.csv',
+          '.doc',
+          '.docx',
+          '.dwg',
+          '.dxf',
+          '.ggb',
+          '.ipynb',
+          '.json',
+          '.odp',
+          '.ods',
+          '.odt',
+          '.pdf',
+          '.pln',
+          '.pro',
+          '.ppt',
+          '.pptx',
+          '.pub',
+          '.rtf',
+          '.skp',
+          '.stl',
+          '.tex',
+          '.tsv',
+          '.txt',
+          '.xls',
+          '.xlsx',
+          '.xml',
+        ]}
+        onAddedFiles={onSave}
+        multiple
+        loading={saving}
+        ariaLabel={t('form.file.dragdrop.ariaLabel')}>
+        <strong>{t('form.file.dragdrop.main')}</strong> {t('form.file.dragdrop.sub')}
+      </UploadDropZone>
+    </div>
+  );
+};
+
+export default injectT(FileUploader);

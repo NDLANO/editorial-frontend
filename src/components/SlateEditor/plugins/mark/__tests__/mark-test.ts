96df77ec7401113b6ccd7bc6f756956eb7b6f401
Henrik Liodden <henrik.liodden@knowit.no>
2021-07-19 11:07:29 +0200

    Fix test

--- src/components/SlateEditor/plugins/mark/__tests__/mark-test.ts
+++ src/components/SlateEditor/plugins/mark/__tests__/mark-test.ts
@@ -82,7 +82,7 @@ const editor: Descendant[][] = [
 ];
 
 const html =
-  '<<section><p><strong>bold</strong></p><p><em>italic</em></p><p><sup>sup</sup></p><p><sub>sub</sub></p><p><code>code</code></p><p><code><sub><sup><em><strong>all</strong></em></sup></sub></code></p></section>';
+  '<section><p><strong>bold</strong></p><p><em>italic</em></p><p><sup>sup</sup></p><p><sub>sub</sub></p><p><code>code</code></p><p><code><sub><sup><em><strong>all</strong></em></sup></sub></code></p></section>';
 
 describe('heading serializing tests', () => {
   test('serializing', () => {

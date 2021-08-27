19ca3e16e586a471a42a18326a6e74f568abd744
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2021-08-27 15:48:24 +0200

    Fixed ndla film editing by reverting to previous version of resetForm.

--- src/containers/FormikForm/ndlaFilmFormHooks.ts
+++ src/containers/FormikForm/ndlaFilmFormHooks.ts
@@ -37,7 +37,8 @@ export function useNdlaFilmFormHooks(
 
       Object.keys(formik.values).map(fieldName => formik.setFieldTouched(fieldName, true, true));
 
-      formik.resetForm({ values: initialValues });
+      //@ts-ignore
+      formik.resetForm(initialValues);
       setSavedToServer(true);
     } catch (err) {
       if (err?.status === 409) {

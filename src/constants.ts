f336b27ec2dff090e3f9cea16b886ce0da4cf445
Gunnar Velle <gunnar.velle@gmail.com>
2021-10-12 09:44:46 +0200

    Adding missing urls-

--- src/constants.ts
+++ src/constants.ts
@@ -58,10 +58,11 @@ export const EXTERNAL_WHITELIST_PROVIDERS = [
   { name: 'YouTube', url: ['youtube.com', 'youtu.be'], height: '486px' },
   { name: 'NRK', url: ['static.nrk.no'], height: '398px' },
   { name: 'Vimeo', url: ['vimeo.com', 'vimeopro.com'], height: '486px' },
+  { name: 'MSDN', url: ['channel9.msdn.com'], height: '486px' },
   { name: 'Norgesfilm', url: ['ndla.filmiundervisning.no'] },
   { name: 'TED', url: ['ted.com', 'embed.ted.com'] },
   { name: 'TV2 Skole', url: ['www.tv2skole.no'], height: '431px' },
-  { name: 'Khan Academy', url: ['nb.khanacademy.org'], height: '486px' },
+  { name: 'Khan Academy', url: ['nb.khanacademy.org', 'www.khanacademy.org'], height: '486px' },
   { name: 'Prezi', url: ['prezi.com'] },
   { name: 'SlideShare', url: ['www.slideshare.net'], height: '500px' },
   { name: 'Scribd', url: ['scribd.com'] },
@@ -93,6 +94,7 @@ export const EXTERNAL_WHITELIST_PROVIDERS = [
     height: '398px',
   },
   { name: 'Ebok', url: ['ebok.no'] },
+  { name: 'VG', url: ['www.vg.no'] },
   { name: 'Trinket', url: ['trinket.io'], height: '700px' },
 ];
 

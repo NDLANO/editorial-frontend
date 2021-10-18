6216cf2a3dc6091b2e3947cc033252f0701cb6b7
Henrik Liodden <henrik.liodden@knowit.no>
2021-10-18 15:16:33 +0200

    Fix typo in cypress test

--- cypress/integration/Editor/toolbar_spec.js
+++ cypress/integration/Editor/toolbar_spec.js
@@ -17,7 +17,7 @@ describe('Selecting text and using the toolbar', () => {
   });
 
   it('change the text styling', () => {
-    cy.get('[data-cy=slate-editor] [data-slate-editor=true][contenteditable=true')
+    cy.get('[data-cy=slate-editor] [data-slate-editor=true][contenteditable=true]')
       .first()
       .focus()
       .wait(500)

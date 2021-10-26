2856f4a6518286b91d2bd1e8fda9b7d9169b7544
Henrik Liodden <henrik.liodden@knowit.no>
2021-10-26 18:32:41 +0200

    Update cypress test

--- cypress/integration/Editor/slate_block_picker_spec.js
+++ cypress/integration/Editor/slate_block_picker_spec.js
@@ -99,9 +99,9 @@ describe('can enter both element types SlateBlockPicker and SlateVisualElementPi
     );
     cy.apiroute('GET', '**/audio-api/v1/audio/*?language=nb', 'editor/audios/audio-1');
     cy.get('[data-cy=create-podcast]').click();
-    cy.apiwait(['@editor/audios/podcastList', '@editor/audios/audio-1']);
     cy.get('[data-cy="modal-header"]').should('be.visible');
     cy.get('[data-cy="modal-body"]').should('be.visible');
+    cy.apiwait(['@editor/audios/podcastList', '@editor/audios/audio-1']);
     cy.get('[data-cy="close-modal-button"]').click();
   });
 

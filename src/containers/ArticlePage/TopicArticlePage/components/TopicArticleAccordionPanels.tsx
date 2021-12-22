b84dcafda99fdef6241514e86af69c2d4ec16b1f
Gunnar Velle <gunnar.velle@gmail.com>
2021-12-22 12:17:36 +0100

    format

--- src/containers/ArticlePage/TopicArticlePage/components/TopicArticleAccordionPanels.tsx
+++ src/containers/ArticlePage/TopicArticlePage/components/TopicArticleAccordionPanels.tsx
@@ -73,7 +73,7 @@ const TopicArticleAccordionPanels = ({
         hasError={
           !!(errors.creators || errors.rightsholders || errors.processors || errors.license)
         }>
-        <CopyrightFieldGroup values={values} enableLicenseNA={true}/>
+        <CopyrightFieldGroup values={values} enableLicenseNA={true} />
       </AccordionSection>
       <AccordionSection
         id={'topic-article-metadata'}

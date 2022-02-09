a2a23a011845292735cbc058eed58eb4d9b86e43
Henrik Liodden <henrik.liodden@knowit.no>
2022-02-09 16:18:22 +0100

    Add license to files

--- src/components/SlateEditor/plugins/span/utils.ts
+++ src/components/SlateEditor/plugins/span/utils.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { Editor, Element, Range, Transforms } from 'slate';
 import { jsx as slatejsx } from 'slate-hyperscript';
 import { SpanElement, TYPE_SPAN } from '.';

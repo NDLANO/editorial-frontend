56b7b1d6fe62af31455eb086d3cb857d762038a0
N4G1 <bartezzzzz@hotmail.com>
2022-03-30 15:29:42 +0200

    worte tests for split-url functions and for isNDLAEdPathUrl function

--- src/components/SlateEditor/plugins/link/__tests__/EditLink-test.ts
+++ src/components/SlateEditor/plugins/link/__tests__/EditLink-test.ts
@@ -6,7 +6,14 @@
  *
  */
 
-import { isNDLAArticleUrl, isNDLALearningPathUrl, isNDLATaxonomyUrl, isPlainId, isNDLAEdPathUrl } from '../EditLink';
+import {
+  isNDLAArticleUrl,
+  isNDLALearningPathUrl,
+  isNDLATaxonomyUrl,
+  isNDLAEdPathUrl,
+  isPlainId,
+} from '../EditLink';
+import { splitArticleUrl, splitLearningPathUrl, splitEdPathUrl, splitPlainUrl } from '../utils';
 
 test('urls are parsed correctly', async () => {
   const articleUrls = [
@@ -41,6 +48,21 @@ test('urls are parsed correctly', async () => {
 
   const plainIds = ['5512', '123', '12', '1', '12314'];
 
+  const edPathUrls = [
+    'https://ed.ndla.no/nb/subject-matter/learning-resource/123/edit/nn',
+    'https://ed.test.ndla.no/nb/subject-matter/learning-resource/123/edit/nn',
+    'https://ed.staging.ndla.no/nb/subject-matter/learning-resource/123/edit/nn',
+    'https://ed.ndla.no/subject-matter/learning-resource/4/edit/nb',
+    'https://ed.test.ndla.no/subject-matter/learning-resource/4/edit/nb',
+    'https://ed.staging.ndla.no/subject-matter/learning-resource/4/edit/nb',
+    'https://ed.ndla.no/subject-matter/topic-article/56/edit/nn',
+    'https://ed.test.ndla.no/subject-matter/topic-article/56/edit/nn',
+    'https://ed.staging.ndla.no/subject-matter/topic-article/56/edit/nn',
+    'https://ed.ndla.no/subject-matter/topic-article/56',
+    'https://ed.test.ndla.no/subject-matter/topic-article/56',
+    'https://ed.staging.ndla.no/subject-matter/topic-article/56',
+  ];
+
   const otherUrls = [
     'https://www.vg.no/',
     'https://www.youtube.com/subject:3/topic:1:2342/resource:1:64323',
@@ -52,6 +74,7 @@ test('urls are parsed correctly', async () => {
     expect(isNDLAArticleUrl(url)).toBe(true);
     expect(isNDLALearningPathUrl(url)).toBe(false);
     expect(isNDLATaxonomyUrl(url)).toBe(false);
+    expect(isNDLAEdPathUrl(url)).toBe(false);
     expect(isPlainId(url)).toBe(false);
   });
 
@@ -59,6 +82,7 @@ test('urls are parsed correctly', async () => {
     expect(isNDLAArticleUrl(url)).toBe(false);
     expect(isNDLALearningPathUrl(url)).toBe(false);
     expect(isNDLATaxonomyUrl(url)).toBe(true);
+    expect(isNDLAEdPathUrl(url)).toBe(false);
     expect(isPlainId(url)).toBe(false);
   });
 
@@ -66,6 +90,15 @@ test('urls are parsed correctly', async () => {
     expect(isNDLAArticleUrl(url)).toBe(false);
     expect(isNDLALearningPathUrl(url)).toBe(true);
     expect(isNDLATaxonomyUrl(url)).toBe(false);
+    expect(isNDLAEdPathUrl(url)).toBe(false);
+    expect(isPlainId(url)).toBe(false);
+  });
+
+  edPathUrls.forEach(url => {
+    expect(isNDLAArticleUrl(url)).toBe(false);
+    expect(isNDLALearningPathUrl(url)).toBe(false);
+    expect(isNDLATaxonomyUrl(url)).toBe(false);
+    expect(isNDLAEdPathUrl(url)).toBe(true);
     expect(isPlainId(url)).toBe(false);
   });
 
@@ -73,6 +106,7 @@ test('urls are parsed correctly', async () => {
     expect(isNDLAArticleUrl(url)).toBe(false);
     expect(isNDLALearningPathUrl(url)).toBe(false);
     expect(isNDLATaxonomyUrl(url)).toBe(false);
+    expect(isNDLAEdPathUrl(url)).toBe(false);
     expect(isPlainId(url)).toBe(true);
   });
 
@@ -80,6 +114,25 @@ test('urls are parsed correctly', async () => {
     expect(isNDLAArticleUrl(url)).toBe(false);
     expect(isNDLALearningPathUrl(url)).toBe(false);
     expect(isNDLATaxonomyUrl(url)).toBe(false);
+    expect(isNDLAEdPathUrl(url)).toBe(false);
     expect(isPlainId(url)).toBe(false);
   });
+});
+
+test('test split methods', async () => {
+  expect(splitArticleUrl('https://www.test.ndla.no/article/64323').resourceId).toBe('64323');
+  expect(splitArticleUrl('https://www.test.ndla.no/sma/article/64323').resourceId).toBe('64323');
+  expect(splitLearningPathUrl('https://www.test.ndla.no/learningpaths/64323').resourceId).toBe(
+    '64323',
+  );
+  expect(splitLearningPathUrl('https://www.test.ndla.no/nb/learningpaths/64323').resourceId).toBe(
+    '64323',
+  );
+  expect(
+    splitEdPathUrl('https://ed.ndla.no/nb/subject-matter/learning-resource/123/edit/nn').resourceId,
+  ).toBe('123');
+  expect(splitEdPathUrl('https://ed.test.ndla.no/subject-matter/topic-article/56').resourceId).toBe(
+    '56',
+  );
+  expect(splitPlainUrl('5512').resourceId).toBe('5512');
 });

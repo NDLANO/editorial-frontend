9493b672f78b1b6860f2d2717c25049b3b6c474f
Henrik Liodden <henrik.liodden@knowit.no>
2022-03-31 12:04:06 +0200

    Add licenses to new files

--- src/components/SlateEditor/plugins/list/types.ts
+++ src/components/SlateEditor/plugins/list/types.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 export const LIST_TYPES = ['numbered-list', 'bulleted-list', 'letter-list'];
 export const TYPE_LIST = 'list';
 export const TYPE_LIST_ITEM = 'list-item';

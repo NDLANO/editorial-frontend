e9dc9f39841284276b78c9f724830d3e31973700
Henrik Liodden <henrik.liodden@knowit.no>
2022-04-21 14:14:50 +0200

    MonacoEditor for editing colgroups in table

--- src/components/SlateEditor/plugins/table/EditColgroupsModal.tsx
+++ src/components/SlateEditor/plugins/table/EditColgroupsModal.tsx
@@ -1,9 +1,32 @@
+/**
+ * Copyright (c) 2022-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { lazy, Suspense, useState } from 'react';
+import { Spinner } from '@ndla/editor';
+import Button from '@ndla/button';
 import Modal, { ModalBody, ModalCloseButton, ModalHeader } from '@ndla/modal';
 import { useSlateStatic } from 'slate-react';
 import { Transforms } from 'slate';
 import { useTranslation } from 'react-i18next';
 import { TableElement } from './interfaces';
 import { Portal } from '../../../Portal';
+
+window.MonacoEnvironment = {
+  getWorkerUrl: function(moduleId: string, label: string) {
+    if (label === 'html') {
+      return '/static/js/html.worker.js';
+    }
+    return '/static/js/editor.worker.js';
+  },
+  globalAPI: true,
+};
+
+const MonacoEditor = lazy(() => import('../../../MonacoEditor'));
 
 interface Props {
   element: TableElement;
@@ -14,10 +37,22 @@ const EditColgroupsModal = ({ element }: Props) => {
   const { t } = useTranslation();
   const { showEditColgroups } = element;
 
+  const [colgroups, setColgroups] = useState(element.colgroups || '');
+
   const onClose = () => {
     Transforms.setNodes(
       editor,
       { showEditColgroups: false },
+      {
+        match: node => node === element,
+      },
+    );
+  };
+
+  const onSave = (content: string) => {
+    Transforms.setNodes(
+      editor,
+      { showEditColgroups: false, colgroups: content },
       {
         match: node => node === element,
       },
@@ -30,6 +65,7 @@ const EditColgroupsModal = ({ element }: Props) => {
         controllable
         isOpen={showEditColgroups}
         onClose={onClose}
+        size={'large'}
         backgroundColor="white"
         minHeight="90vh">
         {() => (
@@ -37,7 +73,18 @@ const EditColgroupsModal = ({ element }: Props) => {
             <ModalHeader>
               <ModalCloseButton title={t('dialog.close')} onClick={onClose} />
             </ModalHeader>
-            <ModalBody>{element.colgroups}</ModalBody>
+            <ModalBody>
+              <h2>{t('form.content.table.edit-colgroups')}</h2>
+              <Suspense fallback={<Spinner />}>
+                <MonacoEditor
+                  onChange={setColgroups}
+                  onSave={onSave}
+                  value={colgroups}
+                  height={'50vh'}
+                />
+              </Suspense>
+              <Button onClick={() => onSave(colgroups)}>{t('form.save')}</Button>
+            </ModalBody>
           </div>
         )}
       </Modal>

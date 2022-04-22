314fd791dd1637017fd13cf3b1d88bf5424dfb90
Henrik Liodden <henrik.liodden@knowit.no>
2022-04-22 11:15:13 +0200

    Upgrade slate. Add custom up/down arrow handling in mathml

--- src/components/SlateEditor/plugins/mathml/utils.ts
+++ src/components/SlateEditor/plugins/mathml/utils.ts
@@ -1,6 +1,13 @@
-import { Editor, Element, Range, Transforms } from 'slate';
+import { Editor, Element, Node, Range, Transforms } from 'slate';
 import { jsx as slatejsx } from 'slate-hyperscript';
+import { MathmlElement } from '.';
+import getCurrentBlock from '../../utils/getCurrentBlock';
 import hasNodeOfType from '../../utils/hasNodeOfType';
+import { TYPE_MATHML } from './types';
+
+export const isMathml = (node: Node | undefined): node is MathmlElement => {
+  return Element.isElement(node) && node.type === TYPE_MATHML;
+};
 
 export const insertMathml = (editor: Editor) => {
   const { selection } = editor;
@@ -32,4 +39,42 @@ export const insertMathml = (editor: Editor) => {
       },
     );
   }
+};
+
+export const onArrowUp = (
+  e: KeyboardEvent,
+  editor: Editor,
+  onKeyDown: ((event: KeyboardEvent) => void) | undefined,
+) => {
+  if (!editor.selection || !Range.isCollapsed(editor.selection)) {
+    return onKeyDown?.(e);
+  }
+
+  const mathml = getCurrentBlock(editor, TYPE_MATHML)?.[0];
+
+  if (isMathml(mathml)) {
+    e.preventDefault();
+    Transforms.move(editor, { unit: 'line', distance: 1, reverse: true });
+    return;
+  }
+  onKeyDown?.(e);
+};
+
+export const onArrowDown = (
+  e: KeyboardEvent,
+  editor: Editor,
+  onKeyDown: ((event: KeyboardEvent) => void) | undefined,
+) => {
+  if (!editor.selection || !Range.isCollapsed(editor.selection)) {
+    return onKeyDown?.(e);
+  }
+
+  const mathml = getCurrentBlock(editor, TYPE_MATHML)?.[0];
+
+  if (isMathml(mathml)) {
+    e.preventDefault();
+    Transforms.move(editor, { unit: 'line' });
+    return;
+  }
+  onKeyDown?.(e);
 };

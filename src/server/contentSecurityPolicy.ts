7d2977c6966e691b8f0363efc95434689e12856b
Gunnar Velle <gunnar.velle@gmail.com>
2022-06-01 09:40:38 +0200

    Hotfix: Forgot to add gapminder to csp.

--- src/server/contentSecurityPolicy.ts
+++ src/server/contentSecurityPolicy.ts
@@ -176,6 +176,7 @@ const frameSrc = (() => {
     'flo.uri.sh',
     'ourworldindata.org',
     '*.sketchup.com',
+    'www.gapminder.org',
   ];
   if (process.env.NODE_ENV === 'development') {
     return [

72ae33889e4164a9ab6797c612c6518017bebb9b
Milla Weium <milla.weium@knowit.no>
2022-07-25 12:27:18 +0200

    :recycle: css -> styled for AlertModal

--- src/components/AlertModal/AlertModal.tsx
+++ src/components/AlertModal/AlertModal.tsx
@@ -9,13 +9,12 @@
 import { MouseEvent, ReactElement } from 'react';
 import { Warning } from '@ndla/icons/editor';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
 import { spacing } from '@ndla/core';
 import AlertModalFooter from './AlertModalFooter';
 import Lightbox from '../Lightbox';
 import { MessageSeverity } from '../../interfaces';
 
-const StyledAlertModal = styled('div')<{ severity: string }>`
+const StyledAlertModal = styled.div<{ severity: string }>`
   height: 100%;
   padding-top: ${spacing.normal};
   font-size: 24px;
@@ -24,17 +23,17 @@ const StyledAlertModal = styled('div')<{ severity: string }>`
   justify-content: space-between;
 `;
 
-const StyledAlertModalBody = styled('div')`
+const StyledAlertModalBody = styled.div`
   display: flex;
 `;
 
-const StyledAlertModalBodyText = styled('span')`
+const StyledAlertModalBodyText = styled.span`
   width: 100%;
   display: inline-block;
   text-align: left;
 `;
 
-const alertModalBodyIconStyle = css`
+const StyledAlertModalBodyIcon = styled(Warning)`
   width: 27px;
   height: 27px;
   margin-right: ${spacing.small};
@@ -58,7 +57,7 @@ const AlertModal = ({ text, onCancel, actions, component, show, severity = 'dang
     <Lightbox display={show} onClose={onCancel} appearance="modal" severity={severity}>
       <StyledAlertModal severity={severity}>
         <StyledAlertModalBody>
-          <Warning css={alertModalBodyIconStyle} />
+          <StyledAlertModalBodyIcon />
           <StyledAlertModalBodyText>{text}</StyledAlertModalBodyText>
         </StyledAlertModalBody>
         <AlertModalFooter actions={actions} component={component} />

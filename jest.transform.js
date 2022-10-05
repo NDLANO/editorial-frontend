9bad1aaa7ed39c81505249f90f45c35d16d1373a
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2022-10-05 13:06:32 +0200

    Update jest transform and update snaps

--- jest.transform.js
+++ jest.transform.js
@@ -1,5 +1,5 @@
 // workaround to make component selectors work with babel and jest.
 module.exports = require('babel-jest').createTransformer({
-  presets: ['razzle/babel'],
-  plugins: ['babel-plugin-emotion'],
+  presets: [['razzle/babel', { runtime: 'automatic', importSource: '@emotion/react' }]],
+  plugins: ['@emotion'],
 });

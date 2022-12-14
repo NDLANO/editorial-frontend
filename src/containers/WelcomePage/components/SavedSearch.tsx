54c0eee096feb42f443326b8788a1141e599a8e8
mapoken <larssoerlie96@gmail.com>
2022-12-14 09:01:12 +0100

    Fix number saved search bug

--- src/containers/WelcomePage/components/SavedSearch.tsx
+++ src/containers/WelcomePage/components/SavedSearch.tsx
@@ -38,7 +38,7 @@ const SavedSearch = ({ deleteSearch, search, index, userData }: Props) => {
   const [searchObject, isFavorite] = useMemo(() => {
     const obj = transformQuery(queryString.parse(searchParams));
     const isFav = obj.subjects === FAVOURITES_SUBJECT_ID;
-    return [isFav ? { ...obj, subjects: userData.favoriteSubjects } : obj, isFav];
+    return [isFav ? { ...obj, subjects: userData.favoriteSubjects?.join(',') } : obj, isFav];
   }, [searchParams, userData?.favoriteSubjects]);
 
   const resourceType = searchObject['resource-types'] || '';

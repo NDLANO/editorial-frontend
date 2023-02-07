f5f6c41cf5d7002bde85fb3a6f98f2311ab9d317
Katrine Wist <katrinew0702@gmail.com>
2023-02-07 15:13:40 +0100

    Move styling to styles

--- src/containers/WelcomePage/styles.tsx
+++ src/containers/WelcomePage/styles.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import { spacing } from '@ndla/core';
+import { spacing, colors } from '@ndla/core';
+import { Link } from 'react-router-dom';
 
 export const StyledColumnHeader = styled.div`
   display: flex;
@@ -8,5 +9,22 @@ export const StyledColumnHeader = styled.div`
   & > span {
     text-transform: uppercase;
     margin-left: ${spacing.small};
+  }
+`;
+export const StyledDashboardInfo = styled.div`
+  background-color: ${colors.brand.lighter};
+  border-radius: 10px;
+  padding: ${spacing.nsmall};
+`;
+
+export const StyledTopRowDashboardInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StyledLink = styled(Link)`
+  line-height: 1.5em;
+  &:any-link {
+    color: ${colors.brand.primary};
   }
 `;

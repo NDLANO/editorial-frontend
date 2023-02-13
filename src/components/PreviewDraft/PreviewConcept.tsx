fcde30e0bf3daeef49023451221feec3ca73b6a9
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-02-13 15:16:44 +0100

    Display tags and subjects in Concept preview

--- src/components/PreviewDraft/PreviewConcept.tsx
+++ src/components/PreviewDraft/PreviewConcept.tsx
@@ -10,17 +10,29 @@ import { extractEmbedMeta } from '@ndla/article-converter';
 import { IConcept } from '@ndla/types-concept-api';
 import { ConceptVisualElementMeta } from '@ndla/types-embed';
 import { ConceptNotionV2 } from '@ndla/ui';
+import { useTaxonomyVersion } from '../../containers/StructureVersion/TaxonomyVersionProvider';
 import { usePreviewArticle } from '../../modules/article/articleGqlQueries';
+import { useSearchNodes } from '../../modules/nodes/nodeQueries';
+
 interface Props {
   concept: IConcept;
   language: string;
 }
 const PreviewConcept = ({ concept, language }: Props) => {
+  const { taxonomyVersion } = useTaxonomyVersion();
   const { data } = usePreviewArticle(
     concept.visualElement?.visualElement!,
     concept.visualElement?.language ?? language,
     undefined,
     { enabled: !!concept.visualElement?.visualElement },
+  );
+
+  const { data: subjects } = useSearchNodes(
+    {
+      ids: concept.subjectIds!,
+      taxonomyVersion,
+    },
+    { enabled: !!concept.subjectIds?.length },
   );
 
   const visualElementMeta = extractEmbedMeta(data ?? '') as ConceptVisualElementMeta;
@@ -32,6 +44,8 @@ const PreviewConcept = ({ concept, language }: Props) => {
       visualElement={visualElementMeta}
       metaImage={concept.metaImage}
       copyright={concept.copyright}
+      tags={concept.tags?.tags}
+      subjects={subjects?.results?.map(res => res.name)}
       previewAlt
     />
   );

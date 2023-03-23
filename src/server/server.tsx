4933ca13858ec6a614c28578e2bbfe724a6fd41a
Gunnar Velle <gunnar.velle@gmail.com>
2023-03-23 16:03:00 +0100

    Hotfix: bigger size limit to translate big articles.

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -55,6 +55,7 @@ app.get('*', (req, res, next) => {
 });
 
 app.use(compression());
+app.use(express.json({ limit: '1mb' }));
 app.use(
   express.static(process.env.RAZZLE_PUBLIC_DIR as string, {
     maxAge: 1000 * 60 * 60 * 24 * 365, // One year

6c5b01c38f0e543d58a6de6512f9d8b136361559
Katrine Wist <katrinew0702@gmail.com>
2023-03-27 08:51:05 +0200

    multi-line imports

--- src/modules/frontpage/filmMutations.ts
+++ src/modules/frontpage/filmMutations.ts
@@ -7,7 +7,10 @@
  */
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { IFilmFrontPageData, INewOrUpdatedFilmFrontPageData } from '@ndla/types-backend/frontpage-api';
+import {
+  IFilmFrontPageData,
+  INewOrUpdatedFilmFrontPageData,
+} from '@ndla/types-backend/frontpage-api';
 import { updateFilmFrontpage } from './frontpageApi';
 import { filmFrontpageQueryKey } from './filmQueries';
 

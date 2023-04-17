758f9f5c64f262cf64e11a31076d19355884538e
Katrine Wist <katrinew0702@gmail.com>
2023-04-17 19:01:51 +0200

    Restructure Resource page, move Comments one level higher in tree structure

--- src/containers/ArticlePage/FrontpageArticlePage/components/FrontpageArticlePanels.tsx
+++ src/containers/ArticlePage/FrontpageArticlePage/components/FrontpageArticlePanels.tsx
@@ -14,8 +14,6 @@ import { CopyrightFieldGroup, VersionAndNotesPanel, MetaDataField } from '../../
 import { FrontpageArticleFormType } from '../../../FormikForm/articleFormHooks';
 import RevisionNotes from '../../components/RevisionNotes';
 import FrontpageArticleFormContent from './FrontpageArticleFormContent';
-import { FlexWrapper, MainContent } from '../../styles';
-import CommentSection from '../../components/CommentSection';
 
 interface Props {
   handleSubmit: (
@@ -33,70 +31,65 @@ const FrontpageArticlePanels = ({ article, getArticle, handleSubmit, articleLang
   const { values, errors, handleBlur } = formikContext;
 
   return (
-    <FlexWrapper>
-      <MainContent>
-        <Accordions>
-          <AccordionSection
-            id={'frontpage-article-content'}
-            title={t('form.contentSection')}
-            className={'u-4/6@desktop u-push-1/6@desktop'}
-            hasError={!!(errors.title || errors.introduction || errors.content)}
-            startOpen
-          >
-            <FrontpageArticleFormContent
-              articleLanguage={articleLanguage}
-              formik={formikContext}
-              handleSubmit={() => handleSubmit(values, formikContext)}
-              handleBlur={handleBlur}
-              values={values}
-            />
-          </AccordionSection>
-          <AccordionSection
-            id={'frontpage-article-copyright'}
-            title={t('form.copyrightSection')}
-            className={'u-6/6'}
-            hasError={
-              !!(errors.creators || errors.rightsholders || errors.processors || errors.license)
-            }
-          >
-            <CopyrightFieldGroup values={values} />
-          </AccordionSection>
-          <AccordionSection
-            id={'frontpage-article-metadata'}
-            title={t('form.metadataSection')}
-            className={'u-6/6'}
-            hasError={!!(errors.metaDescription || errors.metaImageAlt || errors.tags)}
-          >
-            <MetaDataField articleLanguage={articleLanguage} />
-          </AccordionSection>
-          <AccordionSection
-            id={'frontpage-article-revisions'}
-            title={t('form.name.revisions')}
-            className={'u-6/6'}
-            hasError={!!errors.revisionMeta || !!errors.revisionError}
-          >
-            <RevisionNotes />
-          </AccordionSection>
+    <Accordions>
+      <AccordionSection
+        id={'frontpage-article-content'}
+        title={t('form.contentSection')}
+        className={'u-4/6@desktop u-push-1/6@desktop'}
+        hasError={!!(errors.title || errors.introduction || errors.content)}
+        startOpen
+      >
+        <FrontpageArticleFormContent
+          articleLanguage={articleLanguage}
+          formik={formikContext}
+          handleSubmit={() => handleSubmit(values, formikContext)}
+          handleBlur={handleBlur}
+          values={values}
+        />
+      </AccordionSection>
+      <AccordionSection
+        id={'frontpage-article-copyright'}
+        title={t('form.copyrightSection')}
+        className={'u-6/6'}
+        hasError={
+          !!(errors.creators || errors.rightsholders || errors.processors || errors.license)
+        }
+      >
+        <CopyrightFieldGroup values={values} />
+      </AccordionSection>
+      <AccordionSection
+        id={'frontpage-article-metadata'}
+        title={t('form.metadataSection')}
+        className={'u-6/6'}
+        hasError={!!(errors.metaDescription || errors.metaImageAlt || errors.tags)}
+      >
+        <MetaDataField articleLanguage={articleLanguage} />
+      </AccordionSection>
+      <AccordionSection
+        id={'frontpage-article-revisions'}
+        title={t('form.name.revisions')}
+        className={'u-6/6'}
+        hasError={!!errors.revisionMeta || !!errors.revisionError}
+      >
+        <RevisionNotes />
+      </AccordionSection>
 
-          {article && (
-            <AccordionSection
-              id={'frontpage-article-workflow'}
-              title={t('form.workflowSection')}
-              className={'u-6/6'}
-              hasError={!!errors.notes}
-            >
-              <VersionAndNotesPanel
-                article={article}
-                getArticle={getArticle}
-                type="standard"
-                currentLanguage={values.language}
-              />
-            </AccordionSection>
-          )}
-        </Accordions>
-      </MainContent>
-      <CommentSection savedStatus={article?.status} />
-    </FlexWrapper>
+      {article && (
+        <AccordionSection
+          id={'frontpage-article-workflow'}
+          title={t('form.workflowSection')}
+          className={'u-6/6'}
+          hasError={!!errors.notes}
+        >
+          <VersionAndNotesPanel
+            article={article}
+            getArticle={getArticle}
+            type="standard"
+            currentLanguage={values.language}
+          />
+        </AccordionSection>
+      )}
+    </Accordions>
   );
 };
 
--- src/containers/ArticlePage/LearningResourcePage/components/LearningResourcePanels.tsx
+++ src/containers/ArticlePage/LearningResourcePage/components/LearningResourcePanels.tsx
@@ -21,8 +21,6 @@ import { LearningResourceFormType } from '../../../FormikForm/articleFormHooks';
 import { useSession } from '../../../Session/SessionProvider';
 import { ArticleTaxonomy } from '../../../FormikForm/formikDraftHooks';
 import RevisionNotes from '../../components/RevisionNotes';
-import { FlexWrapper, MainContent } from '../../styles';
-import CommentSection from '../../components/CommentSection';
 
 interface Props {
   handleSubmit: (
@@ -50,100 +48,95 @@ const LearningResourcePanels = ({
   const { values, errors, handleBlur } = formikContext;
 
   return (
-    <FlexWrapper>
-      <MainContent>
-        <Accordions>
-          <AccordionSection
-            id={'learning-resource-content'}
-            title={t('form.contentSection')}
-            className={'u-4/6@desktop u-push-1/6@desktop'}
-            hasError={!!(errors.title || errors.introduction || errors.content)}
-            startOpen
-          >
-            <LearningResourceContent
-              articleLanguage={articleLanguage}
-              formik={formikContext}
-              handleSubmit={() => handleSubmit(values, formikContext)}
-              handleBlur={handleBlur}
-              values={values}
-            />
-          </AccordionSection>
-          {article && taxonomy && !!userPermissions?.includes(TAXONOMY_WRITE_SCOPE) && (
-            <AccordionSection
-              id={'learning-resource-taxonomy'}
-              title={t('form.taxonomySection')}
-              className={'u-6/6'}
-            >
-              <LearningResourceTaxonomy
-                article={article}
-                updateNotes={updateNotes}
-                taxonomy={taxonomy}
-              />
-            </AccordionSection>
-          )}
-          <AccordionSection
-            id={'learning-resource-copyright'}
-            title={t('form.copyrightSection')}
-            className={'u-6/6'}
-            hasError={
-              !!(errors.creators || errors.rightsholders || errors.processors || errors.license)
-            }
-          >
-            <CopyrightFieldGroup values={values} />
-          </AccordionSection>
-          <AccordionSection
-            id={'learning-resource-metadata'}
-            title={t('form.metadataSection')}
-            className={'u-6/6'}
-            hasError={!!(errors.metaDescription || errors.metaImageAlt || errors.tags)}
-          >
-            <MetaDataField articleLanguage={articleLanguage} />
-          </AccordionSection>
-          <AccordionSection
-            id={'learning-resource-grepCodes'}
-            title={t('form.name.grepCodes')}
-            className={'u-6/6'}
-            hasError={!!errors.grepCodes}
-          >
-            <GrepCodesField />
-          </AccordionSection>
-          {config.ndlaEnvironment === 'test' && (
-            <AccordionSection
-              id={'learning-resource-related'}
-              title={t('form.name.relatedContent')}
-              className={'u-6/6'}
-              hasError={!!(errors.conceptIds || errors.relatedContent)}
-            >
-              <RelatedContentFieldGroup />
-            </AccordionSection>
-          )}
-          <AccordionSection
-            id={'learning-resource-revisions'}
-            title={t('form.name.revisions')}
-            className={'u-6/6'}
-            hasError={!!errors.revisionMeta || !!errors.revisionError}
-          >
-            <RevisionNotes />
-          </AccordionSection>
-          {article && (
-            <AccordionSection
-              id={'learning-resource-workflow'}
-              title={t('form.workflowSection')}
-              className={'u-6/6'}
-              hasError={!!errors.notes}
-            >
-              <VersionAndNotesPanel
-                article={article}
-                getArticle={getArticle}
-                type="standard"
-                currentLanguage={values.language}
-              />
-            </AccordionSection>
-          )}
-        </Accordions>
-      </MainContent>
-      <CommentSection savedStatus={article?.status} />
-    </FlexWrapper>
+    <Accordions>
+      <AccordionSection
+        id={'learning-resource-content'}
+        title={t('form.contentSection')}
+        className={'u-4/6@desktop u-push-1/6@desktop'}
+        hasError={!!(errors.title || errors.introduction || errors.content)}
+        startOpen
+      >
+        <LearningResourceContent
+          articleLanguage={articleLanguage}
+          formik={formikContext}
+          handleSubmit={() => handleSubmit(values, formikContext)}
+          handleBlur={handleBlur}
+          values={values}
+        />
+      </AccordionSection>
+      {article && taxonomy && !!userPermissions?.includes(TAXONOMY_WRITE_SCOPE) && (
+        <AccordionSection
+          id={'learning-resource-taxonomy'}
+          title={t('form.taxonomySection')}
+          className={'u-6/6'}
+        >
+          <LearningResourceTaxonomy
+            article={article}
+            updateNotes={updateNotes}
+            taxonomy={taxonomy}
+          />
+        </AccordionSection>
+      )}
+      <AccordionSection
+        id={'learning-resource-copyright'}
+        title={t('form.copyrightSection')}
+        className={'u-6/6'}
+        hasError={
+          !!(errors.creators || errors.rightsholders || errors.processors || errors.license)
+        }
+      >
+        <CopyrightFieldGroup values={values} />
+      </AccordionSection>
+      <AccordionSection
+        id={'learning-resource-metadata'}
+        title={t('form.metadataSection')}
+        className={'u-6/6'}
+        hasError={!!(errors.metaDescription || errors.metaImageAlt || errors.tags)}
+      >
+        <MetaDataField articleLanguage={articleLanguage} />
+      </AccordionSection>
+      <AccordionSection
+        id={'learning-resource-grepCodes'}
+        title={t('form.name.grepCodes')}
+        className={'u-6/6'}
+        hasError={!!errors.grepCodes}
+      >
+        <GrepCodesField />
+      </AccordionSection>
+      {config.ndlaEnvironment === 'test' && (
+        <AccordionSection
+          id={'learning-resource-related'}
+          title={t('form.name.relatedContent')}
+          className={'u-6/6'}
+          hasError={!!(errors.conceptIds || errors.relatedContent)}
+        >
+          <RelatedContentFieldGroup />
+        </AccordionSection>
+      )}
+      <AccordionSection
+        id={'learning-resource-revisions'}
+        title={t('form.name.revisions')}
+        className={'u-6/6'}
+        hasError={!!errors.revisionMeta || !!errors.revisionError}
+      >
+        <RevisionNotes />
+      </AccordionSection>
+      {article && (
+        <AccordionSection
+          id={'learning-resource-workflow'}
+          title={t('form.workflowSection')}
+          className={'u-6/6'}
+          hasError={!!errors.notes}
+        >
+          <VersionAndNotesPanel
+            article={article}
+            getArticle={getArticle}
+            type="standard"
+            currentLanguage={values.language}
+          />
+        </AccordionSection>
+      )}
+    </Accordions>
   );
 };
 

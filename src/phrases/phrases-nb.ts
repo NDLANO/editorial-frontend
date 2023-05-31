5bd26b2bdc1051928e44bc4e65bd4e69b30fb80c
MaPoKen <larssoerlie96@gmail.com>
2023-05-31 08:17:29 +0200

    Skrivefeil bokmål

--- src/phrases/phrases-nb.ts
+++ src/phrases/phrases-nb.ts
@@ -599,7 +599,7 @@ const phrases = {
       podcast: 'Podkastepisode',
       conceptList: 'Forklaringsliste',
       concept: 'Forklaring',
-      keyFigure: 'Nykkeltall',
+      keyFigure: 'Nøkkeltall',
       blogPost: 'Bloggpost',
     },
   },

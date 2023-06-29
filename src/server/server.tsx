5f670c2a60ea3c1d61ed0d60d90f9fe39b561559
Jonas Natten <jonas.natten@knowit.no>
2023-06-29 14:22:41 +0200

    Fix vercel deployment
    Four changes are required to fix the deployment:
      - Disable manual listening and export the express application in index.js
      - Disable compression on vercel
      - Set module to `commonjs` in tsconfig.json
      - Set target to `es6` in tsconfig.json
        - Not entierly sure why this is now required on vercel, but the
          error we received (`Constructor Map requires 'new'`) seemed to
          indicate that some es6 features were used somewhere ¯\_(ツ)_/¯

--- src/server/server.tsx
+++ src/server/server.tsx
@@ -54,7 +54,9 @@ app.get('*', (req, res, next) => {
   }
 });
 
-app.use(compression());
+if (!config.isVercel) {
+  app.use(compression());
+}
 app.use(express.json({ limit: '1mb' }));
 app.use(
   express.static(process.env.RAZZLE_PUBLIC_DIR as string, {

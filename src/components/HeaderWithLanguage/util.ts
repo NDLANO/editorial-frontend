6de86ae46b7f0b528edb8478aa3301c2dcf89e55
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-07-14 14:43:21 +0200

    Fix status symbol in header

--- src/components/HeaderWithLanguage/util.ts
+++ src/components/HeaderWithLanguage/util.ts
@@ -3,5 +3,6 @@ import { Node } from '@ndla/types-taxonomy';
 export const getTaxonomyPathsFromTaxonomy = (taxonomy?: Node[], articleId?: number): string[] => {
   const taxPaths = taxonomy?.flatMap((t) => t.paths) ?? [];
   const articlePath = articleId ? `/article/${articleId}` : undefined;
-  return [taxPaths, articlePath].filter((p): p is string => p !== undefined);
+  const paths = articlePath ? taxPaths.concat(articlePath) : taxPaths;
+  return paths.filter((p): p is string => p !== undefined);
 };

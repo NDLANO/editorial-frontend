f86bfd643d66e9ee210b7957868059a9d82ca379
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-08-10 15:48:30 +0200

    Fix header-with-language-test

--- src/components/HeaderWithLanguage/util.ts
+++ src/components/HeaderWithLanguage/util.ts
@@ -1,6 +1,9 @@
 import { Node } from '@ndla/types-taxonomy';
 
-export const getTaxonomyPathsFromTaxonomy = (taxonomy?: Node[], articleId?: number): string[] => {
+export const getTaxonomyPathsFromTaxonomy = (
+  taxonomy?: Pick<Node, 'paths'>[],
+  articleId?: number,
+): string[] => {
   const taxPaths = taxonomy?.flatMap((t) => t.paths) ?? [];
   const articlePath = articleId ? `/article/${articleId}` : undefined;
   const paths = articlePath ? taxPaths.concat(articlePath) : taxPaths;

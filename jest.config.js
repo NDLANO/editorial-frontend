e4731ea212118a2841508018d2fbd57d16a8cb35
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-08-11 18:37:14 +0200

    Fix some test errors and slightly modernize jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  testRegex: '/__tests__/.*-test.(js|jsx|ts|tsx)$',
+  testMatch: ['**/__tests__/*-test.(js|jsx|ts|tsx)'],
   setupFiles: ['./src/__tests__/_initTestEnv.js'],
   setupFilesAfterEnv: ['./src/__tests__/jest.setup.js'],
   snapshotSerializers: ['@emotion/jest/serializer'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-      },
-    ],
-  },
+  preset: 'ts-jest/presets/js-with-babel',
 };

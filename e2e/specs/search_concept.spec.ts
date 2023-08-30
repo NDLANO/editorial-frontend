c7e7dab19db6262372f46591e33e6b47c5001309
MaPoKen <larssoerlie96@gmail.com>
2023-08-30 14:01:04 +0200

    Added search concept test spec

---
+++ e2e/specs/search_concept.spec.ts
@@ -0,0 +1,214 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { test, expect } from '@playwright/test';
+import { mockRoute } from '../apiMock';
+import { responsiblesMock, userDataMock, zendeskMock } from '../mockResponses';
+
+test.beforeEach(async ({ page }) => {
+
+    const licenses = mockRoute({
+        page,
+        path: '**/draft-api/v1/drafts/licenses/',
+        fixture: 'search_concept_licenses',
+    });
+
+    const subjects = mockRoute({
+        page,
+        path: '**/taxonomy/v1/nodes?language=nb&nodeType=SUBJECT',
+        fixture: 'search_concept_subjects',
+    });
+
+    const getEditors = mockRoute({
+        page,
+        path: '**/get_editors',
+        fixture: 'search_concept_get_editors',
+        overrideValue: () => JSON.stringify([
+            {
+                name: 'Ed Test',
+                app_metadata: {
+                    ndla_id: 'PrcePFwCDOsb2_g0Kcb-maN0'
+                }
+            }]),
+    })
+
+
+    const searchConcept = mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=1*',
+        fixture: 'search_concept_search'
+    });
+
+    const searchNextPage = mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=2*',
+        fixture: 'search_concept_next_search'
+    });
+
+    const zendesk = mockRoute({
+        page,
+        path: '**/get_zendesk_token',
+        fixture: 'search_concept_zendesk_token',
+        overrideValue: JSON.stringify(zendeskMock),
+    });
+
+    const noteUsers = mockRoute({
+        page,
+        path: '**/get_note_users?*',
+        fixture: 'search_concept_note_users',
+    });
+
+    const userData = mockRoute({
+        page,
+        path: '**/draft-api/v1/user-data',
+        fixture: 'search_concept_user_data',
+        overrideValue: JSON.stringify(userDataMock),
+    });
+
+
+
+
+    const responsibles = mockRoute({
+        page,
+        path: '**/get_responsibles*',
+        fixture: 'search_concept_responsibles',
+        overrideValue: JSON.stringify(responsiblesMock),
+    });
+
+
+    const statusStateMachine = mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/status-state-machine/',
+        fixture: 'search_concept_status_state_machine',
+    });
+
+
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+
+    await Promise.all([
+        licenses,
+        statusStateMachine,
+        searchConcept,
+        subjects,
+        noteUsers,
+        responsibles,
+        zendesk,
+        userData,
+        searchNextPage,
+        getEditors
+    ]);
+});
+
+
+test('Can use text input', async ({ page }) => {
+    await page.locator('input[name="query"]').fill('Test');
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=*&page-size=10&query=Test&sort=-lastUpdated*',
+        fixture: 'search_concept_query_search',
+    });
+    await page.goto(
+        '/search/concept?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=1&page-size=10&query=Test&sort=-lastUpdated',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('73');
+    await page.locator('input[name="query"]').clear();
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});
+
+test('Can use status dropdown', async ({ page }) => {
+    await page.locator('select[name="status"]').selectOption({ index: 1 });
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=*&page-size=10&sort=-lastUpdated&status=IN_PROGRESS',
+        fixture: 'search_concept_status_search',
+    });
+    await page.goto(
+        '/search/concept?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=1&page-size=10&sort=-lastUpdated&status=IN_PROGRESS',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('949');
+    await page.locator('select[name="status"]').selectOption({ index: 0 });
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});
+
+test('Can use language dropdown', async ({ page }) => {
+    await page.locator('select[name="language"]').selectOption({ index: 1 });
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&language=en&page=*&page-size=10&sort=-lastUpdated',
+        fixture: 'search_concept_lang_search',
+    });
+    await page.goto(
+        '/search/concept?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&language=en&page=1&page-size=10&sort=-lastUpdated',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('34');
+    await page.locator('select[name="language"]').selectOption({ index: 0 });
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});
+
+test('Can use subject dropdown', async ({ page }) => {
+    await page.locator('select[name="subjects"]').selectOption({ index: 3 });
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=*&page-size=10&sort=-lastUpdated&subjects=urn%3Asubject%3Af665de3e-65dc-478e-b736-cb0af3d38ad4',
+        fixture: 'search_concept_subjects_search',
+    });
+    await page.goto(
+        '/search/concept?include-other-statuses=false&fallback=false&page=1&page-size=10&sort=-lastUpdated&subjects=urn:subject:f665de3e-65dc-478e-b736-cb0af3d38ad4&exclude-revision-log=false&filter-inactive=true',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('429');
+    await page.locator('select[name="subjects"]').selectOption({ index: 0 });
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});
+
+test('Can use responsible dropdown', async ({ page }) => {
+    await page.locator('select[name="responsible-ids"]').selectOption({ index: 1 });
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=*&page-size=10&responsible-ids=Gxfx7B-MXoFdgVZZ6p611C6w&sort=-lastUpdated',
+        fixture: 'search_concept_responsible_search',
+    });
+    await page.goto(
+        '/search/concept?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=1&page-size=10&responsible-ids=Gxfx7B-MXoFdgVZZ6p611C6w&sort=-lastUpdated',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('12');
+    await page.locator('select[name="responsible-ids"]').selectOption({ index: 0 });
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});
+
+test('Can use user dropdown', async ({ page }) => {
+    await page.locator('select[name="users"]').selectOption({ index: 1 });
+    await mockRoute({
+        page,
+        path: '**/concept-api/v1/drafts/?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=*&page-size=10&sort=-lastUpdated&users=Gxfx7B-MXoFdgVZZ6p611C6w',
+        fixture: 'search_concept_users_search',
+    });
+    await page.goto(
+        '/search/concept?exclude-revision-log=false&fallback=false&filter-inactive=true&include-other-statuses=false&page=1&page-size=10&sort=-lastUpdated&users=Gxfx7B-MXoFdgVZZ6p611C6w',
+    );
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('17');
+    await page.locator('select[name="users"]').selectOption({ index: 0 });
+    await page.goto('/search/concept?page=1&page-size=10&sort=-lastUpdated');
+    await page.getByTestId('concept-search-result').first().waitFor();
+    expect(await page.getByTestId('searchTotalCount').innerText()).toEqual('4872');
+});

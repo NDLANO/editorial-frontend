9b68d4fed4ec2cd98a741da6e02337ecff257eb3
MaPoKen <larssoerlie96@gmail.com>
2023-09-14 16:38:56 +0200

    Added jonases regex

--- src/components/validators.ts
+++ src/components/validators.ts
@@ -95,6 +95,7 @@ export const isNumeric = (value: any) => !Number.isNaN(value - parseFloat(value)
 export const objectHasBothField = (obj: Dictionary<any>) =>
   Object.keys(obj).filter((key) => isEmpty(obj[key])).length === 0;
 
-const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-export const isEmail = (value: string) => emailRegex.test(value);
+export const isEmail = (value: string) => EMAIL_REGEX.test(value);

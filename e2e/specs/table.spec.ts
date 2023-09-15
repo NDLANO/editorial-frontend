ce2e9ad70be7085b1665fe22a2bca172ad5d19d8
MaPoKen <larssoerlie96@gmail.com>
2023-09-15 16:14:45 +0200

    Split table tests into more managable chunks

--- e2e/specs/table.spec.ts
+++ e2e/specs/table.spec.ts
@@ -45,13 +45,16 @@ test.beforeEach(async ({ page }) => {
   await page.locator('[data-cy="create-table"]').click();
 });
 
-test('all table functions work', async ({ page }) => {
-  expect(await page.locator('th').count()).toEqual(2);
-  expect(await page.locator('td').count()).toEqual(2);
-  expect(await page.locator('tr').count()).toEqual(2);
+test('can fill table caption', async ({ page }) => {
   await page.locator('caption').click();
   await page.keyboard.type('TITTEL!');
   await expect(page.locator('caption')).toHaveText('TITTEL!');
+});
+
+test('can move around and place text in table', async ({ page }) => {
+  expect(await page.locator('th').count()).toEqual(2);
+  expect(await page.locator('td').count()).toEqual(2);
+  expect(await page.locator('tr').count()).toEqual(2);
   await page.locator('tbody > tr > td').first().fill('Cell');
   await page.locator('thead > tr > th').first().click();
   await page.keyboard.type('Header 1');
@@ -60,18 +63,6 @@ test('all table functions work', async ({ page }) => {
   await expect(page.locator('thead > tr > th').first()).toHaveText('Header 1');
   await expect(page.locator('thead > tr > th').last()).toHaveText('Header 2');
   await page.locator('[data-cy="column-add"]').click();
-  expect(await page.locator('th').count()).toEqual(3);
-  expect(await page.locator('td').count()).toEqual(3);
-
-  await page.locator('thead > tr > th').last().click();
-  await page.keyboard.type('Test new column');
-
-  await page.locator('[data-cy="row-remove"]').click();
-  expect(await page.locator('th').count()).toEqual(0);
-  expect(await page.locator('td').count()).toEqual(3);
-  expect(await page.locator('tr').count()).toEqual(1);
-  await page.locator('[data-cy="head-add"]').click();
-  expect(await page.locator('tr').count()).toEqual(2);
   expect(await page.locator('th').count()).toEqual(3);
   expect(await page.locator('td').count()).toEqual(3);
 
@@ -91,7 +82,51 @@ test('all table functions work', async ({ page }) => {
   expect(await page.locator('td').count()).toEqual(2);
   await page.locator('[data-cy="row-remove"]').click();
   expect(await page.locator('tr').count()).toEqual(2);
-  await page.locator('[data-cy="table-remove"]').click();
+});
+
+test('can add rows and columns', async ({ page }) => {
+  await page.locator('thead > tr > th').last().click();
+  await page.keyboard.type('Test new header');
+  await page.keyboard.press('ArrowDown');
+  await page.locator('[data-cy="row-add"]').click();
+  await page.locator('tbody > tr > td').last().click();
+  expect(await page.locator('tr').count()).toEqual(3);
+  expect(await page.locator('td').count()).toEqual(4);
+  await page.keyboard.type('Test new row');
+  await page.locator('[data-cy="toggle-row-headers"]').click();
+  expect(await page.locator('th').count()).toEqual(4);
+  expect(await page.locator('td').count()).toEqual(2);
+  await page.locator('[data-cy="column-remove"]').click();
+  expect(await page.locator('th').count()).toEqual(3);
+  expect(await page.locator('td').count()).toEqual(0);
+  await page.locator('[data-cy="row-remove"]').click();
+  expect(await page.locator('tr').count()).toEqual(2);
+});
+
+test('can remove and add table headers', async ({ page }) => {
+  expect(await page.locator('th').count()).toEqual(2);
+  expect(await page.locator('td').count()).toEqual(2);
+  expect(await page.locator('tr').count()).toEqual(2);
+  await page.locator('thead > tr > th').first().click();
+  await page.locator('[data-cy="row-remove"]').click();
+  expect(await page.locator('th').count()).toEqual(0);
+  expect(await page.locator('td').count()).toEqual(2);
+  expect(await page.locator('tr').count()).toEqual(1);
+  await page.locator('[data-cy="head-add"]').click();
+  expect(await page.locator('th').count()).toEqual(2);
+  expect(await page.locator('td').count()).toEqual(2);
+  expect(await page.locator('tr').count()).toEqual(2);
+});
+
+test('can toggle row headers', async ({ page }) => {
+  expect(await page.locator('th').count()).toEqual(2);
+  expect(await page.locator('td').count()).toEqual(2);
+  expect(await page.locator('tr').count()).toEqual(2);
+  await page.locator('tbody > tr > td').first().click();
+  await page.locator('[data-cy="toggle-row-headers"]').click();
+  expect(await page.locator('th').count()).toEqual(3);
+  expect(await page.locator('td').count()).toEqual(1);
+  expect(await page.locator('tr').count()).toEqual(2);
 });
 
 test('blockpicker to give limited options in table', async ({ page }) => {

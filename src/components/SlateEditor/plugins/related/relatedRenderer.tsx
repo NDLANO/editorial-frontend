b447506d83c1411ca74f9226265238886cb564b3
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-11-03 10:09:59 +0100

    Add missing copyright

--- src/components/SlateEditor/plugins/related/relatedRenderer.tsx
+++ src/components/SlateEditor/plugins/related/relatedRenderer.tsx
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2023-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { Editor, Transforms } from 'slate';
 import { MouseEvent } from 'react';
 import { ReactEditor } from 'slate-react';

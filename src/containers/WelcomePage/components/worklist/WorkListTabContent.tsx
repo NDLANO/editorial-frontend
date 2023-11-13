bce99272f5e3e9d8e36f58938168549f1f4cf8c5
Katrine Wist <katrinew0702@gmail.com>
2023-11-13 20:05:00 +0100

    Add new tab in worklist for articles on hold

--- src/containers/WelcomePage/components/worklist/WorkListTabContent.tsx
+++ src/containers/WelcomePage/components/worklist/WorkListTabContent.tsx
@@ -57,19 +57,21 @@ const StyledIconWrapper = styled.div`
 `;
 
 interface Props {
-  data?: IMultiSearchResult;
-  filterSubject?: SingleValue;
+  data: IMultiSearchResult | undefined;
   isLoading: boolean;
   setSortOption: (o: Prefix<'-', SortOption>) => void;
   sortOption: string;
   error: string | undefined;
-  setFilterSubject: (fs: SingleValue) => void;
-  ndlaId?: string;
+  ndlaId: string | undefined;
   setPage: (page: number) => void;
-  setPrioritized: (prioritized: boolean) => void;
-  prioritized: boolean;
   pageSize: SingleValue;
   setPageSize: (p: SingleValue) => void;
+  filterSubject?: SingleValue;
+  setFilterSubject?: (fs: SingleValue) => void;
+  setPrioritized?: (prioritized: boolean) => void;
+  prioritized?: boolean;
+  headerText?: string;
+  descriptionText?: string;
 }
 const WorkListTabContent = ({
   data,
@@ -85,6 +87,8 @@ const WorkListTabContent = ({
   prioritized,
   pageSize,
   setPageSize,
+  headerText = 'welcomePage.workList.heading',
+  descriptionText = 'welcomePage.workList.description',
 }: Props) => {
   const { t } = useTranslation();
 
@@ -172,35 +176,41 @@ const WorkListTabContent = ({
   return (
     <>
       <StyledTopRowDashboardInfo>
-        <TableTitle
-          title={t('welcomePage.workList.heading')}
-          description={t('welcomePage.workList.description')}
-          Icon={Calendar}
-        />
+        <TableTitle title={t(headerText)} description={t(descriptionText)} Icon={Calendar} />
         <ControlWrapperDashboard>
           <TopRowControls>
             <PageSizeDropdown pageSize={pageSize} setPageSize={setPageSize} />
-            <SubjectDropdown
-              subjectIds={subjectIds || []}
-              filterSubject={filterSubject}
-              setFilterSubject={setFilterSubject}
-            />
-            <GoToSearch ndlaId={ndlaId} filterSubject={filterSubject?.value} searchEnv="content" />
+            {setFilterSubject && (
+              <>
+                <SubjectDropdown
+                  subjectIds={subjectIds || []}
+                  filterSubject={filterSubject}
+                  setFilterSubject={setFilterSubject}
+                />
+                <GoToSearch
+                  ndlaId={ndlaId}
+                  filterSubject={filterSubject?.value}
+                  searchEnv="content"
+                />
+              </>
+            )}
           </TopRowControls>
-          <Tooltip tooltip={t('welcomePage.prioritizedLabel')}>
-            <SwitchWrapper>
-              <StyledSwitch
-                checked={prioritized}
-                onChange={() => {
-                  setPrioritized(!prioritized);
-                  setPage(1);
-                }}
-                label={t('welcomePage.prioritizedLabel')}
-                id="filter-prioritized-switch"
-                thumbCharacter="P"
-              />
-            </SwitchWrapper>
-          </Tooltip>
+          {setPrioritized && prioritized && (
+            <Tooltip tooltip={t('welcomePage.prioritizedLabel')}>
+              <SwitchWrapper>
+                <StyledSwitch
+                  checked={prioritized}
+                  onChange={() => {
+                    setPrioritized(!prioritized);
+                    setPage(1);
+                  }}
+                  label={t('welcomePage.prioritizedLabel')}
+                  id="filter-prioritized-switch"
+                  thumbCharacter="P"
+                />
+              </SwitchWrapper>
+            </Tooltip>
+          )}
         </ControlWrapperDashboard>
       </StyledTopRowDashboardInfo>
       <TableComponent

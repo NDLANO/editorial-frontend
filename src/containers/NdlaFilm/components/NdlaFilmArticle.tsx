b51a0cdb885ee62d3f9e444060fc8f5537ae89d5
Gaute Rauboti <gaute.rauboti@knowit.no>
2023-11-26 17:58:20 +0100

    Removed unused import

--- src/containers/NdlaFilm/components/NdlaFilmArticle.tsx
+++ src/containers/NdlaFilm/components/NdlaFilmArticle.tsx
@@ -6,7 +6,7 @@
  */
 
 import { useField, useFormikContext } from 'formik';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';

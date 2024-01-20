39325890cf58c4fe3674efebcffe3ab8cdef790a
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-01-20 17:48:59 +0100

    Remove webpack setup and related deps

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,16 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-typescript',
-    ['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }],
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['> 0.25%', 'not dead'],
-        },
-      },
-    ],
-  ],
-
   plugins: [['@emotion', { autoLabel: 'always' }], '@loadable/babel-plugin'],
 };

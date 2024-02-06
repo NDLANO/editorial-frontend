b01cfb27b90c3fcc1235b8e32789c23062ceb554
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-02-06 13:50:03 +0100

    Fixup: Revert unrelated changes after rebase

--- src/containers/FrontpageEditPage/frontpageHelpers.ts
+++ src/containers/FrontpageEditPage/frontpageHelpers.ts
@@ -16,7 +16,7 @@ export const extractArticleIds = (menu: MenuWithArticle): number[] => {
   return [menu.articleId].concat(childIds);
 };
 
-const _addArticlesToMenu = (menu: IMenu | IFrontPage, articles: Record<number, IArticleSummaryV2>): MenuWithArticle => {
+const _addArticlesToMenu = (menu: IMenu, articles: Record<number, IArticleSummaryV2>): MenuWithArticle => {
   const article = articles[menu.articleId];
   const children = menu.menu.map((m) => _addArticlesToMenu(m, articles));
   return { article: article, articleId: menu.articleId, menu: children, hideLevel: menu.hideLevel ?? false };

28dc2d1ad7ad7e39bbfa216b89da9486bbfbaf1f
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-02-08 13:31:15 +0100

    Added serializer tests for disclaimer with content

--- src/components/SlateEditor/plugins/uuDisclaimer/__tests__/serializer-test.ts
+++ src/components/SlateEditor/plugins/uuDisclaimer/__tests__/serializer-test.ts
@@ -8,26 +8,64 @@
 
 import { Descendant } from "slate";
 import { blockContentToEditorValue, blockContentToHTML } from "../../../../../util/articleContentConverter";
+import { TYPE_FRAMED_CONTENT } from "../../framedContent/types";
+import { TYPE_PARAGRAPH } from "../../paragraph/types";
+import { TYPE_SECTION } from "../../section/types";
 import { TYPE_DISCLAIMER } from "../types";
 
-const editor: Descendant[] = [
-  {
-    type: TYPE_DISCLAIMER,
-    data: { resource: "uu-disclaimer", disclaimer: "disclaimer text" },
-    children: [{ text: "" }],
-  },
-];
+describe("disclaimer with content serializing tests", () => {
+  const editorContent: Descendant[] = [
+    {
+      type: TYPE_DISCLAIMER,
+      data: { resource: "uu-disclaimer", disclaimer: "disclaimer text" },
+      children: [
+        {
+          type: TYPE_SECTION,
+          children: [
+            { type: TYPE_PARAGRAPH, children: [{ text: "" }] },
+            {
+              type: TYPE_FRAMED_CONTENT,
+              children: [{ type: TYPE_PARAGRAPH, children: [{ text: "content" }] }],
+            },
+            { type: TYPE_PARAGRAPH, children: [{ text: "" }] },
+          ],
+        },
+      ],
+    },
+  ];
 
-const html = '<ndlaembed data-resource="uu-disclaimer" data-disclaimer="disclaimer text"></ndlaembed>';
+  const htmlContent =
+    '<ndlaembed data-resource="uu-disclaimer" data-disclaimer="disclaimer text"><section><div data-type="framed-content"><p>content</p></div></section></ndlaembed>';
 
-describe("disclaimer serializing tests", () => {
   test("serializing", () => {
-    const res = blockContentToHTML(editor);
-    expect(res).toMatch(html);
+    const res = blockContentToHTML(editorContent);
+    expect(res).toMatch(htmlContent);
   });
 
   test("deserializing", () => {
-    const res = blockContentToEditorValue(html);
-    expect(res).toEqual(editor);
+    const res = blockContentToEditorValue(htmlContent);
+    expect(res).toEqual(editorContent);
+  });
+});
+
+describe("disclaimer without content serializing tests", () => {
+  const editorEmpty: Descendant[] = [
+    {
+      type: TYPE_DISCLAIMER,
+      data: { resource: "uu-disclaimer", disclaimer: "disclaimer text" },
+      children: [{ text: "" }],
+    },
+  ];
+
+  const htmlEmpty = '<ndlaembed data-resource="uu-disclaimer" data-disclaimer="disclaimer text"></ndlaembed>';
+
+  test("serializing", () => {
+    const res = blockContentToHTML(editorEmpty);
+    expect(res).toMatch(htmlEmpty);
+  });
+
+  test("deserializing", () => {
+    const res = blockContentToEditorValue(htmlEmpty);
+    expect(res).toEqual(editorEmpty);
   });
 });

d2b911deb660778056530528228752b21919e3c8
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-02-08 13:49:44 +0100

    I have never seen that file in my life

--- src/containers/FrontpageEditPage/__tests__/frontpageHelpers-test.ts
+++ src/containers/FrontpageEditPage/__tests__/frontpageHelpers-test.ts
@@ -12,69 +12,70 @@ describe("extractArticleIds", () => {
   it("should return a single value for a menu with no children", () => {
     const menu: IMenu = {
       articleId: 1,
-      hideLevel: false,
       menu: [],
+      hideLevel: false,
     };
     expect(extractArticleIds(menu)).toEqual([1]);
   });
   it("should handle nested values", () => {
     const menu: IMenu = {
       articleId: 1,
-      hideLevel: false,
       menu: [
         {
           articleId: 2,
-          hideLevel: false,
           menu: [],
+          hideLevel: false,
         },
         {
           articleId: 3,
-          hideLevel: false,
           menu: [],
+          hideLevel: false,
         },
       ],
+      hideLevel: false,
     };
     expect(extractArticleIds(menu)).toEqual([1, 2, 3]);
   });
   it("should handle deeply nested values", () => {
     const menu: IMenu = {
       articleId: 1,
-      hideLevel: false,
       menu: [
         {
           articleId: 2,
-          hideLevel: false,
           menu: [
             {
               articleId: 3,
-              hideLevel: false,
               menu: [
-                { articleId: 4, hideLevel: false, menu: [] },
-                { articleId: 5, hideLevel: false, menu: [] },
+                { articleId: 4, menu: [], hideLevel: false },
+                { articleId: 5, menu: [], hideLevel: false },
               ],
+              hideLevel: false,
             },
           ],
+          hideLevel: false,
         },
         {
           articleId: 6,
-          hideLevel: false,
           menu: [
             {
               articleId: 7,
-              hideLevel: false,
               menu: [],
+              hideLevel: false,
             },
             {
               articleId: 8,
-              hideLevel: false,
+
               menu: [
-                { articleId: 9, hideLevel: false, menu: [] },
-                { articleId: 10, hideLevel: false, menu: [] },
+                { articleId: 9, menu: [], hideLevel: false },
+                { articleId: 10, menu: [], hideLevel: false },
               ],
+              hideLevel: false,
             },
           ],
+          hideLevel: false,
         },
       ],
+      hideLevel: false,
     };
     expect(extractArticleIds(menu)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });

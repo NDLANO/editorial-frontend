596eb32eab0b49d29f59fe76797109384bd7e174
Katrine Wist <katrinew0702@gmail.com>
2024-04-15 14:50:30 +0200

    Update phrases + remove new lines after license

--- src/containers/WelcomePage/components/SubjectView.tsx
+++ src/containers/WelcomePage/components/SubjectView.tsx
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "@emotion/styled";
--- src/containers/WelcomePage/components/SubjectViewContent.tsx
+++ src/containers/WelcomePage/components/SubjectViewContent.tsx
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
+
 import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import styled from "@emotion/styled";

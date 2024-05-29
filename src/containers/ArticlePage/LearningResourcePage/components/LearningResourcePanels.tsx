9769f65ead67b6ba9a491b96fec34f24ed1d3d51
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-05-29 10:19:05 +0200

    Create a variant prop for FormAccordion, to reduce className usage

--- src/containers/ArticlePage/LearningResourcePage/components/LearningResourcePanels.tsx
+++ src/containers/ArticlePage/LearningResourcePage/components/LearningResourcePanels.tsx
@@ -65,7 +65,7 @@ const LearningResourcePanels = ({
             fieldsToIndicatedChangesFor={contentTitleFields}
           />
         }
-        className="u-10/12 u-push-1/12"
+        variant="center"
         hasError={!!(errors.title || errors.introduction || errors.content)}
       >
         <IsNewArticleLanguageProvider locale={articleLanguage} article={article}>

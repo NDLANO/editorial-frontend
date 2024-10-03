5edc06d6329006ba4af6949aa78671996a1ce3aa
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-10-03 15:38:11 +0200

    chore: drop some unused dependencies

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,8 +7,6 @@
  */
 
 import fetch from "cross-fetch";
-import jwt from "jsonwebtoken";
-import { v4 as uuidv4 } from "uuid";
 import { getEnvironmentVariabel, getUniversalConfig } from "../config";
 
 const url = `https://${getUniversalConfig().auth0Domain}/oauth/token`;

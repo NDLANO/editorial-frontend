3461bf7f9d000e41f33cfeaa53db283091151ded
MaPoKen <larssoerlie96@gmail.com>
2024-10-24 13:59:14 +0200

    Added new dialog items slate video

--- src/components/SlateEditor/plugins/video/EditVideo.tsx
+++ src/components/SlateEditor/plugins/video/EditVideo.tsx
@@ -11,7 +11,17 @@ import { useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { Descendant } from "slate";
 import { ModalBody, ModalCloseButton, ModalHeader, ModalTitle } from "@ndla/modal";
-import { Button, FieldInput, FieldErrorMessage, FieldLabel, FieldRoot, Text } from "@ndla/primitives";
+import {
+  Button,
+  FieldInput,
+  FieldErrorMessage,
+  FieldLabel,
+  FieldRoot,
+  Text,
+  DialogHeader,
+  DialogTitle,
+  DialogBody,
+} from "@ndla/primitives";
 import { styled } from "@ndla/styled-system/jsx";
 import { BrightcoveEmbedData } from "@ndla/types-embed";
 import { VideoWrapper } from "./SlateVideo";
@@ -20,6 +30,7 @@ import { InlineField } from "../../../../containers/FormikForm/InlineField";
 import { inlineContentToEditorValue } from "../../../../util/articleContentConverter";
 import { isFormikFormDirty } from "../../../../util/formHelper";
 import { addBrightCoveTimeStampVideoid, getBrightCoveStartTime } from "../../../../util/videoUtil";
+import { DialogCloseButton } from "../../../DialogCloseButton";
 import { FormField } from "../../../FormField";
 import { FormActionsContainer, FormikForm } from "../../../FormikForm";
 import validateFormik, { RulesType } from "../../../formikValidationSchema";
@@ -79,11 +90,11 @@ const EditVideo = ({ onSave, setHasError, embed, onClose }: Props) => {
 
   return (
     <>
-      <ModalHeader>
-        <ModalTitle>{t("form.video.editVideo")}</ModalTitle>
-        <ModalCloseButton />
-      </ModalHeader>
-      <ModalBody>
+      <DialogHeader>
+        <DialogTitle>{t("form.video.editVideo")}</DialogTitle>
+        <DialogCloseButton />
+      </DialogHeader>
+      <DialogBody>
         <VideoWrapper>
           <StyledVideo title={`Video: ${embed?.title}`} src={activeSrc(embed)} allowFullScreen />
         </VideoWrapper>
@@ -96,7 +107,7 @@ const EditVideo = ({ onSave, setHasError, embed, onClose }: Props) => {
         >
           {(field) => <VideoEmbedForm {...field} setHasError={setHasError} close={onClose} />}
         </Formik>
-      </ModalBody>
+      </DialogBody>
     </>
   );
 };

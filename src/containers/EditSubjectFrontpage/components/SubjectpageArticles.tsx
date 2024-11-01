314e0b5a13e5b03b7f1361f371dc236beb671fbb
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-01 16:16:42 +0100

    refactor: use primitives in SubjectPageArticles

--- src/containers/EditSubjectFrontpage/components/SubjectpageArticles.tsx
+++ src/containers/EditSubjectFrontpage/components/SubjectpageArticles.tsx
@@ -10,19 +10,23 @@ import { useField, useFormikContext } from "formik";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { DragVertical } from "@ndla/icons/editor";
+import { ComboboxLabel } from "@ndla/primitives";
 import { styled } from "@ndla/styled-system/jsx";
 import { IArticle } from "@ndla/types-backend/draft-api";
 import { ILearningPathV2 } from "@ndla/types-backend/learningpath-api";
 import { IMultiSearchSummary } from "@ndla/types-backend/search-api";
+import { GenericComboboxInput, GenericComboboxItemContent } from "../../../components/abstractions/Combobox";
 import DndList from "../../../components/DndList";
 import { DragHandle } from "../../../components/DraggableItem";
-import FieldHeader from "../../../components/Field/FieldHeader";
+import { GenericSearchCombobox } from "../../../components/Form/GenericSearchCombobox";
 import ListResource from "../../../components/Form/ListResource";
+import { FormContent } from "../../../components/FormikForm";
 import { fetchDraft } from "../../../modules/draft/draftApi";
+import { useMoviesQuerySearch } from "../../../modules/frontpage/filmQueries";
 import { fetchLearningpath } from "../../../modules/learningpath/learningpathApi";
 import handleError from "../../../util/handleError";
 import { routes, toLearningpathFull } from "../../../util/routeHelpers";
-import DropdownSearch from "../../NdlaFilm/components/DropdownSearch";
+import { usePaginatedQuery } from "../../../util/usePaginatedQuery";
 
 const StyledList = styled("ul", {
   base: {
@@ -44,19 +48,37 @@ const getSubject = (elementId: string) => {
 };
 
 const SubjectpageArticles = ({ editorsChoices, elementId, fieldName }: Props) => {
+  const { query, delayedQuery, setQuery, page, setPage } = usePaginatedQuery();
   const { i18n, t } = useTranslation();
   const [resources, setResources] = useState<(IArticle | ILearningPathV2)[]>(editorsChoices);
   const { setFieldTouched } = useFormikContext();
   const [fieldInputProps] = useField<(IArticle | ILearningPathV2)[]>(fieldName);
   const subjectId = getSubject(elementId);
 
+  const searchQuery = useMoviesQuerySearch(
+    {
+      page,
+      subjects: subjectId,
+      sort: "-relevance",
+      "page-size": 10,
+      query: delayedQuery,
+    },
+    { placeholderData: (prev) => prev },
+  );
+
   const onAddResultToList = async (result: IMultiSearchSummary) => {
     try {
-      const f = result.learningResourceType === "learningpath" ? fetchLearningpath : fetchDraft;
-      const newResource = await f(result.id);
-      const updatedResource = [...resources, { ...newResource, metaImage: result.metaImage }];
-      setResources(updatedResource);
-      updateFormik(updatedResource);
+      if (resources.some((r) => r.id === result.id)) {
+        const newResources = resources.filter((r) => r.id !== result.id);
+        setResources(newResources);
+        updateFormik(newResources);
+      } else {
+        const f = result.learningResourceType === "learningpath" ? fetchLearningpath : fetchDraft;
+        const newResource = await f(result.id);
+        const newResources = resources.concat({ ...newResource, metaImage: result.metaImage });
+        setResources(newResources);
+        updateFormik(newResources);
+      }
     } catch (e) {
       handleError(e);
     }
@@ -83,8 +105,32 @@ const SubjectpageArticles = ({ editorsChoices, elementId, fieldName }: Props) =>
   };
 
   return (
-    <>
-      <FieldHeader title={t("subjectpageForm.editorsChoices")} subTitle={t("subjectpageForm.articles")} />
+    <FormContent>
+      <GenericSearchCombobox
+        value={resources.map((r) => r.id.toString())}
+        closeOnSelect={false}
+        selectionBehavior="preserve"
+        onValueChange={(details) => onAddResultToList(details.items[0])}
+        items={searchQuery.data?.results ?? []}
+        itemToString={(item) => item.title.title}
+        itemToValue={(item) => item.id.toString()}
+        inputValue={query}
+        onInputValueChange={(details) => setQuery(details.inputValue)}
+        isSuccess={searchQuery.isSuccess}
+        paginationData={searchQuery.data}
+        onPageChange={(details) => setPage(details.page)}
+        renderItem={(item) => (
+          <GenericComboboxItemContent
+            title={item.title.title}
+            description={item.metaDescription.metaDescription}
+            image={item.metaImage}
+            useFallbackImage
+          />
+        )}
+      >
+        <ComboboxLabel>{t("subjectpageForm.editorsChoices")}</ComboboxLabel>
+        <GenericComboboxInput placeholder={t("subjectpageForm.addArticle")} isFetching={searchQuery.isFetching} />
+      </GenericSearchCombobox>
       <StyledList>
         <DndList
           data-testid="editors-choices-article-list"
@@ -112,15 +158,7 @@ const SubjectpageArticles = ({ editorsChoices, elementId, fieldName }: Props) =>
           onDragEnd={(_, newArray) => onUpdateElements(newArray)}
         />
       </StyledList>
-      <DropdownSearch
-        selectedElements={resources}
-        onClick={(event: Event) => event.stopPropagation()}
-        onChange={(result: IMultiSearchSummary) => onAddResultToList(result)}
-        placeholder={t("subjectpageForm.addArticle")}
-        subjectId={subjectId}
-        clearInputField
-      />
-    </>
+    </FormContent>
   );
 };
 

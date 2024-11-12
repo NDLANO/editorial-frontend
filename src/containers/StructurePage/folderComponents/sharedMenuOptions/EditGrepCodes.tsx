f6e13be4e36f31a65b3f020715e364240633e1d1
Katrine Wist <katrinew0702@gmail.com>
2024-11-12 13:30:24 +0100

    Refactor EditGrepCodes

--- src/containers/StructurePage/folderComponents/sharedMenuOptions/EditGrepCodes.tsx
+++ src/containers/StructurePage/folderComponents/sharedMenuOptions/EditGrepCodes.tsx
@@ -6,128 +6,210 @@
  *
  */
 
-import { useState } from "react";
+import { useState, KeyboardEvent, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import styled from "@emotion/styled";
-import { ButtonV2 } from "@ndla/button";
-import { spacing } from "@ndla/core";
 import { AddLine, PencilFill, DeleteBinLine } from "@ndla/icons/action";
-import { IconButton } from "@ndla/primitives";
+import { CheckLine } from "@ndla/icons/editor";
+import { Text, Button, IconButton, Input, Spinner } from "@ndla/primitives";
+import { styled } from "@ndla/styled-system/jsx";
 import { Node } from "@ndla/types-taxonomy";
 import MenuItemButton from "./components/MenuItemButton";
-import MenuItemEditField from "./components/MenuItemEditField";
 import RoundIcon from "../../../../components/RoundIcon";
-import Spinner from "../../../../components/Spinner";
-import { useGrepCodes } from "../../../../modules/grep/grepQueries";
+import { fetchGrepCodeTitle } from "../../../../modules/grep/grepApi";
+import { GrepCode } from "../../../../modules/grep/grepApiInterfaces";
 import { useUpdateNodeMetadataMutation } from "../../../../modules/nodes/nodeMutations";
 import { getRootIdForNode, isRootNode } from "../../../../modules/nodes/nodeUtil";
+import { isGrepCodeValid } from "../../../../util/articleUtil";
+import handleError from "../../../../util/handleError";
+import { convertGrepCodesToObject } from "../../../FormikForm/GrepCodesField";
 import { useTaxonomyVersion } from "../../../StructureVersion/TaxonomyVersionProvider";
 import { EditModeHandler } from "../SettingsMenuDropdownType";
+
+const GrepCodesList = styled("ul", {
+  base: {
+    listStyle: "none",
+    display: "flex",
+    flexDirection: "column",
+    gap: "3xsmall",
+  },
+});
+
+const GrepCodeListItem = styled("li", {
+  base: {
+    display: "flex",
+    justifyContent: "space-between",
+    gap: "3xsmall",
+  },
+});
+
+const StyledButton = styled(Button, {
+  base: { alignSelf: "flex-start" },
+});
+
+const StyledMenuItemEditField = styled("div", {
+  base: {
+    display: "flex",
+    gap: "3xsmall",
+  },
+});
 
 interface Props {
   editModeHandler: EditModeHandler;
   node: Node;
 }
 
-export const DropDownWrapper = styled("div")`
-  font-size: 0.9rem;
-  background-color: white;
-  padding: calc(${spacing.small} / 2);
-`;
-
-const StyledGrepItem = styled("div")`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: calc(var(--spacing--small) / 2);
-`;
-
 const EditGrepCodes = ({ node, editModeHandler: { editMode, toggleEditMode } }: Props) => {
   const rootId = getRootIdForNode(node);
   const { t } = useTranslation();
   const { id, metadata } = node;
-  const [grepCodes, setGrepCodes] = useState<string[]>(metadata?.grepCodes ?? []);
+  const [grepCodes, setGrepCodes] = useState<Record<string, string>>({});
+  const [status, setStatus] = useState("initial");
+  const [input, setInput] = useState<string>("");
   const [addingNewGrepCode, setAddingNewGrepCode] = useState(false);
   const { taxonomyVersion } = useTaxonomyVersion();
   const { mutateAsync: patchMetadata } = useUpdateNodeMetadataMutation();
-  const grepCodesWithName = useGrepCodes(grepCodes, editMode === "editGrepCodes");
+
+  useEffect(() => {
+    (async () => {
+      const grepCodesObject = await convertGrepCodesToObject(metadata?.grepCodes ?? []);
+      setGrepCodes(grepCodesObject);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const updateMetadata = async (codes: string[]) => {
-    await patchMetadata({
-      id,
-      metadata: { grepCodes: codes, visible: metadata.visible },
-      rootId: isRootNode(node) ? undefined : rootId,
-      taxonomyVersion,
-    });
-    setGrepCodes(codes);
+    try {
+      await patchMetadata({
+        id,
+        metadata: { grepCodes: codes, visible: metadata.visible },
+        rootId: isRootNode(node) ? undefined : rootId,
+        taxonomyVersion,
+      });
+      setStatus("success");
+    } catch (e) {
+      handleError(e);
+      setStatus("error");
+    }
   };
 
-  const toggleEditModes = () => toggleEditMode("editGrepCodes");
+  const fetchGrepCodeTitles = async (grepCode: string): Promise<GrepCode | undefined> => {
+    if (!isGrepCodeValid(grepCode) || grepCodes[grepCode]) {
+      setStatus("error");
+      return;
+    }
+    const grepCodeTitle = await fetchGrepCodeTitle(grepCode);
+    const isGrepCodeSaved = grepCodes[grepCode];
 
-  const addGrepCode = async (code: string) => updateMetadata([...grepCodes, code.toUpperCase()]);
+    if (grepCodeTitle && !isGrepCodeSaved) {
+      return {
+        code: grepCode,
+        title: `${grepCode} - ${grepCodeTitle}`,
+      };
+    } else if (!isGrepCodeSaved) {
+      setStatus("error");
+    }
+  };
+  const addGrepCode = async (newValue: string) => {
+    setStatus("loading");
+    const trimmedValue = newValue.toUpperCase().trim();
+    const grepCodeWithName = await fetchGrepCodeTitles(trimmedValue);
+    if (!grepCodeWithName) return;
+    setGrepCodes((prev) => ({ ...prev, [grepCodeWithName.code]: grepCodeWithName.title }));
+    await updateMetadata([...Object.keys(grepCodes), grepCodeWithName.code]);
+    setInput("");
+    setAddingNewGrepCode(!addingNewGrepCode);
+  };
 
-  const deleteGrepCode = (code: string) => updateMetadata(grepCodes.filter((c) => c !== code));
+  const handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      setStatus("initial");
+    }
+    if (e.key === "Enter") {
+      addGrepCode(input);
+    }
+  };
 
-  const grepCodesList = (
-    <DropDownWrapper>
-      {grepCodesWithName?.length > 0 ? (
-        grepCodesWithName.map((grepCode, index) => {
-          if (grepCode.isLoading) {
-            return <Spinner key={index} />;
-          }
-          if (!grepCode.data) {
-            return null;
-          }
-          return (
-            <StyledGrepItem key={index}>
-              {grepCode.data.title}
-              <IconButton
-                variant="danger"
-                size="small"
-                aria-label={t("taxonomy.grepCodes.delete", {
-                  grepCode: grepCode.data.title,
-                })}
-                title={t("taxonomy.grepCodes.delete", {
-                  grepCode: grepCode.data.title,
-                })}
-                data-testid="deleteGrepCode"
-                onClick={() => deleteGrepCode(grepCode.data.code)}
-              >
-                <DeleteBinLine />
-              </IconButton>
-            </StyledGrepItem>
-          );
-        })
-      ) : (
-        <p>{t("taxonomy.grepCodes.empty")}</p>
-      )}
+  const deleteGrepCode = async (code: string) => {
+    setStatus("loading");
+    const { [code]: _, ...remaining } = grepCodes;
+    await updateMetadata(Object.keys(remaining));
+    setGrepCodes(remaining);
+  };
 
-      {addingNewGrepCode ? (
-        <MenuItemEditField
-          currentVal=""
-          messages={{ errorMessage: t("taxonomy.errorMessage") }}
-          dataTestid="addGrepCopde"
-          onClose={() => setAddingNewGrepCode(!addingNewGrepCode)}
-          onSubmit={addGrepCode}
-          icon={<PencilFill />}
-          placeholder={t("form.grepCodes.placeholder")}
-        />
-      ) : (
-        <ButtonV2 variant="link" data-testid="addFilterButton" onClick={() => setAddingNewGrepCode(!addingNewGrepCode)}>
-          <AddLine />
-          {t("taxonomy.grepCodes.addNew")}
-        </ButtonV2>
-      )}
-    </DropDownWrapper>
-  );
+  const grepCodesList = Object.entries(grepCodes);
 
   return (
     <>
-      <MenuItemButton data-testid="editGrepCodes" onClick={() => toggleEditModes()}>
+      <MenuItemButton data-testid="editGrepCodes" onClick={() => toggleEditMode("editGrepCodes")}>
         <RoundIcon small icon={<PencilFill />} />
         {t("taxonomy.grepCodes.edit")}
+        {status === "loading" && <Spinner size="small" />}
       </MenuItemButton>
-      {editMode === "editGrepCodes" && grepCodesList}
+      {editMode === "editGrepCodes" && (
+        <GrepCodesList>
+          {grepCodesList.length ? (
+            grepCodesList.map(([code, title]) => (
+              <GrepCodeListItem key={code}>
+                <Text>{title}</Text>
+                <IconButton
+                  variant="danger"
+                  size="small"
+                  aria-label={t("taxonomy.grepCodes.delete", {
+                    grepCode: title,
+                  })}
+                  title={t("taxonomy.grepCodes.delete", {
+                    grepCode: title,
+                  })}
+                  data-testid="deleteGrepCode"
+                  onClick={() => deleteGrepCode(code)}
+                  disabled={status === "loading"}
+                  loadingContent={status === "loading"}
+                >
+                  <DeleteBinLine />
+                </IconButton>
+              </GrepCodeListItem>
+            ))
+          ) : (
+            <Text>{t("taxonomy.grepCodes.empty")}</Text>
+          )}
+          {addingNewGrepCode ? (
+            <>
+              <StyledMenuItemEditField>
+                <Input
+                  type="text"
+                  componentSize="small"
+                  placeholder={t("form.grepCodes.placeholder")}
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  onKeyDown={handleKeyPress}
+                />
+                <IconButton
+                  size="small"
+                  data-testid="inlineEditSaveButton"
+                  onClick={() => addGrepCode(input)}
+                  title={t("save")}
+                  aria-label={t("save")}
+                  disabled={status === "loading"}
+                  loadingContent={status === "loading"}
+                >
+                  <CheckLine />
+                </IconButton>
+              </StyledMenuItemEditField>
+              {status === "error" && <Text color="text.error">{t("taxonomy.errorMessage")}</Text>}
+            </>
+          ) : (
+            <StyledButton
+              variant="secondary"
+              size="small"
+              data-testid="addFilterButton"
+              onClick={() => setAddingNewGrepCode(!addingNewGrepCode)}
+            >
+              <AddLine />
+              {t("taxonomy.grepCodes.addNew")}
+            </StyledButton>
+          )}
+        </GrepCodesList>
+      )}
     </>
   );
 };

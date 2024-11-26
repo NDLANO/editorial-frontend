729766ce7ac76b3ba947c5f556fbcfdf2c4a1f30
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-26 15:54:17 +0100

    chore: simplify expression. This already checks for falsy

--- src/containers/Masthead/components/MastheadSearch.tsx
+++ src/containers/Masthead/components/MastheadSearch.tsx
@@ -112,7 +112,7 @@ export const MastheadSearch = () => {
     if (
       !urlId.includes("urn:topic") &&
       Number.isNaN(parseFloat(urlId)) &&
-      !splittedNdlaUrl.find((e) => e.match(/subject:*/)) === undefined
+      !splittedNdlaUrl.find((e) => e.match(/subject:*/))
     ) {
       return;
     }

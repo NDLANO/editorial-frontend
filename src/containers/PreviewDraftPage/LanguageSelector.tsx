cd157c9066bfc4211855ba2a3f66b5675b8dc1e5
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-28 13:16:17 +0100

    refactor: use primitives for preview language selector

--- src/containers/PreviewDraftPage/LanguageSelector.tsx
+++ src/containers/PreviewDraftPage/LanguageSelector.tsx
@@ -6,36 +6,67 @@
  *
  */
 
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
-import styled from "@emotion/styled";
-import { toPreviewDraft } from "../../util/routeHelpers";
-
-const StyledSelect = styled.select`
-  margin: 0 auto;
-  width: 100%;
-  display: block;
-`;
+import { createListCollection } from "@ark-ui/react";
+import { SelectContent, SelectLabel, SelectRoot, SelectValueText } from "@ndla/primitives";
+import { styled } from "@ndla/styled-system/jsx";
+import { GenericSelectItem, GenericSelectTrigger } from "../../components/abstractions/Select";
+import { routes } from "../../util/routeHelpers";
 
 interface Props {
   supportedLanguages: string[];
 }
 
+const StyledGenericSelectTrigger = styled(GenericSelectTrigger, {
+  base: {
+    width: "100%",
+  },
+});
+
+const StyledSelectContent = styled(SelectContent, {
+  base: {
+    color: "text.default",
+  },
+});
+
 const LanguageSelector = ({ supportedLanguages }: Props) => {
   const { t } = useTranslation();
   const { draftId, language } = useParams<"draftId" | "language">();
   const navigate = useNavigate();
-  if (supportedLanguages.length === 0) {
+
+  const collection = useMemo(() => {
+    return createListCollection({
+      items: supportedLanguages,
+      itemToValue: (item) => item,
+      itemToString: (item) => t(`languages.${item}`),
+    });
+  }, [supportedLanguages, t]);
+
+  if (!collection.items.length) {
     return null;
   }
+
   return (
-    <StyledSelect onChange={(evt) => navigate(toPreviewDraft(Number(draftId), evt.target.value))} value={language}>
-      {supportedLanguages.map((supportedLanguage) => (
-        <option key={supportedLanguage} value={supportedLanguage}>
-          {t(`languages.${supportedLanguage}`)}
-        </option>
-      ))}
-    </StyledSelect>
+    <SelectRoot
+      collection={collection}
+      value={language ? [language] : undefined}
+      onValueChange={(details) => navigate(routes.preview.draft(Number(draftId), details.value[0]))}
+      positioning={{ sameWidth: true }}
+    >
+      <SelectLabel>{t("languages.prefixChangeLanguage")}</SelectLabel>
+      <StyledGenericSelectTrigger>
+        <SelectValueText />
+      </StyledGenericSelectTrigger>
+      <StyledSelectContent>
+        {collection.items.map((item) => (
+          <GenericSelectItem key={item} item={item}>
+            {t(`languages.${item}`)}
+          </GenericSelectItem>
+        ))}
+      </StyledSelectContent>
+    </SelectRoot>
   );
 };
 

bbb0bfb2ce898d67a65e8480d6a3e37f7c819bb8
Gunnar Velle <gunnar.velle@gmail.com>
2024-12-17 14:24:52 +0100

    Supports linking to new short url in link-plugin. Use pretty urls for preview

--- src/components/SlateEditor/plugins/link/utils.ts
+++ src/components/SlateEditor/plugins/link/utils.ts
@@ -9,6 +9,7 @@
 import { Editor, Element, Range, Transforms } from "slate";
 import { jsx as slatejsx } from "slate-hyperscript";
 import { isValidLocale } from "../../../../i18n";
+import { fetchNodes } from "../../../../modules/nodes/nodeApi";
 import { resolveUrls } from "../../../../modules/taxonomy/taxonomyApi";
 
 export const insertLink = (editor: Editor) => {
@@ -90,6 +91,26 @@ export const splitTaxonomyUrl = async (href: string) => {
   return { resourceId, resourceType };
 };
 
+export const splitTaxonomyContextUrl = async (href: string) => {
+  const { pathname } = new URL(href);
+  const paths = pathname.split("/");
+  const language = isValidLocale(paths[1]) ? paths[1] : "nb";
+  const contextId = paths.at(-1);
+  const nodes = await fetchNodes({
+    contextId,
+    language,
+    taxonomyVersion: "default",
+  });
+  if (!nodes.length) {
+    return { resourceId: null, resourceType: null };
+  }
+
+  const contentUriSplit = nodes[0].contentUri?.split(":") ?? [];
+  const resourceId = contentUriSplit.pop();
+  const resourceType = contentUriSplit.pop();
+  return { resourceId, resourceType };
+};
+
 export const splitEdPathUrl = (href: string) => {
   const id = href.split("subject-matter/")[1].split("/")[1];
   return {
@@ -109,6 +130,8 @@ export const splitEdPreviewUrl = (href: string) => {
 export const isNDLAArticleUrl = (url: string) => /^http(s)?:\/\/((.*)\.)?ndla.no\/((.*)\/)?article\/\d*/.test(url);
 export const isNDLATaxonomyUrl = (url: string) =>
   /^http(s)?:\/\/((.*)\.)?ndla.no\/((.*)\/)?subject:(.*)\/topic(.*)/.test(url);
+export const isNDLATaxonomyContextUrl = (url: string) =>
+  /^http(s)?:\/\/((.*)\.)?ndla.no\/((.*)\/)?(f|e|r)\/(.*)\/?(.*)\/?[a-z0-9]{10}/.test(url);
 export const isNDLALearningPathUrl = (url: string) =>
   /^http(s)?:\/\/((.*)\.)?ndla.no\/((.*)\/)?learningpaths\/(.*)/.test(url);
 export const isNDLAEdPathUrl = (url: string) =>
@@ -128,6 +151,8 @@ export const getIdAndTypeFromUrl = async (href: string) => {
     return splitPlainUrl(baseHref);
   } else if (isNDLATaxonomyUrl(baseHref)) {
     return await splitTaxonomyUrl(baseHref);
+  } else if (isNDLATaxonomyContextUrl(baseHref)) {
+    return await splitTaxonomyContextUrl(baseHref);
   } else if (isNDLAEdPathUrl(baseHref)) {
     return splitEdPathUrl(baseHref);
   } else if (isNDLAEdPreviewUrl(baseHref)) {

24e883510da029135e3ed2e8dc62271f5cee3101
Jonas Natten <jonas.natten@knowit.no>
2025-04-08 14:55:51 +0200

    refactor: Use `openapi-fetch` in `articleApi.ts`

--- src/containers/FrontpageEditPage/FrontpageEditPage.tsx
+++ src/containers/FrontpageEditPage/FrontpageEditPage.tsx
@@ -74,10 +74,7 @@ const FrontpageEditPage = () => {
     [frontpageQuery.data],
   );
 
-  const articlesQuery = useArticleSearch(
-    { ids: articleIds.join(","), license: "all" },
-    { enabled: !!articleIds.length },
-  );
+  const articlesQuery = useArticleSearch({ ids: articleIds, license: "all" }, { enabled: !!articleIds.length });
 
   const transformedMenu: MenuWithArticle | undefined = useMemo(() => {
     if (frontpageQuery.isLoading || articlesQuery.isLoading || !articlesQuery.data) {

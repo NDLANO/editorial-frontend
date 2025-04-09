cf6841caa17075da1c480779f00a589baf725700
Jonas Natten <jonas.natten@knowit.no>
2025-04-09 06:55:34 +0200

    refactor: Use `openapi-fetch` in `audioApi.ts`

--- src/modules/audio/audioApi.ts
+++ src/modules/audio/audioApi.ts
@@ -16,94 +16,143 @@ import {
   ISeriesSearchParamsDTO,
   ISearchParamsDTO,
   ITranscriptionResultDTO,
+  openapi,
+  INewAudioMetaInformationDTO,
+  UpdatedAudioMetaInformationDTO,
 } from "@ndla/types-backend/audio-api";
 import { StringSort } from "../../containers/SearchPage/components/form/SearchForm";
-import { apiResourceUrl, fetchAuthorized, resolveJsonOrRejectWithError } from "../../util/apiHelpers";
-import {
-  resolveJsonOrVoidOrRejectWithError,
-  resolveTextOrRejectWithError,
-} from "../../util/resolveJsonOrRejectWithError";
+import { createAuthClient } from "../../util/apiHelpers";
+import { resolveJsonOATS } from "../../util/resolveJsonOrRejectWithError";
+import { createFormData } from "../../util/formDataHelper";
 
-const baseUrl = apiResourceUrl("/audio-api/v1/audio");
-const seriesBaseUrl = apiResourceUrl("/audio-api/v1/series");
-const transcribeUrl = apiResourceUrl("/audio-api/v1/transcription");
+const client = createAuthClient<openapi.paths>();
 
-export const postAudio = (formData: FormData): Promise<IAudioMetaInformationDTO> =>
-  fetchAuthorized(`${baseUrl}`, {
-    method: "POST",
-    headers: { "Content-Type": undefined }, // Without this we're missing a boundary: https://stackoverflow.com/questions/39280438/fetch-missing-boundary-in-multipart-form-data-post
-    body: formData,
-  }).then((r) => resolveJsonOrRejectWithError<IAudioMetaInformationDTO>(r));
+export const postAudio = (metadata: INewAudioMetaInformationDTO, file: Blob): Promise<IAudioMetaInformationDTO> =>
+  client
+    .POST("/audio-api/v1/audio", {
+      body: {
+        metadata,
+        file,
+      },
+      bodySerializer(body) {
+        return createFormData(body.file, body.metadata);
+      },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const fetchAudio = (id: number, locale?: string): Promise<IAudioMetaInformationDTO> => {
-  const languageParam = locale ? `?language=${locale}&fallback=true` : "";
-  return fetchAuthorized(`${baseUrl}/${id}${languageParam}`).then((r) =>
-    resolveJsonOrRejectWithError<IAudioMetaInformationDTO>(r),
-  );
-};
+export const fetchAudio = async (id: number, locale?: string): Promise<IAudioMetaInformationDTO> =>
+  client
+    .GET("/audio-api/v1/audio/{audio-id}", {
+      params: {
+        path: {
+          "audio-id": id,
+        },
+        query: {
+          language: locale,
+          fallback: true,
+        },
+      },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const updateAudio = (id: number, formData: FormData): Promise<IAudioMetaInformationDTO> =>
-  fetchAuthorized(`${baseUrl}/${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": undefined }, // Without this we're missing a boundary: https://stackoverflow.com/questions/39280438/fetch-missing-boundary-in-multipart-form-data-post
-    body: formData,
-  }).then((r) => resolveJsonOrRejectWithError<IAudioMetaInformationDTO>(r));
+export const updateAudio = async (
+  id: number,
+  metadata: UpdatedAudioMetaInformationDTO,
+  file: Blob | undefined,
+): Promise<IAudioMetaInformationDTO> =>
+  client
+    .PUT("/audio-api/v1/audio/{audio-id}", {
+      params: {
+        path: {
+          "audio-id": id,
+        },
+      },
+      body: {
+        metadata,
+        file,
+      },
+      bodySerializer(body) {
+        return createFormData(body.file, body.metadata);
+      },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const postSearchAudio = async (body: StringSort<ISearchParamsDTO>): Promise<IAudioSummarySearchResultDTO> => {
-  const response = await fetchAuthorized(`${baseUrl}/search/`, { method: "POST", body: JSON.stringify(body) });
-  return resolveJsonOrRejectWithError(response);
-};
+export const postSearchAudio = async (body: StringSort<ISearchParamsDTO>): Promise<IAudioSummarySearchResultDTO> =>
+  client
+    .POST("/audio-api/v1/audio/search", {
+      body: {
+        ...body,
+        // @ts-expect-error TODO: API's use different sorting types and we share them in the frontend
+        sort: body.sort,
+      },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const deleteLanguageVersionAudio = (audioId: number, locale: string): Promise<IAudioMetaInformationDTO | void> =>
-  fetchAuthorized(`${baseUrl}/${audioId}/language/${locale}`, {
-    method: "DELETE",
-  }).then((r) => resolveJsonOrVoidOrRejectWithError(r));
+export const deleteLanguageVersionAudio = async (
+  audioId: number,
+  locale: string,
+): Promise<IAudioMetaInformationDTO | void> =>
+  client
+    .DELETE("/audio-api/v1/audio/{audio-id}/language/{language}", {
+      params: { path: { "audio-id": audioId, language: locale } },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const deleteLanguageVersionSeries = (seriesId: number, language: string): Promise<ISeriesDTO | void> => {
-  return fetchAuthorized(`${seriesBaseUrl}/${seriesId}/language/${language}`, {
-    method: "DELETE",
-  }).then((r) => resolveJsonOrVoidOrRejectWithError(r));
-};
+export const deleteLanguageVersionSeries = async (seriesId: number, language: string): Promise<ISeriesDTO | void> =>
+  client
+    .DELETE("/audio-api/v1/series/{series-id}/language/{language}", {
+      params: { path: { "series-id": seriesId, language } },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const fetchSearchTags = async (input: string, language: string): Promise<ITagsSearchResultDTO> => {
-  const response = await fetchAuthorized(`${baseUrl}/tag-search/?language=${language}&query=${input}`);
-  return resolveJsonOrRejectWithError(response);
-};
+export const fetchSearchTags = async (query: string, language: string): Promise<ITagsSearchResultDTO> =>
+  client
+    .GET("/audio-api/v1/audio/tag-search", {
+      params: { query: { language, query } },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const fetchSeries = (id: number, language?: string): Promise<ISeriesDTO> => {
-  const languageParam = language ? `?language=${language}` : "";
-  return fetchAuthorized(`${seriesBaseUrl}/${id}${languageParam}`).then((r) =>
-    resolveJsonOrRejectWithError<ISeriesDTO>(r),
-  );
-};
+export const fetchSeries = async (id: number, language?: string): Promise<ISeriesDTO> =>
+  client
+    .GET("/audio-api/v1/series/{series-id}", {
+      params: { path: { "series-id": id }, query: { language } },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const postSeries = (newSeries: INewSeriesDTO): Promise<ISeriesDTO> =>
-  fetchAuthorized(`${seriesBaseUrl}`, {
-    method: "POST",
-    body: JSON.stringify(newSeries),
-  }).then((r) => resolveJsonOrRejectWithError<ISeriesDTO>(r));
+export const postSeries = async (newSeries: INewSeriesDTO): Promise<ISeriesDTO> =>
+  client.POST("/audio-api/v1/series", { body: newSeries }).then((r) => resolveJsonOATS(r));
 
 export const updateSeries = (id: number, newSeries: INewSeriesDTO): Promise<ISeriesDTO> =>
-  fetchAuthorized(`${seriesBaseUrl}/${id}`, {
-    method: "PUT",
-    body: JSON.stringify(newSeries),
-  }).then((r) => resolveJsonOrRejectWithError<ISeriesDTO>(r));
+  client
+    .PUT("/audio-api/v1/series/{series-id}", {
+      params: { path: { "series-id": id } },
+      body: newSeries,
+    })
+    .then((r) => resolveJsonOATS(r));
 
 export const postSearchSeries = async (
   body: StringSort<ISeriesSearchParamsDTO>,
-): Promise<ISeriesSummarySearchResultDTO> => {
-  const response = await fetchAuthorized(`${seriesBaseUrl}/search/`, { method: "POST", body: JSON.stringify(body) });
-  return resolveJsonOrRejectWithError(response);
-};
+): Promise<ISeriesSummarySearchResultDTO> =>
+  client
+    .POST("/audio-api/v1/series/search", {
+      body: {
+        ...body,
+        // @ts-expect-error TODO: API's use different sorting types and we share them in the frontend
+        sort: body.sort,
+      },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const postAudioTranscription = async (audioName: string, audioId: number, language: string): Promise<string> => {
-  const response = await fetchAuthorized(`${transcribeUrl}/audio/${audioName}/${audioId}/${language}`, {
-    method: "POST",
-  });
-  return resolveTextOrRejectWithError(response);
-};
+export const postAudioTranscription = async (audioName: string, audioId: number, language: string): Promise<string> =>
+  client
+    .POST("/audio-api/v1/transcription/audio/{audioName}/{audioId}/{language}", {
+      params: { path: { audioName, audioId, language } },
+    })
+    .then((r) => resolveJsonOATS(r));
 
-export const fetchAudioTranscription = async (audioId: number, language: string): Promise<ITranscriptionResultDTO> => {
-  const response = await fetchAuthorized(`${transcribeUrl}/audio/${audioId}/${language}`, { method: "GET" });
-  return resolveJsonOrRejectWithError<ITranscriptionResultDTO>(response);
-};
+export const fetchAudioTranscription = async (audioId: number, language: string): Promise<ITranscriptionResultDTO> =>
+  client
+    .GET("/audio-api/v1/transcription/audio/{audioId}/{language}", {
+      params: { path: { audioId, language } },
+    })
+    .then((r) => resolveJsonOATS(r));

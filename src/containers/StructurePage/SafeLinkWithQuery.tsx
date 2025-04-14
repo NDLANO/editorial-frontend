0576fb4bc16d97286f26c793d6db1b35145e2bc4
Katrine Wist <katrinew0702@gmail.com>
2025-04-14 15:25:57 +0200

    Add search param to enable matomo

---
+++ src/containers/StructurePage/SafeLinkWithQuery.tsx
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2025-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { useLocation } from "react-router-dom";
+import { SafeLink, SafeLinkProps } from "@ndla/safelink";
+
+export const SafeLinkWithQuery = ({ children, to, ...props }: SafeLinkProps) => {
+  const { search } = useLocation();
+
+  return (
+    <SafeLink to={to + search} {...props}>
+      {children}
+    </SafeLink>
+  );
+};

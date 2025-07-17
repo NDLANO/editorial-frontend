fb7f3efaf1ad6a9a9f548ffdb7e60b0893825100
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-07-17 09:17:54 +0200

    add specific youtube support

--- src/server/api.ts
+++ src/server/api.ts
@@ -11,6 +11,7 @@ import { GetVerificationKey, expressjwt as jwt, Request } from "express-jwt";
 import jwksRsa from "jwks-rsa";
 import prettier from "prettier";
 import openGraph from "open-graph-scraper";
+import { youtube } from "@googleapis/youtube";
 import { getToken, getBrightcoveToken, fetchAuth0UsersById, getEditors, getResponsibles } from "./auth";
 import { OK, INTERNAL_SERVER_ERROR, NOT_ACCEPTABLE, FORBIDDEN, BAD_REQUEST, NOT_FOUND, FOUND } from "./httpCodes";
 import errorLogger from "./logger";
@@ -22,6 +23,8 @@ import { fetchMatomoStats } from "./matomo";
 import { generateAnswer, getDefaultPrompts, getTranscription, initializeTranscription } from "./llm";
 import { isValidRequestBody } from "./utils";
 import { isLlmLanguageCode } from "./llmTypes";
+
+const googleApiKey = getEnvironmentVariabel("NDLA_GOOGLE_API_KEY");
 
 const router = express.Router();
 
@@ -277,6 +280,8 @@ router.get("/transcribe/:jobName", jwtMiddleware, aiMiddleware, async (req, res)
   }
 });
 
+const youtubeApi = youtube({ version: "v3", auth: googleApiKey });
+
 // TODO: Consider adding specific YouTube handling like we do in GraphQL.
 router.get("/opengraph", jwtMiddleware, async (req, res) => {
   const { url } = req.query;
@@ -284,10 +289,27 @@ router.get("/opengraph", jwtMiddleware, async (req, res) => {
     res.status(BAD_REQUEST).send({ error: "Missing 'url' query parameter" });
     return;
   }
+  let urlObj: URL;
   try {
-    new URL(url);
+    urlObj = new URL(url);
   } catch (e) {
     res.status(BAD_REQUEST).send({ error: "Invalid 'url' query parameter" });
+    return;
+  }
+  if (url.includes("youtu")) {
+    const videoId = urlObj.pathname.startsWith("/watch")
+      ? urlObj.searchParams.get("v")
+      : urlObj.pathname.split("/")?.[1]?.split("?")[0];
+    const yt_metadata = await youtubeApi.videos.list({ id: [videoId ?? ""], part: ["snippet"] });
+    const data = yt_metadata.data.items?.[0]?.snippet;
+    const json = {
+      title: data?.title ?? undefined,
+      description: data?.description ?? undefined,
+      imageUrl: data?.thumbnails?.default?.url ?? undefined,
+      imageAlt: data?.title ?? undefined,
+      url,
+    };
+    res.status(OK).json(json);
     return;
   }
   const ogs = await openGraph({ url });

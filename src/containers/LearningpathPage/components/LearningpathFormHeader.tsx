3cb1e058e7e7d9ab9357df8711b186216c916d86
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-07-18 15:10:30 +0200

    Add more info to header

--- src/containers/LearningpathPage/components/LearningpathFormHeader.tsx
+++ src/containers/LearningpathPage/components/LearningpathFormHeader.tsx
@@ -15,12 +15,16 @@ import { ContentTypeBadge } from "@ndla/ui";
 import HeaderActions from "../../../components/HeaderWithLanguage/HeaderActions";
 import { HeaderCurrentLanguagePill } from "../../../components/HeaderWithLanguage/HeaderCurrentLanguagePill";
 import HeaderFavoriteStatus from "../../../components/HeaderWithLanguage/HeaderFavoriteStatus";
+import { ResourcePublishedLink } from "../../../components/HeaderWithLanguage/ResourcePublishedLink";
+import { PUBLISHED } from "../../../constants";
 import { usePostCopyLearningpathMutation } from "../../../modules/learningpath/learningpathMutations";
 import { routes } from "../../../util/routeHelpers";
 import {
   FormHeaderHeading,
   FormHeaderHeadingContainer,
+  FormHeaderResponsibleInfo,
   FormHeaderSegment,
+  FormHeaderStatusInfo,
   FormHeaderStatusWrapper,
 } from "../../FormHeader/FormHeader";
 
@@ -34,6 +38,8 @@ export const LearningpathFormHeader = ({ learningpath, language }: Props) => {
   const isNewLanguage = !!learningpath?.id && !learningpath.supportedLanguages.includes(language);
   const cloneLearningpathMutation = usePostCopyLearningpathMutation();
   const navigate = useNavigate();
+  // TODO: I don't know all of the learningpath statuses. We need to ensure we have translations for all of them.
+  const statusText = learningpath?.status ? t(`form.status.${learningpath.status.toLowerCase()}`) : "";
 
   const onClone = useCallback(async () => {
     if (!learningpath) return;
@@ -58,7 +64,17 @@ export const LearningpathFormHeader = ({ learningpath, language }: Props) => {
           )}
         </FormHeaderHeadingContainer>
         <FormHeaderStatusWrapper>
+          {learningpath?.status === PUBLISHED && (
+            <ResourcePublishedLink type="learningpath" slugOrId={learningpath.id} />
+          )}
+          {/* TODO: Revision */}
+          {/* TODO:  taxonomy */}
           <HeaderFavoriteStatus id={learningpath?.id} type="learningpath" />
+          <div>
+            {/* TODO: Add responsible here when supported */}
+            <FormHeaderResponsibleInfo responsibleName={undefined} />
+            <FormHeaderStatusInfo isNewLanguage={isNewLanguage} statusText={statusText} />
+          </div>
         </FormHeaderStatusWrapper>
         {/* TODO: Published status, taxonomy, favorite count, responsible and status */}
       </FormHeaderSegment>

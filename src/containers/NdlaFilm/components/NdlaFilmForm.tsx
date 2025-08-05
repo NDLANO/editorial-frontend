8918870198997d0f8c78a26ffa03b8dc38345419
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-08-05 10:21:39 +0200

    remove some redundant changes

--- src/containers/NdlaFilm/components/NdlaFilmForm.tsx
+++ src/containers/NdlaFilm/components/NdlaFilmForm.tsx
@@ -113,6 +113,7 @@ const NdlaFilmForm = ({ filmFrontpage, selectedLanguage }: Props) => {
               articleType="filmfrontpage"
               editUrl={(_, lang) => toEditNdlaFilm(lang)}
               id={20}
+              isSubmitting={isSubmitting}
               language={selectedLanguage}
               supportedLanguages={values.supportedLanguages}
               title={values.name}

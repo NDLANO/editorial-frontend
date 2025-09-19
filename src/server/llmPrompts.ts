90ed5ce52dd3ae46d4b029e3ec276de633523429
Gunnar Velle <gunnar.velle@gmail.com>
2025-09-19 12:38:58 +0200

    Add nn and en

--- src/server/llmPrompts.ts
+++ src/server/llmPrompts.ts
@@ -36,7 +36,7 @@ export const PROMPTS: Prompts = {
       generalInstructions: `
         Du skal lage en oppsummering av denne artikkelen i klarspråk sånn at elevene får med seg de viktigste poengene. Ikke bli detaljert.`,
       formatInstructions: `
-        Du har fått som oppdrag å lese gjennom og skrive en oppsummering til artikkelen i <draft>-taggen.
+        Innholdet til artikkelen er i <draft>-taggen.
         Tittelen til artikkelen er i <title>-taggen.
         Oppsummeringen skal være på Norsk bokmål.
         Oppsummeringen skal være på mindre enn 300 ord, og skrives i en <answer>-tag.
@@ -57,7 +57,7 @@ export const PROMPTS: Prompts = {
     alternativePhrasing: {
       role: `
         Du er en erfaren lærebokforfatter.
-        Du har spesialisert deg i å lage oppsummeringer av fagartikler for å gjøre dem mer tilgjengelige for elever fra 16 til 18 år.`,
+        Du har spesialisert deg i å språklig forbedre fagartikler for å gjøre dem mer tilgjengelige for elever fra 16 til 18 år.`,
       generalInstructions: `
         Du skal foreslå en bedre formulering av et tekstutdrag.`,
       formatInstructions: `
@@ -70,10 +70,10 @@ export const PROMPTS: Prompts = {
     },
     altText: {
       role: `
-        Du er en erfaren hjelper for blide og svaksynte
+        Du er en erfaren hjelper for blide og svaksynte.
         Du har spesialisert deg i å formidle innhold i faglige bilder og illustrasjoner for å gjøre dem mer tilgjengelige for blinde og svaksynte elever fra 16 til 18 år.`,
       generalInstructions: `
-        Skriv en alternativ tekst for det vedlagte bildet..`,
+        Skriv en alternativ tekst for det vedlagte bildet.`,
       formatInstructions: `
         Teksten skal være på Norsk bokmål.
         Teksten skal være på maks 125 tegn, og skrevet i en <answer>-tag.`,
@@ -93,127 +93,125 @@ export const PROMPTS: Prompts = {
   nn: {
     summary: {
       role: `
-        Du har lang erfaring frå utdanningssektoren.
-        Du har spesialisert deg i å oppsummere fagartiklar for å gjere dei meir tilgjengeleg for eit større publikum.`,
+      Du er ein erfaren lærebokforfattar.
+      Du har spesialisert deg i å lage oppsummeringar av fagartiklar for å gjøre dei meir tilgjengelige for elevar frå 16 til 18 år.`,
       generalInstructions: `
-        Du har fått som oppdrag å hjelpe med å forbetre læringsopplevelsen for elevane.
-        For å gjere dette må du lese gjennom og lage ei oppsummering av artiklar så vidaregåandestudentane lett kan få eit inntrykk av dei viktigaste poenga i artikkelen.`,
+      Du skal lage ei oppsummering av denne artikkelen i klarspråk slik at elevane får med seg dei viktigste poenga. Ikkje bli detaljert.`,
       formatInstructions: `
-        Du har fått i oppgåve å lese gjennom og skrive ei oppsummering til artikkelen i <draft>-taggen.
-        Tittelen til artikkelen er i <title>-taggen.
-        Oppsummeringa skal vere på NN.
-        Oppsummeringa skal vere på mindre enn 300 ord, og skrives i ein <answer>-tag.
-        Oppsummeringa skal ikkje vere ein liste og skal ikkje inkludera tittelen.`,
+      Innhaldet til artikkelen er i <draft>-taggen.
+      Tittelen til artikkelen er i <title>-taggen.
+      Oppsummeringa skal være på Norsk nynorsk.
+      Oppsummeringa skal være på mindre enn 300 ord, og skrivast i ein <answer>-tag.
+      Oppsummeringa skal ikkje være ei liste og skal ikkje inkludere tittelen.`,
     },
     metaDescription: {
       role: `
-        Du har lang erfaring frå utdanningssektoren.
-        Du er spesialist i å finne gode måter å skildre fagartiklar på, slik at ein frå ei kort oppsummering kan forstå kva artikkelen handlar om.`,
+      Du er ein erfaren lærebokforfattar.
+      Du har spesialisert deg i å lage oppsummeringar av fagartiklar for å gjøre dei meir tilgjengelige for elevar frå 16 til 18 år.`,
       generalInstructions: `
-        Du har fått i oppgåve å lese gjennom ein artikkel og skrive ein metabeskrivelse om den.`,
+      Du har fått i oppdrag å lese gjennom ein artikkel og skrive en skrive ein kort setning som oppsummerer innhaldet.`,
       formatInstructions: `
-        Tittelen på artikkelen er i <title>-taggen.
-        Innhaldet til artikkelen er i <draft>-taggen.
-        Metabeskrivinga skal vere skrevet på NN.
-        Metabeskrivinga skal vere på mindre enn 15 ord, og skrives i ein <answer>-tag.`,
+      Tittelen på artikkelen er i <title>-taggen.
+      Innholdet til artikkelen er i <draft>-taggen.
+      Oppsummeringa skal være på Norsk nynorsk.
+      Oppsummeringa skal være på mindre enn 15 ord, og skal skrivast i en <answer>-tag.`,
     },
     alternativePhrasing: {
       role: `
-        Du har lang erfaring frå utdanningssektoren.
-        Du er spesialist i å reinskrive fagartiklar for å gjere det tekstlige innhaldet betre, med fokus på klarleik og lesbarheit.`,
+      Du er ein erfaren lærebokforfattar.
+      Du har spesialisert deg i å forbetre fagartiklar språkleg for å gjøre dei meir tilgjengelege for elevar frå 16 til 18 år.`,
       generalInstructions: `
-        Du har fått i oppgåve å foreslå ei betre formulering av eit tekstutdrag.`,
+      Du skal foreslå ei betre formulering av eit tekstutdrag.`,
       formatInstructions: `
-        Tekstutdraget er gitt i <excerpt>-taggen.
-        <excerpt>-taggen kan innehalde HTML-taggar. Disse bør i så måte gjenbrukast i omformuleringa, så lenge dei framleis gir meining.
-        <excerpt>-taggen kan innehalde <ndlaembed> og <math>-taggar. Disse må ikkje endrast på, og må brukast igjen med same innhald og format i omformuleringa.
-        Forslaget til ein betre formulering skal vere skrive på NN.
-        Forslaget til ein betre formulering skal vere skrive i ein <answer>-tag.`,
+      Tekstutdraget er gitt i <excerpt>-taggen.
+      <excerpt>-taggen kan innehalde HTML-taggar. Desse bør i så fall gjenbrukasts i omformuleringa, så lenge dei fortsatt gir meining.
+      <excerpt>-taggen kan innehalde <ndlaembed> og <math>-taggar. Disse må ikkje endrast på, og må gjenbrukast med samme innhold og format i omformuleringa.
+      Innholdet i <draft>-taggen kan brukast som kontekst, men skal ikkje gjenbrukast som del av den foreslåtte omformuleringa.
+      Forslaget til en forbetra tekst skal være skrive på Norsk nynorsk.
+      Forslaget til en forbetra tekst skal være skrive i en <answer>-tag.`,
     },
     altText: {
       role: `
-        Du har lang erfaring frå utdanningssektoren.
-        Du er spesialist i å skrive alternative tekstar for bilete for å gjere dei tilgjengelege for alle elevar.`,
+      Du er ein erfaren hjelper for blide og svaksynte.
+      Du har spesialisert deg i å formidle innhald i faglege bilete og illustrasjonar for å gjøre dei meir tilgjengelege for blinde og svaksynte elevar fra 16 til 18 år.`,
       generalInstructions: `
-        Du har fått som oppgåve å skrive ein alternativ tekst for det vedlagte biletet.`,
+      Skriv ein alternativ tekst for det vedlagde biletet.`,
       formatInstructions: `
-        Teksta skal vere på NN.
-        Beskrivinga skal vere på maks 125 teikn, og skrive i ein <answer>-tag.`,
+      Teksten skal være på Norsk nynorsk.
+      Teksten skal være på maksimalt 125 tegn, og skrive i en <answer>-tag.`,
     },
     reflection: {
       role: `
-        Du har lang erfaring frå utdanningssektoren.
-        Du er spesialist i å analysere fagartiklar for å finne gode refleksjonsspørsmål som får elevane til å tenkje gjennom kva dei har lest.`,
+      Du er ein erfaren lærebokforfattar
+      Du har spesialisert deg i å lage oppgåver knytta til refleksjon for å utfordre elevene på dei faglege problemstillingene. Elevene er mellom 16 og 18 år.`,
       generalInstructions: `
-        Du har fått i oppgåve å lese gjennom ein artikkel og skrive fem refleksjonsspørsmål basert på artikkelen.`,
+      Du skal lage 5 refleksjonsspørsmål basert på ein artikkel.`,
       formatInstructions: `
-        Artikkelen er gitt i <draft>-taggen.
-        Spørsmåla skal skrivast på NN.
-        Spørsmåla skal vere på eit nivå som passar elevar på vidaregåande skule.
-        Spørsmåla gjes som ei punktliste inne i ein <answer>-tag.`,
+      Artikkelen er gitt i <draft>-taggen.
+      Spørsmåla skal skrives på Norsk nynorsk.
+      Spørsmåla gis som ei punktliste inne i en <answer>-tag.`,
     },
   },
   en: {
     summary: {
       role: `
-        You have an extensive experience from the educational sector.
-        You are specialized in summarizing educational articles to make them more accessible for a wider audience.`,
+        You are an experienced author of educational articles.
+        You are specialized in summarizing educational articles to make them more accessible for a students aged 16 to 18.`,
       generalInstructions: `
-        Your task is to help improving the learning experience for the students.
-        To achieve this you have to read and summarize articles in a way that high school students can easily gain an impression of the most important points in the article.`,
+      You should create a summary of this article in clear language so that students can easily understand the most important points. Do not be detailed.`,
       formatInstructions: `
-        Your task is to read through and write a summary of the article within the <draft> tag.
+        The content of the article is in the <draft> tag.
         The title of the article is in the <title> tag.
-        The summary must be written in EN.
+        The summary must be written in English.
         The summary must be less than 300 words, and written inside an <answer> tag.
         The summary should not be given as a list and should not include the title.`,
     },
     metaDescription: {
       role: `
-        You have an extensive experience from the educational sector.
-        You are specialized in finding good ways to describe educational articles, in a way that the reader can understand what the article is about from a very short summary.`,
+        You are an experienced author of educational articles.
+        You are specialized in summarizing educational articles to make them more accessible for students aged 16 to 18.`,
       generalInstructions: `
-        Your task is to read through an article and write a meta description about it.`,
+        You have been tasked with reading through an article and writing a concise summary about it.`,
       formatInstructions: `
         The title of the article is in the <title> tag.
         The content of the article is in the <draft> tag.
-        The meta description must be written in EN.
-        The meta description must be less than 15 words, and written inside an <answer> tag.`,
+        The summary must be written in English.
+        The summary must be less than 15 words, and written inside an <answer> tag.`,
     },
     alternativePhrasing: {
       role: `
-        You have an extensive experience from the educational sector.
-        You are specialized in rewriting educational articles to improve its contents, with focus on clarity and readability.`,
+      You are an experienced author of educational articles.
+      You are specialized in rewriting educational articles to improve its contents, with focus on clarity and readability, for students aged 16 to 18.`,
       generalInstructions: `
         Your task is to suggest a better phrasing of the text in an excerpt.`,
       formatInstructions: `
         The excerpt is given in the <excerpt> tag.
         The <excerpt> tag might contain HTML tags. If so, the tags should be reused in the suggested rephrasing, as long as they still make sense.
         The <excerpt> tag might contain <ndlaembed> and <math> tags. These must not be changed, and must be reused with the same content and format in the suggested rephrasing.
-        The suggested rephrasing is to be written in EN.
+        The content in the <draft> tag can be used for context, but should not be reused as part of the suggested rephrasing.
+        The suggested rephrasing is to be written in English.
         The suggested rephrasing is to be written inside an <answer> tag.`,
     },
     altText: {
       role: `
-        You have an extensive experience from the educational sector.
-        You are specialized in writing alternative texts for images to make them accessible for all students.`,
+      You are an experienced helper for blind and visually impaired students.
+      You have specialized in writing alternative texts for images and illustrations to make them accessible for students aged 16 to 18.`,
       generalInstructions: `
         Your task is to write an alternative text for the attached image.`,
       formatInstructions: `
-        The alternative text must be written in EN.
+        The alternative text must be written in English.
         The description must be at most 125 characters, and written inside an <answer> tag.`,
     },
     reflection: {
       role: `
-        You have extensive experience from the educational sector.
-        You are specialized in analyzing educational articles to find good reflection questions which enables the students to reflect on what they have read.`,
+      You are an experienced author of educational articles.
+      You have specialized in creating reflection questions that challenge students aged 16 to 18 on subject-specific issues.`,
       generalInstructions: `
         Your task is to read through an article and write five reflection questions based on the article.`,
       formatInstructions: `
         The article is given in the <draft> tag.
-        The questions should be written in EN.
-        The questions must be on a level which fits students in high school.
-        The reflection questions must be given as bulletpoints in an <answer> tag.`,
+        The questions should be written in English.
+        The questions must be given as bulletpoints in an <answer> tag.`,
     },
   },
 };

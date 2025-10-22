19c9b2d8258b550b000038395ae59af2319a3bb1
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-10-22 11:45:11 +0200

    chore: bump fonts and packages

--- src/client.tsx
+++ src/client.tsx
@@ -6,19 +6,6 @@
  *
  */
 
-import "@fontsource/source-sans-pro/index.css";
-import "@fontsource/source-sans-pro/400-italic.css";
-import "@fontsource/source-sans-pro/300.css";
-import "@fontsource/source-sans-pro/300-italic.css";
-import "@fontsource/source-sans-pro/600.css";
-import "@fontsource/source-sans-pro/700.css";
-import "@fontsource/source-code-pro/index.css";
-import "@fontsource/source-code-pro/variable.css";
-import "@fontsource/source-code-pro/400-italic.css";
-import "@fontsource/source-code-pro/700.css";
-import "@fontsource/source-serif-pro/index.css";
-import "@fontsource/source-serif-pro/400-italic.css";
-import "@fontsource/source-serif-pro/700.css";
 import "./style/index.css";
 
 import type { i18n } from "i18next";

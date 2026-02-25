86dd492b50d90f2717075486546da5f5084f4cf9
Amandus Søve Thorsrud <amandus.thorsrud@knowit.no>
2026-02-25 14:32:51 +0100

    feat: log error from `openid-client` if refresh token request fails

--- src/index.ts
+++ src/index.ts
@@ -87,9 +87,9 @@ app.use(
     frameguard:
       config.runtimeType === "development"
         ? {
-            action: "allow-from",
-            domain: "*://localhost",
-          }
+          action: "allow-from",
+          domain: "*://localhost",
+        }
         : undefined,
   }),
 );
@@ -120,7 +120,7 @@ app.get("*splat", async (req, res) => {
       try {
         await refreshAccessToken(req, res);
       } catch (e) {
-        log.error("Failed to refresh token on SSR request");
+        log.error("Failed to refresh token on SSR request:", e);
       }
     }
 
